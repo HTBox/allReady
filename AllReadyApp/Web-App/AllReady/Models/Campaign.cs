c342f0e0d2bb76a5465b1cd29fc7e16dd4070111
Michael Heggeseth <mheggeseth@yahoo.com>
2015-08-09 15:38:00 -0500

    Make campaign name required

--- AllReadyApp/Web-App/AllReady/Models/Campaign.cs
+++ AllReadyApp/Web-App/AllReady/Models/Campaign.cs
@@ -11,6 +11,7 @@ namespace AllReady.Models
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
