05882fbb02a8e6463b8022fe47f93f49d5f8c983
Michael Heggeseth <mheggeseth@yahoo.com>
2015-08-09 11:23:57 -0500

    Text and date filtering on campaigns list view

---
+++ AllReadyApp/Web-App/AllReady/wwwroot/js/campaigns.js
@@ -0,0 +1,11 @@
+﻿///<reference path="../lib/jquery/dist/jquery.js" />
+///<reference path="../lib/knockout/dist/knockout.js" />
+
+(function (ko, $, campaigns) {
+
+    function CampaignsViewModel(campaigns) {
+        this.campaigns = ko.observableArray(campaigns).filterBeforeDate("EndDate").textFilter(["Name", "Description"]);
+    }
+
+    ko.applyBindings(new CampaignsViewModel(campaigns));
+})(ko, $, modelCampaigns);
