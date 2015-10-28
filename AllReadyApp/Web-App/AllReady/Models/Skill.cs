4caf737b103418c9096a7eae16c76056302629bc
Michael Heggeseth <mheggeseth@yahoo.com>
2015-10-27 21:07:50 -0500

    Remove attributes from Skill

--- AllReadyApp/Web-App/AllReady/Models/Skill.cs
+++ AllReadyApp/Web-App/AllReady/Models/Skill.cs
@@ -6,7 +6,6 @@ namespace AllReady.Models
 {
     public class Skill
     {
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         public string Name { get; set; }
