dcdc291f8377ee3684928504cf447bc625c65cef
Michael Heggeseth <mheggeseth@yahoo.com>
2015-10-27 21:21:19 -0500

    Remove skill association from ApplicationUser (not ready for that yet)

--- AllReadyApp/Web-App/AllReady/Models/ApplicationUser.cs
+++ AllReadyApp/Web-App/AllReady/Models/ApplicationUser.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
-using System.Collections.Generic;
 
 namespace AllReady.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
-  {
-        public List<Skill> AssociatedSkills { get; set; }
-
-  }
+    {
+    }
 }
