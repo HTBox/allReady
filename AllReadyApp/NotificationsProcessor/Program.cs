b1ecd96d97f9d2b4dc9b3d0162a8bbc3b7cb6268
James Chambers <james@jameschambers.com>
2015-11-06 14:05:50 -0800

    Completion of initial rev of notifications
    - web job added
    - sms and email queues established
    - added required tokens to support email/queues/sms

---
+++ AllReadyApp/NotificationsProcessor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace NotificationsProcessor
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var connectionString = Environment.GetEnvironmentVariable("Data:Storage:AzureStorage");
+
+            var config = new JobHostConfiguration
+            {
+                StorageConnectionString = connectionString,
+                DashboardConnectionString = connectionString
+            };
+
+            var host = new JobHost(config);
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
