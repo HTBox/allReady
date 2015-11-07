0ff682203108c4483719d1fbf18863de10666d46
Bill Wagner <bill@thebillwagner.com>
2015-11-07 13:10:33 -0800

    set deployment source correctly
    Still debugging on the fork

--- deploy.cmd
+++ deploy.cmd
@@ -23,7 +23,7 @@ setlocal enabledelayedexpansion
 SET ARTIFACTS=%~dp0%..\artifacts
 
 IF NOT DEFINED DEPLOYMENT_SOURCE (
-  SET DEPLOYMENT_SOURCE=%~dp0%.
+  SET DEPLOYMENT_SOURCE=%~dp0%..\AllReadyApp
 )
 
 IF NOT DEFINED DEPLOYMENT_TARGET (
