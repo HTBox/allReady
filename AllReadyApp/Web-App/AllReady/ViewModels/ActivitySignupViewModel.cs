e66e3746f9df5c184a0e6d828c1d9bf2d541c568
Mike Heggeseth <mheggeseth@yahoo.com>
2015-11-08 08:13:50 -0800

    Prep for collecting additional user info on volunteering

---
+++ AllReadyApp/Web-App/AllReady/ViewModels/ActivitySignupViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.ViewModels
+{
+    public class ActivitySignupViewModel
+    {
+        public int ActivityId { get; set; }
+        public string PreferredEmail { get; set; }
+        public string PreferredPhoneNumber { get; set; }
+        public string AdditionalInfo { get; set; }
+    }
+}
