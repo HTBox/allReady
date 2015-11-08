3129abd0e1809e8ec54512a4edaad2ada36c08d9
David Paquette <david.paquette@usask.ca>
2015-11-08 15:09:52 -0800

    Notify task volunteers command

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Tasks/MessageTaskVolunteersCommand.cs
@@ -0,0 +1,10 @@
+﻿using AllReady.Areas.Admin.Models;
+using MediatR;
+
+namespace AllReady.Areas.Admin.Features.Tasks
+{
+    public class MessageTaskVolunteersCommand : IRequest
+    {
+        public MessageTaskVolunteersModel Model {get; set;}
+    }
+}
