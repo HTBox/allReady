a7ef37d2f413d3766dfeb0a8b08111a7fb82921e
James Chambers <james@jameschambers.com>
2015-11-08 11:06:42 -0800

    Building link for admin to access the site

--- AllReadyApp/Web-App/AllReady/Features/Notifications/NotifyAdminForActivitySignup.cs
+++ AllReadyApp/Web-App/AllReady/Features/Notifications/NotifyAdminForActivitySignup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AllReady.Models;
 using MediatR;
 using Microsoft.Data.Entity;
+using Microsoft.Framework.OptionsModel;
 
 namespace AllReady.Features.Notifications
 {
@@ -12,11 +13,13 @@ namespace AllReady.Features.Notifications
     {
         private readonly AllReadyContext _context;
         private readonly IMediator _bus;
+        private readonly IOptions<GeneralSettings> _options;
 
-        public NotifyAdminForActivitySignup(AllReadyContext context, IMediator bus)
+        public NotifyAdminForActivitySignup(AllReadyContext context, IMediator bus, IOptions<GeneralSettings> options)
         {
             _context = context;
             _bus = bus;
+            _options = options;
         }
 
         public void Handle(VolunteerInformationAdded notification)
@@ -26,6 +29,7 @@ namespace AllReady.Features.Notifications
             var campaign = _context.Campaigns
                 .Include(c => c.Organizer)
                 .Single(c => c.Id == activity.CampaignId);
+            var link = $"View activity: http://{_options.Value.SiteBaseUrl}/Admin/Activity/Details/{activity.Id}";
 
             var subject = $"A volunteer has signed up for {activity.Name}";
 
@@ -33,7 +37,7 @@ namespace AllReady.Features.Notifications
             {
                 ViewModel = new NotifyVolunteersViewModel
                 {
-                    EmailMessage = $"Your {campaign.Name} campaign activity '{activity.Name}' has a new volunteer. {voluteer.UserName} can be reached at {voluteer.Email}.",
+                    EmailMessage = $"Your {campaign.Name} campaign activity '{activity.Name}' has a new volunteer. {voluteer.UserName} can be reached at {voluteer.Email}. {link}",
                     EmailRecipients = new List<string> { campaign.Organizer.Email },
                     Subject = subject
                 }
