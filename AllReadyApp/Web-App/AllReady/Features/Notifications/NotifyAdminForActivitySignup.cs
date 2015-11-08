fb29420d80b9edc7e801049357f459fb55c676e6
James Chambers <james@jameschambers.com>
2015-11-08 10:25:42 -0800

    Correcting class, adding constructor

--- AllReadyApp/Web-App/AllReady/Features/Notifications/NotifyAdminForActivitySignup.cs
+++ AllReadyApp/Web-App/AllReady/Features/Notifications/NotifyAdminForActivitySignup.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AllReady.Models;
 using MediatR;
 
 namespace AllReady.Features.Notifications
 {
-    public class NotifyAdminForActivitySignu: INotificationHandler<VolunteerInformationAdded>
+    public class NotifyAdminForActivitySignup: INotificationHandler<VolunteerInformationAdded>
     {
+        private readonly AllReadyContext _context;
+        public NotifyAdminForActivitySignup(AllReadyContext context)
+        {
+            _context = context;
+        }
+
         public void Handle(VolunteerInformationAdded notification)
         {
             throw new NotImplementedException();
