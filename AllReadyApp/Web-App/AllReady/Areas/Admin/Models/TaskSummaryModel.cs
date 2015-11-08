31cd291e9864f58b9c05286717b411c35666a585
Brian Beatty <bcbeatty@gmail.com>
2015-11-08 11:02:48 -0800

    ModelView to Model rename changes

--- AllReadyApp/Web-App/AllReady/Areas/Admin/Models/TaskSummaryModel.cs
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Models/TaskSummaryModel.cs
@@ -1,9 +1,7 @@
-﻿using AllReady.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace AllReady.Areas.Admin.Models
 {
--- AllReadyApp/Web-App/AllReady/Areas/Admin/Models/VolunteerModel.cs
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Models/VolunteerModel.cs
@@ -1,4 +1,4 @@
-﻿namespace AllReady.Areas.Admin.ViewModels
+﻿namespace AllReady.Areas.Admin.Models
 {
     public class VolunteerModel
     {
