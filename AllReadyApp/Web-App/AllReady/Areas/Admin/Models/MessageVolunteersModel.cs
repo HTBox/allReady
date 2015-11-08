74e84a38b1e64b397915dc93531e5070d8017085
David Paquette <david.paquette@usask.ca>
2015-11-08 12:55:18 -0800

    Merging with ViewModel rename changes

--- AllReadyApp/Web-App/AllReady/Areas/Admin/Models/MessageVolunteersModel.cs
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Models/MessageVolunteersModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AllReady.Areas.Admin.ViewModels
+namespace AllReady.Areas.Admin.Models
 {
     public class MessageActivityVolunteersModel
     {
