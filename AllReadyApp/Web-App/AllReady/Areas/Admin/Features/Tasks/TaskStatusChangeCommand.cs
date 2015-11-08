1c94a966e493974b1a78b023b963f4bfe69179c7
Rockford Lhotka <rocky@lhotka.net>
2015-11-08 11:46:35 -0800

    #41 Add task status change command/handler

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Tasks/TaskStatusChangeCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.Areas.Admin.Features.Tasks
+{
+    public class TaskStatusChangeCommand : IRequest
+    {
+        public int TaskId { get; set; }
+        public string UserId { get; set; }
+        public TaskStatus TaskStatus { get; set; }
+    }
+
+    public enum TaskStatus
+    {
+        Assigned,
+        Accepted,
+        Rejected,
+        Completed,
+        CanNotComplete
+    }
+}
