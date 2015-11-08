9710aa06826b0a982480947b2db6d37b8c9d565a
James Chambers <james@jameschambers.com>
2015-11-08 10:20:43 -0800

    Adding event signature and rough-in of handler

---
+++ AllReadyApp/Web-App/AllReady/Features/Notifications/NotifyAdminForActivitySignup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace AllReady.Features.Notifications
+{
+    public class NotifyAdminForActivitySignu: INotificationHandler<VolunteerInformationAdded>
+    {
+        public void Handle(VolunteerInformationAdded notification)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
