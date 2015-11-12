b3e64406bd647f249c722dd4854eddcd9193e222
joelhulen <joelhulen@gmail.com>
2015-11-12 02:27:40 -0800

    * Completed logo/image upload simplification within the Campaign edit page, per issue #246 * Added FileUploadExtensions, which contains an extension method to check whether the uploaded file is a valid image type * Fixed LocationExtensions typo (was named "LocationExtenstions"), and modified to return a new LocationDisplayModel if the Location parameter is null, rather than returning null, which was causing null reference errors on the associated views

--- AllReadyApp/Web-App/AllReady/Services/IImageService.cs
+++ AllReadyApp/Web-App/AllReady/Services/IImageService.cs
@@ -9,6 +9,7 @@ namespace AllReady.Services
     public interface IImageService
     {
         Task<string> UploadActivityImageAsync(int tenantId, int activityId, IFormFile image);
+        Task<string> UploadCampaignImageAsync(int tenantId, int campaignId, IFormFile image);
         Task<string> UploadImageAsync(IFormFile image);
         Task<string> UploadTenantImageAsync(int tenantId, IFormFile image);
     }
