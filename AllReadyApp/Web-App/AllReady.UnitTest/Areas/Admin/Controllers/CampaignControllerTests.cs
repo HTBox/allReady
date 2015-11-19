44fb9add24382e205407029adcbccb955e16330f
WilliamBerryiii <wberry@lucidmotions.net>
2015-11-19 00:17:10 -0800

    Added tests covering branching logic for CampaignController Detail action.

--- AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Controllers/CampaignControllerTests.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Controllers/CampaignControllerTests.cs
@@ -1,8 +1,11 @@
-﻿using AllReady.Areas.Admin.Controllers;
+﻿using System.Security.Claims;
+using AllReady.Areas.Admin.Controllers;
 using AllReady.Areas.Admin.Features.Campaigns;
+using AllReady.Areas.Admin.Models;
 using AllReady.Models;
 using AllReady.Services;
 using MediatR;
+using Microsoft.AspNet.Http;
 using Moq;
 using Xunit;
 using Microsoft.AspNet.Mvc;
@@ -26,6 +29,70 @@ namespace AllReady.UnitTest.Areas.Admin.Controllers
             var actionResult = controller.Details(0);
             Assert.IsType<HttpNotFoundResult>(actionResult);
             mockMediator.Verify(mock => mock.Send(It.IsAny<CampaignDetailQuery>()), Times.Once);
+        }
+
+        [Fact]
+        public void CampaignDetailReturnsUnauthorizedForNonAdmin()
+        {
+            const int tennantId = 1;
+            var controller = SetupCampaignController(tennantId);
+
+            var claimsProvider = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new[]
+                        {
+                            new Claim(AllReady.Security.ClaimTypes.UserType, UserType.BasicUser.ToString()),
+                            new Claim(AllReady.Security.ClaimTypes.Tenant, tennantId.ToString()),
+                        }));
+            var mockHttpContext = new Mock<HttpContext>();
+            mockHttpContext.Setup(mock => mock.User)
+                .Returns(() => claimsProvider);
+            var mockContext = new Mock<ActionContext>();
+            mockContext.Object.HttpContext = mockHttpContext.Object;
+            controller.ActionContext = mockContext.Object;
+
+            var actionResult = controller.Details(0);
+            Assert.IsType<HttpUnauthorizedResult>(actionResult);
+        }
+
+
+        [Fact]
+        public void CampaignDetailReturnsViewForAdmin()
+        {
+            const int tennantId = 1;
+            var controller = SetupCampaignController(tennantId);
+
+            var claimsProvider = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new[]
+                        {
+                            new Claim(AllReady.Security.ClaimTypes.UserType, UserType.TenantAdmin.ToString()),
+                            new Claim(AllReady.Security.ClaimTypes.Tenant, tennantId.ToString()),
+                        }));
+            var mockHttpContext = new Mock<HttpContext>();
+            mockHttpContext.Setup(mock => mock.User)
+                .Returns(() => claimsProvider);
+            var mockContext = new Mock<ActionContext>();
+            mockContext.Object.HttpContext = mockHttpContext.Object;
+            controller.ActionContext = mockContext.Object;
+
+            var actionResult = controller.Details(0);
+            Assert.IsType<ViewResult>(actionResult);
+        }
+
+        private static CampaignController SetupCampaignController(int tennantId)
+        {
+            var mockMediator = new Mock<IMediator>();
+            mockMediator.Setup(mock => mock.Send(It.IsAny<CampaignDetailQuery>()))
+                .Returns(() => new CampaignDetailModel {TenantId = tennantId})
+                .Verifiable();
+            var mockImageService = new Mock<IImageService>();
+            var mockDataAccess = new Mock<IAllReadyDataAccess>();
+            var controller = new CampaignController(
+                mockMediator.Object,
+                mockImageService.Object,
+                mockDataAccess.Object);
+            return controller;
         }
     }
 }
