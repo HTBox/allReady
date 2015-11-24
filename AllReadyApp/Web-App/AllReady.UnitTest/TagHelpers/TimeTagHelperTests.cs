1b8608c2369986a8745eab29f4d42ea33276d0e2
David Paquette <david.paquette@usask.ca>
2015-11-24 18:09:31 -0500

    Unit tests and comments for TimeTagHelper

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/TagHelpers/TimeTagHelperTests.cs
@@ -0,0 +1,72 @@
+﻿using AllReady.TagHelpers;
+using Microsoft.AspNet.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AllReady.UnitTest.TagHelpers
+{
+    public class TimeTagHelperTests
+    {
+        private Func<bool, Task<TagHelperContent>> GetEmptyChildContent()
+        {
+            TagHelperContent content = new DefaultTagHelperContent();
+            return b => Task.FromResult(content);
+        }
+
+        private TagHelperContext GetContext()
+        {
+            return new TagHelperContext(new IReadOnlyTagHelperAttribute[] { }, new Dictionary<object, object>(), Guid.NewGuid().ToString());
+        }
+
+        private TagHelperOutput GetOutput()
+        {
+            return new TagHelperOutput("time", new TagHelperAttributeList(), GetEmptyChildContent());
+        }
+
+        [Fact]
+        public void ValueShouldBeFormattedUsingDefaultFormat()
+        {
+            TimeTagHelper tagHelper = new TimeTagHelper();
+            tagHelper.Value = new DateTimeOffset(2014, 12, 25, 13, 21, 0, TimeSpan.FromHours(0));
+
+            var output = GetOutput();
+            tagHelper.Process(GetContext(), output);
+
+            Assert.Null(output.TagName);
+            Assert.Equal("12/25/2014 1:21 PM", output.Content.GetContent());
+        }
+
+        [Fact]
+        public void ValueShouldBeFormattedUsingSpecifiedFormat()
+        {
+            TimeTagHelper tagHelper = new TimeTagHelper();
+            tagHelper.Value = new DateTimeOffset(2014, 12, 25, 13, 21, 0, TimeSpan.FromHours(0));
+            tagHelper.Format = "yyyy-MM-dd";
+
+            var output = GetOutput();
+            tagHelper.Process(GetContext(), output);
+
+            Assert.Null(output.TagName);
+            Assert.Equal("2014-12-25", output.Content.GetContent());
+        }
+
+        [Fact]
+        public void ValueShouldBeConvertedToSpecifiedTimeZone()
+        {
+            TimeTagHelper tagHelper = new TimeTagHelper();
+            tagHelper.Value = new DateTimeOffset(2014, 12, 25, 13, 21, 0, TimeSpan.FromHours(0));
+            tagHelper.Format = "yyyy-MM-dd h:mm tt";
+            tagHelper.TargetTimeZoneId = "Central Standard Time";
+
+            var output = GetOutput();
+            tagHelper.Process(GetContext(), output);
+
+            Assert.Null(output.TagName);
+            Assert.Equal("2014-12-25 7:21 AM", output.Content.GetContent());
+
+        }
+    }
+}
--- AllReadyApp/Web-App/AllReady/TagHelpers/TimeTagHelper.cs
+++ AllReadyApp/Web-App/AllReady/TagHelpers/TimeTagHelper.cs
@@ -6,21 +6,34 @@ using System.Threading.Tasks;
 
 namespace AllReady.TagHelpers
 {
+    /// <summary>
+    /// Formats a DateTimeOffset
+    /// </summary>
     [HtmlTargetElement("time", Attributes="value")]
     public class TimeTagHelper : TagHelper
     {
+        /// <summary>
+        /// The DateTimeOffset value to format
+        /// </summary>
         [HtmlAttributeName("value")]
         public DateTimeOffset? Value { get; set; }
 
+        /// <summary>
+        /// The DateTime format string used to format the value.
+        /// Default of "g" is used if none is specified
+        /// </summary>
         [HtmlAttributeName("format")]
         public string Format { get; set; } = "g";
 
+        /// <summary>
+        /// If specified, the value will be converted to this target time zone
+        /// </summary>
         [HtmlAttributeName("target-time-zone-id")]
         public string TargetTimeZoneId { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            output.TagName = "span";
+            output.TagName = null;
             
             if (!Value.HasValue)
             {
