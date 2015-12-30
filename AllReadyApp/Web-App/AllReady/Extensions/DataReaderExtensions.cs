cd39b90f4529a69bdb9114395a9da78c0cf048b1
Russ Hammett <Kritner@gmail.com>
2015-12-30 16:42:08 -0500

    -Removed ClosestLocationService, moved into DbContext w/ EF. -Updated references to IClosestLocation to IAlreadyDataAccess w/ same name and signature Since "DataReaderExtensions" was only used in ClosestLocationService, I moved it to the extension folder (as well as unit tests) and removed ClosestLocationService. -Upated test relying on a second parameter in ActivityApiController
    -Note I'm pretty unclear with using EF in the code first approach, so hopefully my "mapping" in AllreadyContext.cs is not wrong.

---
+++ AllReadyApp/Web-App/AllReady/Extensions/DataReaderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.Extensions
+{
+    public static class DataReaderExtensions
+    {
+        public static List<T> DataReaderMapToList<T>(this IDataReader dr)
+        {
+            var list = new List<T>();
+            while (dr.Read())
+            {
+                var obj = Activator.CreateInstance<T>();
+                foreach (var prop in obj
+                    .GetType()
+                    .GetProperties()
+                    .Where(prop => !Equals(dr[prop.Name], DBNull.Value)))
+                {
+                    prop.SetValue(obj, dr[prop.Name], null);
+                }
+                list.Add(obj);
+            }
+            return list;
+        }
+    }
+}
