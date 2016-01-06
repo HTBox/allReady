e51bb159f29fc8c7fdd39d7390cdfcea0d967fe9
Eric Potter <eric.b.potter@gmail.com>
2016-01-06 08:16:20 -0500

    Making sure firefox is installed

--- AllReadyApp/AllReady.IntegrationTest/Program.fs
+++ AllReadyApp/AllReady.IntegrationTest/Program.fs
@@ -2,10 +2,14 @@
 open runner
 open System
 
+Console.WriteLine("Launching Scenario Tests")
+
 //start an instance of the firefox browser
 start firefox
 pin Left
 resize (1024, 768)
+
+Console.WriteLine("Launched Firefox")
 
 CreatingOrgsAndCampaigns.All Constants.UrlLocalHost
 CreatingActivitiesForCampaigns.All Constants.UrlLocalHost
