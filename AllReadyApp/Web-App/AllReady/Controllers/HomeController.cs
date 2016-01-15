3d5112ace812f34f949367d2950f659ca67c84df
Michael Moore <mike@fiddlebits.net>
2016-01-15 08:58:05 -0800

    implemented changes as discussed in PR review

--- AllReadyApp/Web-App/AllReady/Controllers/HomeController.cs
+++ AllReadyApp/Web-App/AllReady/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using AllReady.Models;
 using AllReady.ViewModels;
 using Microsoft.AspNet.Mvc;
@@ -16,7 +17,7 @@ namespace AllReady.Controllers
 
         public IActionResult Index()
         {
-            return View(_dataAccess.Campaigns.ToViewModel().ToList());
+            return View(_dataAccess.Campaigns.Where(c => c.EndDateTime.UtcDateTime.Date > DateTime.UtcNow.Date).ToViewModel().OrderBy(vm => vm.EndDate).ToList());
         }
 
         public IActionResult About()
--- AllReadyApp/Web-App/AllReady/wwwroot/js/index.js
+++ AllReadyApp/Web-App/AllReady/wwwroot/js/index.js
@@ -13,12 +13,7 @@
 
     function IndexViewModel(campaigns) {
         var list = campaigns.map(function (item) { return new Campaign(item); })
-        this.campaigns = ko.observableArray(list).filterBeforeDate("EndDate").textFilter(["Name", "Description"]);
-        this.total = campaigns.length;
-
-        this.status = ko.observable();
-        this.activities = ko.observableArray();
-        this.loadingDone = ko.observable(false);
+        this.campaigns = ko.observableArray(list);
     }
 
     ko.applyBindings(new IndexViewModel(campaigns));
