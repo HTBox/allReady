46a8905b3b745f494d8c563bceb1992f48018493
James Chambers <james@jameschambers.com>
2016-02-06 12:18:44 -0600

    implementation of handler

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Users/DeleteUserCommandHandlerShould.cs
@@ -0,0 +1,43 @@
+﻿using AllReady.Areas.Admin.Features.Users;
+using AllReady.Models;
+using Microsoft.AspNet.Identity;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AllReady.UnitTest.Areas.Admin.Features.Users
+{
+    public class DeleteUserCommandHandlerShould : InMemoryContextTest
+    {
+
+        protected override void LoadTestData()
+        {
+            var userId = "foo_id";
+            var userName = "foo_user";
+
+            var testuser = new ApplicationUser() { UserName = userName, Email = userName, Id = userId };
+            var createResult = UserManager.CreateAsync(testuser).Result;
+        }
+
+        [Fact]
+        public void RemoveUserFromUserManager()
+        {
+            // arrange
+            var handler = new DeleteUserCommandHandler(Context, UserManager);
+            var userId = "foo_id";
+
+            // act
+            handler.Handle(new DeleteUserCommand { UserId = userId });
+
+            // assert
+            var user = UserManager.FindByIdAsync(userId).Result;
+            Assert.Null(user);
+
+
+        }
+
+    }
+}
--- AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Users/DeleteUserCommandHandler.cs
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Users/DeleteUserCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using AllReady.Models;
+using MediatR;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,19 @@ namespace AllReady.Areas.Admin.Features.Users
 {
     public class DeleteUserCommandHandler : RequestHandler<DeleteUserCommand>
     {
+        private readonly AllReadyContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DeleteUserCommandHandler(AllReadyContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
         protected override void HandleCore(DeleteUserCommand message)
         {
-            throw new NotImplementedException();
+            var user = _userManager.FindByIdAsync(message.UserId).Result;
+            _userManager.DeleteAsync(user);
         }
     }
 }
