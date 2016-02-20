680d419070e6db408830dd7ae41f218db4ea3918
Steve Gordon <sgordon@hotmail.co.uk>
2016-02-20 13:26:51 +0000

    Updating model and migrations to support org level privacy policy

--- AllReadyApp/Web-App/AllReady/Models/Organization.cs
+++ AllReadyApp/Web-App/AllReady/Models/Organization.cs
@@ -2,15 +2,15 @@
 
 namespace AllReady.Models
 {
-  /// <summary>
-  /// The highest level of organization in the application, typically represents a non-government organization (NGO)
-  /// </summary>
-  public class Organization
-  {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string LogoUrl { get; set; }
-    public string WebUrl { get; set; }
+    /// <summary>
+    /// The highest level of organization in the application, typically represents a non-government organization (NGO)
+    /// </summary>
+    public class Organization
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LogoUrl { get; set; }
+        public string WebUrl { get; set; }
 
         /// <summary>
         /// Collection of the campaigns directly managed by this Organization
@@ -26,5 +26,10 @@ namespace AllReady.Models
         public Location Location { get; set; }
 
         public List<OrganizationContact> OrganizationContacts { get; set; }
-  }
+
+        /// <summary>
+        /// Represents html for an organization specific privacy policy
+        /// </summary>
+        public string PrivacyPolicy { get; set; }
+    }
 }
