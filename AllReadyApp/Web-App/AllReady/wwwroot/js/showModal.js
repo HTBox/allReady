300feb71c6bee831d1fb989f0dc592557eb18e52
Michael Moore <mike@fiddlebits.net>
2016-02-25 14:46:38 -0800

    one more refactoring

--- AllReadyApp/Web-App/AllReady/wwwroot/js/showModal.js
+++ AllReadyApp/Web-App/AllReady/wwwroot/js/showModal.js
@@ -8,16 +8,22 @@ var HTBox = HTBox || {};
     // This is a wrapper around the Bootstrap modal dialog that encapsulates the Bootstrap modal with a Knockout view model,
     // providing a clear separation of concerns between the view model and UI.
     //
+    var __viewModel;
+    var __modalId;
+    var __$modalElement;
+    var __onCloseCallbackFn;
+    var __onCancelCallbackFn;
+
     function showModal(options) {
         if (typeof options === "undefined") throw new Error("An options argument is required.");
         if (typeof options.viewModel !== "object") throw new Error("options.viewModel is required.");
         if (!options.modalId) throw new Error("options.modalId is required.");
 
-        var __viewModel = options.viewModel;
-        var __modalId = options.modalId;
-        var __$modalElement = $("#" + __modalId);
-        var __onCloseCallbackFn = null;
-        var __onCancelCallbackFn = null;
+        __viewModel = options.viewModel;
+        __modalId = options.modalId;
+        __$modalElement = $("#" + __modalId);
+        __onCloseCallbackFn = null;
+        __onCancelCallbackFn = null;
 
         ko.cleanNode(__$modalElement[0]);
         ko.applyBindings(__viewModel, __$modalElement[0]);
@@ -41,29 +47,28 @@ var HTBox = HTBox || {};
 
 
         function __close(callbackParam) {
-            this.$modalElement.modal("hide");
+            __hide();
             if (typeof __onCloseCallbackFn === "function") {
                 __onCloseCallbackFn(callbackParam);
             }
         }
 
         function __cancel(callbackParam) {
-            this.$modalElement.modal("hide");
+            __hide();
             if (typeof __onCancelCallbackFn === "function") {
                 __onCancelCallbackFn(callbackParam);
             }
         }
 
         function __hide() {
-            this.$modalElement.modal("hide");
+            __$modalElement.modal("hide");
         }
 
         function __show() {
-            this.$modalElement.modal("show");
+            __$modalElement.modal("show");
         }
 
         return {
-            $modalElement: __$modalElement,
             hide: __hide,
             show: __show,
             close: __close,
