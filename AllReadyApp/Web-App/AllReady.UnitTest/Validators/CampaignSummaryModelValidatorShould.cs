d5e2d8b995cfe236487955c5c77cd4f10d8f1796
James Chambers <james@jameschambers.com>
2016-03-12 12:24:49 -0600

    Adding validator for model

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Validators/CampaignSummaryModelValidatorShould.cs
@@ -0,0 +1,37 @@
+﻿using AllReady.Areas.Admin.Features.Campaigns;
+using AllReady.Areas.Admin.Models;
+using AllReady.Areas.Admin.Models.Validators;
+using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AllReady.UnitTest.Validators
+{
+    public class CampaignSummaryModelValidatorShould
+    {
+        [Fact]
+        public async Task ReportErrorsWhenDatesAreInvalid()
+        {
+            // arrage
+            var mediator = new Mock<IMediator>();
+            var validator = new CampaignSummaryModelValidator(mediator.Object);
+            var model = new CampaignSummaryModel
+            {
+                StartDate = new DateTimeOffset(new DateTime(2000, 1, 1)),
+                EndDate = new DateTimeOffset(new DateTime(1999, 1, 1))
+            };
+
+            // act
+            var errors = await validator.Validate(model);
+
+            // assert
+            Assert.True(errors.ContainsKey("EndDate"));
+        }
+
+
+    }
+}
---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Models/Validators/CampaignSummaryModelValidator.cs
@@ -0,0 +1,32 @@
+﻿using AllReady.Areas.Admin.Features.Campaigns;
+using AllReady.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.Areas.Admin.Models.Validators
+{
+    public class CampaignSummaryModelValidator
+    {
+        private IMediator _mediator;
+
+        public CampaignSummaryModelValidator(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<Dictionary<string, string>> Validate (CampaignSummaryModel model)
+        {
+            var result = new Dictionary<string, string>();
+
+            if (model.EndDate < model.StartDate)
+            {
+                result.Add(nameof(model.EndDate), "The end date must fall after the start date.");
+            }
+
+            return result;
+        }
+    }
+}
