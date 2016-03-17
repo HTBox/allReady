57a74fec729b0d1e3241fe521b86591b73bd7fb2
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-03-17 14:48:05 -0400

    inital commit of AdminControllerTests. Added Register() tests

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Controllers/AdminControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AllReady.Controllers;
+using AllReady.UnitTest.Extensions;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.OptionsModel;
+using Moq;
+using Xunit;
+
+namespace AllReady.UnitTest.Controllers
+{
+    public class AdminControllerTests
+    {
+        [Fact]
+        public void RegisterReturnsViewResult()
+        {
+            var sut = new AdminController(null, null, null, null, Mock.Of<IOptions<SampleDataSettings>>(), Mock.Of<IOptions<GeneralSettings>>());
+            var result = sut.Register();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void RegisterHasHttpGetAttribute()
+        {
+            var sut = new AdminController(null, null, null, null, Mock.Of<IOptions<SampleDataSettings>>(), Mock.Of<IOptions<GeneralSettings>>());
+            var attribute = sut.GetAttributesOn(x => x.Register()).OfType<HttpGetAttribute>().SingleOrDefault();
+            Assert.NotNull(attribute);
+        }
+
+        [Fact]
+        public void RegisterHasAllowAnonymousAttribute()
+        {
+            var sut = new AdminController(null, null, null, null, Mock.Of<IOptions<SampleDataSettings>>(), Mock.Of<IOptions<GeneralSettings>>());
+            var attribute = sut.GetAttributesOn(x => x.Register()).OfType<AllowAnonymousAttribute>().SingleOrDefault();
+            Assert.NotNull(attribute);
+        }
+    }
+}
