8fa3b2515b3607b4c928c29fc044d907de84272b
Patrick Boyer <boyer.patrick@gmail.com>
2016-03-29 18:44:30 -0400

    add unit tests

--- AllReadyApp/Web-App/AllReady.UnitTest/Activities/EditActivity.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Activities/EditActivity.cs
@@ -128,5 +128,102 @@ namespace AllReady.UnitTest.Activities
             Assert.Equal(-6, data.EndDateTime.Offset.TotalHours);
         }
 
+        [Fact]
+        public void ExistingActivityUpdateLocation()
+        {
+            PostalCodeGeo seattlePostalCode = new PostalCodeGeo() { City = "Seattle", PostalCode = "98117", State = "WA" };
+            Location seattle = new Location()
+            {
+                Id = 1,
+                Address1 = "123 Main Street",
+                Address2 = "Unit 2",
+                City = "Seattle",
+                PostalCode = seattlePostalCode,
+                Country = "USA",
+                State = "WA",
+                Name = "Organizer name",
+                PhoneNumber = "555-555-5555"
+            };
+
+            var context = ServiceProvider.GetService<AllReadyContext>();
+            Organization htb = new Organization()
+            {
+                Id = 123,
+                Name = "Humanitarian Toolbox",
+                LogoUrl = "http://www.htbox.org/upload/home/ht-hero.png",
+                WebUrl = "http://www.htbox.org",
+                Campaigns = new List<Campaign>()
+            };
+            Campaign firePrev = new Campaign()
+            {
+                Id = 1,
+                Name = "Neighborhood Fire Prevention Days",
+                ManagingOrganization = htb,
+                TimeZoneId = "Central Standard Time"
+            };
+            htb.Campaigns.Add(firePrev);
+            Activity queenAnne = new Activity()
+            {
+                Id = 100,
+                Name = "Queen Anne Fire Prevention Day",
+                Campaign = firePrev,
+                CampaignId = firePrev.Id,
+                StartDateTime = new DateTime(2015, 7, 4, 10, 0, 0).ToUniversalTime(),
+                EndDateTime = new DateTime(2015, 12, 31, 15, 0, 0).ToUniversalTime(),
+                Location = seattle,
+                RequiredSkills = new List<ActivitySkill>()
+            };
+            context.Locations.Add(seattle);
+            context.Organizations.Add(htb);
+            context.Activities.Add(queenAnne);
+            context.SaveChanges();
+
+            var NEW_LOCATION = LocationExtensions.ToEditModel(new Location()
+            {
+                Address1 = "123 new address",
+                Address2 = "new suite",
+                PostalCode = new PostalCodeGeo() { City = "Bellevue", PostalCode = "98004", State = "WA" },
+                City = "Bellevue",
+                State = "WA",
+                Country = "USA",
+                Name = "New name",
+                PhoneNumber = "New number"
+            });
+
+            var locationEdit = new ActivityDetailModel
+            {
+                CampaignId = queenAnne.CampaignId,
+                CampaignName = queenAnne.Campaign.Name,
+                Description = queenAnne.Description,
+                EndDateTime = queenAnne.EndDateTime,
+                Id = queenAnne.Id,
+                ImageUrl = queenAnne.ImageUrl,
+                Location = NEW_LOCATION,
+                Name = queenAnne.Name,
+                RequiredSkills = queenAnne.RequiredSkills,
+                TimeZoneId = "Central Standard Time",
+                StartDateTime = queenAnne.StartDateTime,
+                Tasks = null,
+                OrganizationId = queenAnne.Campaign.ManagingOrganizationId,
+                OrganizationName = queenAnne.Campaign.ManagingOrganization.Name,
+                Volunteers = null
+            };
+
+            var query = new EditActivityCommand { Activity = locationEdit };
+            var handler = new EditActivityCommandHandler(context);
+            var result = handler.Handle(query);
+            Assert.Equal(100, result); // should get back the activity id
+
+            var data = context.Activities.Single(a => a.Id == result);
+            Assert.Equal(data.Location.Address1, NEW_LOCATION.Address1);
+            Assert.Equal(data.Location.Address2, NEW_LOCATION.Address2);
+            Assert.Equal(data.Location.City, NEW_LOCATION.City);
+            Assert.Equal(data.Location.PostalCode?.PostalCode, NEW_LOCATION.PostalCode);
+            Assert.Equal(data.Location.State, NEW_LOCATION.State);
+            Assert.Equal(data.Location.Country, NEW_LOCATION.Country);
+            Assert.Equal(data.Location.PhoneNumber, NEW_LOCATION.PhoneNumber);
+            Assert.Equal(data.Location.Name, NEW_LOCATION.Name);
+        }
     }
 }
+
--- AllReadyApp/Web-App/AllReady.UnitTest/Activities/GetActivityDetail.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Activities/GetActivityDetail.cs
@@ -12,6 +12,22 @@ namespace AllReady.UnitTest.Activities
         protected override void LoadTestData()
         {
             var context = ServiceProvider.GetService<AllReadyContext>();
+
+            PostalCodeGeo seattlePostalCode = new PostalCodeGeo() { City = "Seattle", PostalCode = "98117", State = "WA" };
+            Location seattle = new Location()
+            {
+                Id = 1,
+                Address1 = "123 Main Street",
+                Address2 = "Unit 2",
+                City = "Seattle",
+                PostalCode = seattlePostalCode,
+                Country = "USA",
+                State = "WA",
+                Name = "Organizer name",
+                PhoneNumber = "555-555-5555"                            
+            };
+         
+
             Organization htb = new Organization()
             {
                 Name = "Humanitarian Toolbox",
@@ -36,6 +52,8 @@ namespace AllReady.UnitTest.Activities
                 Location = new Location { Id = 1 },
                 RequiredSkills = new List<ActivitySkill>()
             };
+            context.PostalCodes.Add(seattlePostalCode);
+            context.Locations.Add(seattle);
             context.Organizations.Add(htb);
             context.Activities.Add(queenAnne);
             context.SaveChanges();
@@ -60,6 +78,25 @@ namespace AllReady.UnitTest.Activities
             var result = handler.Handle(query);
             Assert.Null(result);
         }
+
+        [Fact]
+        public void ActivityIncludesAllLocationInformation()
+        {
+            var context = ServiceProvider.GetService<AllReadyContext>();
+            var query = new ActivityDetailQuery() { ActivityId = 1 };
+            var handler = new ActivityDetailQueryHandler(context);
+            var result = handler.Handle(query);
+            Assert.NotNull(result.Location);
+            Assert.NotNull(result.Location?.Id);
+            Assert.NotNull(result.Location?.Address1);
+            Assert.NotNull(result.Location?.Address2);
+            Assert.NotNull(result.Location?.PostalCode);
+            Assert.NotNull(result.Location?.State);
+            Assert.NotNull(result.Location?.Name);
+            Assert.NotNull(result.Location?.PhoneNumber);
+            Assert.NotNull(result.Location?.Country);
+        }
+
 
     }
 }
