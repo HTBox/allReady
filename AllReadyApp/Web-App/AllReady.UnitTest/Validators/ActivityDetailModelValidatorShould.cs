bbbc5f675955689b2cfd59700db3695b29a7e495
Patrick Boyer <boyer.patrick@gmail.com>
2016-03-29 23:24:02 -0400

    add unit tests for validators and make unit tests pass
    validators address problems identified in issue 545

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Validators/ActivityDetailModelValidatorShould.cs
@@ -0,0 +1,104 @@
+﻿using AllReady.Areas.Admin.Models;
+using AllReady.Areas.Admin.Models.Validators;
+using MediatR;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AllReady.UnitTest.Validators
+{
+    public class ActivityDetailModelValidatorShould
+    {
+        [Fact]
+        public async Task ReportErrorsWhenEndDateIsInvalid()
+        {
+            // arrage
+            var mediator = new Mock<IMediator>();
+            var validator = new ActivityDetailModelValidator(mediator.Object);
+            var campaign = new CampaignSummaryModel();
+            var model = new ActivityDetailModel
+            {
+                StartDateTime = new DateTimeOffset(new DateTime(2000, 1, 1)),
+                EndDateTime = new DateTimeOffset(new DateTime(1999, 1, 1))
+            };
+
+            // act
+            var errors = await validator.Validate(model, campaign);
+
+            // assert
+            Assert.True(errors.Exists(x => x.Key.Equals("EndDateTime")));
+        }
+
+        [Fact]
+        public async Task ReportErrorsWhenEndDateOutsideCampaignWindow()
+        {
+            // arrage
+            var mediator = new Mock<IMediator>();
+            var validator = new ActivityDetailModelValidator(mediator.Object);
+            var campaign = new CampaignSummaryModel()
+            {
+                StartDate = new DateTimeOffset(new DateTime(2000, 1, 1)),
+                EndDate = new DateTimeOffset(new DateTime(1999, 1, 1))
+            };
+
+            var model = new ActivityDetailModel
+            {
+                EndDateTime = new DateTimeOffset(new DateTime(2001, 1, 1))
+            };
+
+            // act
+            var errors = await validator.Validate(model, campaign);
+
+            // assert
+            Assert.True(errors.Exists(x => x.Key.Equals("EndDateTime")));
+        }
+
+        [Fact]
+        public async Task ReportErrorsWhenStartDateOutsideCampaignWindow()
+        {
+            // arrage
+            var mediator = new Mock<IMediator>();
+            var validator = new ActivityDetailModelValidator(mediator.Object);
+            var campaign = new CampaignSummaryModel()
+            {
+                StartDate = new DateTimeOffset(new DateTime(2000, 1, 1)),
+                EndDate = new DateTimeOffset(new DateTime(2001, 1, 1))
+            };
+            var model = new ActivityDetailModel
+            {
+                StartDateTime = new DateTimeOffset(new DateTime(1999, 1, 1)),
+            };
+
+            // act
+            var errors = await validator.Validate(model, campaign);
+
+            // assert
+            Assert.True(errors.Exists(x => x.Key.Equals("StartDateTime")));
+        }
+
+        [Fact]
+        public async Task ReportErrorsWhenPostalCodeInvalid()
+        {
+            // arrage
+            var mediator = new Mock<IMediator>();
+            var validator = new ActivityDetailModelValidator(mediator.Object);
+            var campaign = new CampaignSummaryModel();
+            var model = new ActivityDetailModel
+            {
+                Location = new LocationEditModel()
+                {
+                    PostalCode = "12345",
+                    State = "WA",
+                    City = "Seattle"
+                }
+            };
+
+            // act
+            var errors = await validator.Validate(model, campaign);
+
+            // assert
+            Assert.True(errors.Exists(x => x.Key.Equals("PostalCode")));
+        }
+    }
+}
---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Validators/LocationEditModelValidatorShould.cs
@@ -0,0 +1,32 @@
+﻿using AllReady.Areas.Admin.Models;
+using AllReady.Areas.Admin.Models.Validators;
+using MediatR;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AllReady.UnitTest.Validators
+{
+    public class LocationEditModelValidatorShould
+    {
+        [Fact]
+        public async Task ReportErrorsWhenPostalCodeInvalid()
+        {
+            // arrage
+            var mediator = new Mock<IMediator>();
+            var validator = new LocationEditModelValidator(mediator.Object);
+            var model = new LocationEditModel()
+            {
+                PostalCode = "12345",
+                State = "WA",
+                City = "Seattle"
+            };
+
+            // act
+            var errors = await validator.Validate(model);
+
+            // assert
+            Assert.True(errors.ContainsKey("PostalCode"));
+        }
+    }
+}
