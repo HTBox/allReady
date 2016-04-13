8943c96e135f777179a927cb8534ddb1c0101987
Darren Cauthon <darren@cauthon.com>
2016-04-13 10:01:37 -0500

    Revert back to the original code so we can verify the tests run against it.

--- AllReadyApp/Web-App/AllReady/Controllers/AccountController.cs
+++ AllReadyApp/Web-App/AllReady/Controllers/AccountController.cs
@@ -114,54 +114,42 @@ namespace AllReady.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            if (!ModelState.IsValid) return View(model);
-
-            var user = BuildANewUser(model);
-            var creationResult = await AttemptToCreateTheUser(model, user);
-
-            if (!creationResult.Succeeded)
+            if (ModelState.IsValid)
             {
-                AddErrorsToModelState(creationResult);
-                return View(model);
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    TimeZoneId = _generalSettings.Value.DefaultTimeZone
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // Send an email with this link
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                    var callbackUrl = Url.Action(new UrlActionContext
+                    {
+                        Action = nameof(ConfirmEmail),
+                        Controller = "Account",
+                        Values = new { userId = user.Id, token = token },
+                        Protocol = HttpContext.Request.Scheme
+                    });
+
+                    await _emailSender.SendEmailAsync(model.Email, "Confirm your allReady account",
+                        $"Please confirm your allReady account by clicking this link: <a href=\"{callbackUrl}\">link</a>");
+                    await _userManager.AddClaimAsync(user, new Claim(Security.ClaimTypes.ProfileIncomplete, "NewUser"));
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+
+                AddErrorsToModelState(result);
             }
 
-            await SendAConfirmationEmail(model, user);
-            await _userManager.AddClaimAsync(user, new Claim(Security.ClaimTypes.ProfileIncomplete, "NewUser"));
-            await _signInManager.SignInAsync(user, isPersistent: false);
-
-            return RedirectToAction(nameof(HomeController.Index), "Home");
-        }
-
-        private Task<IdentityResult> AttemptToCreateTheUser(RegisterViewModel model, ApplicationUser user)
-        {
-            return _userManager.CreateAsync(user, model.Password);
-        }
-
-        private ApplicationUser BuildANewUser(RegisterViewModel model)
-        {
-            var user = new ApplicationUser
-            {
-                UserName = model.Email,
-                Email = model.Email,
-                TimeZoneId = _generalSettings.Value.DefaultTimeZone
-            };
-            return user;
-        }
-
-        private async Task SendAConfirmationEmail(RegisterViewModel model, ApplicationUser user)
-        {
-            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-
-            var callbackUrl = Url.Action(new UrlActionContext
-            {
-                Action = nameof(ConfirmEmail),
-                Controller = "Account",
-                Values = new { userId = user.Id, token = token },
-                Protocol = HttpContext.Request.Scheme
-            });
-
-            await _emailSender.SendEmailAsync(model.Email, "Confirm your allReady account",
-                $"Please confirm your allReady account by clicking this link: <a href=\"{callbackUrl}\">link</a>");
+            // If we got this far, something failed, redisplay form
+            return View(model);
         }
 
         //
