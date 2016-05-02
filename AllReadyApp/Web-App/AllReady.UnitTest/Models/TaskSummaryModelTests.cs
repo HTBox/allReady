052eea1b92e08c4f31666fdb09d0adfe6dc808da
Steve Gordon <sgordon@hotmail.co.uk>
2016-05-02 12:10:47 +0100

    Fixes #733
    Adding required attribute to TaskSummaryModel and a test to ensure that a missing name results in expected validation error.

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Models/TaskSummaryModelTests.cs
@@ -0,0 +1,30 @@
+﻿using AllReady.Areas.Admin.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Xunit;
+
+namespace AllReady.UnitTest.ViewModels
+{
+    public class TaskSummaryModelTests
+    {
+        [Fact]
+        public void ValidationShouldFail_IfNoNameIsSupplied()
+        {
+            var model = new TaskSummaryModel();
+            var result = ValidateModel(model);
+
+            var nameError = result.Where(r => r.ErrorMessage == "The Name field is required.").FirstOrDefault();
+
+            Assert.NotNull(nameError);
+        }
+
+        private IList<ValidationResult> ValidateModel(object model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, ctx, validationResults, true);
+            return validationResults;
+        }
+    }
+}
