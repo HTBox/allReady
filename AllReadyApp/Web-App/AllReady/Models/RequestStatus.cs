c2b575cbfe5248d1799764367cbc429cbf96aa9c
James Chambers <james@jameschambers.com>
2016-06-04 16:13:18 -0500

    adding types

---
+++ AllReadyApp/Web-App/AllReady/Models/RequestStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.Models
+{
+    public enum RequestStatus
+    {
+        New,
+        Assigned,
+        Completed,
+        Canceled
+    }
+}
