982542108e886d2ab194c438cef65bb427ab0591
James Chambers <james@jameschambers.com>
2016-06-05 21:03:12 -0500

    adding new controller and related test

---
+++ AllReadyApp/Web-App/AllReady/Controllers/TokenApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.Controllers
+{
+    [Route("api/token")]
+    public class TokenApiController : Controller
+    {
+
+    }
+}
