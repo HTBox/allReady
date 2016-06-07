0c0a2212d38d6b23df03c209295613d9f9c5d992
Steve Gordon <sgordon@hotmail.co.uk>
2016-06-07 21:26:59 +0100

    Adding Itineray controller and initial itinerary details page.
    Link to itinerary details included in the itinerary list on the event details page.

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Controllers/ItineraryController.cs
@@ -0,0 +1,46 @@
+﻿using AllReady.Areas.Admin.Features.Itineraries;
+using AllReady.Security;
+using MediatR;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace AllReady.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize("OrgAdmin")]
+    public class ItineraryController : Controller
+    {
+        private readonly IMediator _mediator;       
+
+        public ItineraryController(IMediator mediator)
+        {            
+            if (mediator == null)
+            {
+                throw new ArgumentNullException(nameof(mediator));
+            }
+
+            _mediator = mediator;            
+        }
+
+        [HttpGet]
+        [Route("Admin/Itinerary/Details/{id}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            var Itinerary = await _mediator.SendAsync(new ItineraryDetailQuery { ItineraryId = id });
+
+            if (Itinerary == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (!User.IsOrganizationAdmin(Itinerary.OrganizationId))
+            {
+                return HttpUnauthorized();
+            }
+
+            return View("Details", Itinerary);
+        }
+    }
+}
---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Itineraries/ItineraryDetailQueryHandlerAsync.cs
@@ -0,0 +1,49 @@
+﻿using AllReady.Areas.Admin.Models.ItineraryModels;
+using AllReady.Models;
+using MediatR;
+using Microsoft.Data.Entity;
+using System.Threading.Tasks;
+
+namespace AllReady.Areas.Admin.Features.Itineraries
+{
+    public class ItineraryDetailQueryHandlerAsync : IAsyncRequestHandler<ItineraryDetailQuery, ItineraryDetailsModel>
+    {
+        private readonly AllReadyContext _context;
+
+        public ItineraryDetailQueryHandlerAsync(AllReadyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ItineraryDetailsModel> Handle(ItineraryDetailQuery message)
+        {
+            ItineraryDetailsModel result = null;
+
+            var itinerary = await GetItinerary(message);
+
+            if (itinerary != null)
+            {
+                result = new ItineraryDetailsModel();
+
+                result.Id = itinerary.Id;
+                result.Name = itinerary.Name;
+                result.Date = itinerary.Date;
+                result.EventId = itinerary.EventId;
+                result.EventName = itinerary.Event.Name;
+                result.OrganizationId = itinerary.Event.Campaign.ManagingOrganization.Id;
+            }
+
+            return result;
+        }
+
+        private async Task<Itinerary> GetItinerary(ItineraryDetailQuery message)
+        {
+            return await _context.Itineraries
+                .AsNoTracking()    
+                .Include(x => x.Event)           
+                .Include(x => x.Event.Campaign.ManagingOrganization)
+                .SingleOrDefaultAsync(a => a.Id == message.ItineraryId)
+                .ConfigureAwait(false);
+        }
+    }
+}
