6ba26946b9df819cb03ad05f0c367f54ff75a10b
James Chambers <james@jameschambers.com>
2016-06-09 15:47:47 -0500

    simple comment

--- AllReadyApp/Web-App/AllReady/Controllers/TokenApiController.cs
+++ AllReadyApp/Web-App/AllReady/Controllers/TokenApiController.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace AllReady.Controllers
 {
+    /// <summary>
+    /// The token controller for the API is to allow third-party services
+    /// with the proper claim to generate transient tokens on-demand to 
+    /// access other services within AllReady.
+    /// </summary>
+
     [Route("api/token")]
     public class TokenApiController : Controller
     {
