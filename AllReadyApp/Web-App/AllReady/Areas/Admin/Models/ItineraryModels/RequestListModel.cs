6d83e6a791a44e520202ae50bc0eb63448fa1466
Steve Gordon <sgordon@hotmail.co.uk>
2016-06-13 01:53:42 +0100

    Adding assigned requests on itinerary details page (#850)

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Models/ItineraryModels/RequestListModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AllReady.Areas.Admin.Models.ItineraryModels
+{
+    public class RequestListModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Status { get; set; }
+    }
+}
