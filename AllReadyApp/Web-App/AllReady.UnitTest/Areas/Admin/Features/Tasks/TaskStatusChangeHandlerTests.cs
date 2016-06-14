f160a7e437eea4a03f6ff774dbe6b1ff67671a56
Ryan McCafferty <ryano.mcc13+devtools@gmail.com>
2016-06-14 12:38:25 -0400

    Finish test coverage for TaskStatusChangeHandlerAsync (#855)
    * #787 TaskStatusChangeHandlerAsync base behavior tests
    Added unit tests for testing the basic behavior of the TaskStatusChangeHandlerAsync request handler.
    - Verify Assigned status change
    - Verify change from Assigned to Accepted
    - Verify change from Assigned to Rejected
    - Verify change from Accepted to Completed
    - Verify change from Accepted to CanNotComplete
    
    * Refactored tests
    
    Refactored unit tests to test all TaskSignup properties that get changed
    when changing the signup status. I changed the 'VolunteerAssignedTask' and
    'VolunteerAcceptsTask' tests.
    
    * Updated remaining tests
    
    Updated remaining tests based on the comments in
    PR #836
    
    * test transition task status from 'CanNotComplete' to 'Accepted'
    
    Added a unit test for testing the task status transition from the
    'CanNotComplete' state to the 'Accepted' state as part of the Tests for Success
    task list on #787
    
    * Finished tests for success checklist
    
    Finished the 'Tests for Success' checklist on #787
    
    * Finished tests for failure checklist
    
    As part of issue #787, finished the tests for failure
    checklist.

--- AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Tasks/TaskStatusChangeHandlerTests.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Tasks/TaskStatusChangeHandlerTests.cs
@@ -93,6 +93,27 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Tasks
             context.SaveChanges();
         }
 
+        protected async Task InitStatus(TaskStatus status)
+        {
+            var taskSignup = Context.TaskSignups.First();
+            taskSignup.Status = status.ToString();
+            await Context.SaveChangesAsync();
+        }
+
+        protected TaskStatusChangeCommandAsync CreateCommand(TaskStatus status, string description = "")
+        {
+            var user = Context.Users.First();
+            var task = Context.Tasks.First();
+
+            return new TaskStatusChangeCommandAsync
+            {
+                UserId = user.Id,
+                TaskId = task.Id,
+                TaskStatus = status,
+                TaskStatusDescription = description ?? string.Empty
+            };
+        }
+
         [Fact]
         public async Task VolunteerAssignedTask()
         {
@@ -208,6 +229,22 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Tasks
         }
 
         [Fact]
+        public async Task VolunteerAcceptsTaskFromAcceptedStatusShouldThrow()
+        {
+            await InitStatus(TaskStatus.Accepted);
+            var command = CreateCommand(TaskStatus.Accepted, "User accepted task");
+            Should.Throw<Exception>(() => handler.Handle(command));
+        }
+
+        [Fact]
+        public async Task VolunteerAcceptsTaskFromRejectedStatusShouldThrow()
+        {
+            await InitStatus(TaskStatus.Rejected);
+            var command = CreateCommand(TaskStatus.Accepted, "User accepted task");
+            Should.Throw<Exception>(() => handler.Handle(command));
+        }
+
+        [Fact]
         public async Task VolunteerRejectsTask()
         {
             var dateTime = DateTime.UtcNow;
@@ -230,6 +267,38 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Tasks
             taskSignup.User.Id.ShouldBe(command.UserId);
             taskSignup.StatusDescription.ShouldBe(command.TaskStatusDescription);
             taskSignup.StatusDateTimeUtc.ShouldBe(dateTime, TimeSpan.FromSeconds(1));
+        }
+
+        [Fact]
+        public async Task VolunteerRejectsTaskFromAcceptedStatusShouldThrow()
+        {
+            await InitStatus(TaskStatus.Accepted);
+            var command = CreateCommand(TaskStatus.Rejected, "User rejects task");
+            Should.Throw<Exception>(() => handler.Handle(command));
+        }
+
+        [Fact]
+        public async Task VolunteerRejectsTaskFromRejectedStatusShouldThrow()
+        {
+            await InitStatus(TaskStatus.Rejected);
+            var command = CreateCommand(TaskStatus.Rejected, "User rejects task");
+            Should.Throw<Exception>(() => handler.Handle(command));
+        }
+
+        [Fact]
+        public async Task VolunteerRejectsTaskFromCompletedStatusShouldThrow()
+        {
+            await InitStatus(TaskStatus.Completed);
+            var command = CreateCommand(TaskStatus.Rejected, "User rejected task");
+            Should.Throw<Exception>(() => handler.Handle(command));
+        }
+
+        [Fact]
+        public async Task VolunteerRejectsTaskFromCanNotCompleteStatusShouldThrow()
+        {
+            await InitStatus(TaskStatus.CanNotComplete);
+            var command = CreateCommand(TaskStatus.CanNotComplete, "User rejected task");
+            Should.Throw<Exception>(() => handler.Handle(command));
         }
 
         [Fact]
@@ -289,6 +358,30 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Tasks
         }
 
         [Fact]
+        public async Task VolunteerCompletesTaskFromRejectedStatusShouldThrow()
+        {
+            await InitStatus(TaskStatus.Rejected);
+            var command = CreateCommand(TaskStatus.Completed, "User completed task");
+            Should.Throw<Exception>(() => handler.Handle(command));
+        }
+
+        [Fact]
+        public async Task VolunteerCompletesTaskFromCompletedStatusShouldThrow()
+        {
+            await InitStatus(TaskStatus.Completed);
+            var command = CreateCommand(TaskStatus.Completed, "User completed task");
+            Should.Throw<Exception>(() => handler.Handle(command));
+        }
+
+        [Fact]
+        public async Task VolunteerCompletesTaskFromCanNotCompleteStatusShouldThrow()
+        {
+            await InitStatus(TaskStatus.CanNotComplete);
+            var command = CreateCommand(TaskStatus.Completed, "User completed task");
+            Should.Throw<Exception>(() => handler.Handle(command));
+        }
+
+        [Fact]
         public async Task VolunteerCannotCompleteTaskFromAcceptedStatus()
         {
             var dateTime = DateTime.UtcNow;
@@ -341,6 +434,46 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Tasks
             taskSignup.Task.Id.ShouldBe(command.TaskId);
             taskSignup.StatusDescription.ShouldBe(command.TaskStatusDescription);
             taskSignup.StatusDateTimeUtc.ShouldBe(dateTime, TimeSpan.FromSeconds(1));
+        }
+
+        [Fact]
+        public async Task VolunteerCannotCompleteTaskFromRejectedStatusShouldThrow()
+        {
+            await InitStatus(TaskStatus.Rejected);
+            var command = CreateCommand(TaskStatus.CanNotComplete, "User cannot complete task");
+            Should.Throw<Exception>(() => handler.Handle(command));
+        }
+
+        [Fact]
+        public async Task VolunteerCannotCompleteTaskFromCompletedStatusShouldThrow()
+        {
+            await InitStatus(TaskStatus.Completed);
+            var command = CreateCommand(TaskStatus.CanNotComplete, "User cannot complete task");
+            Should.Throw<Exception>(() => handler.Handle(command));
+        }
+
+        [Fact]
+        public async Task VolunteerCannotCompleteTaskFromCanNotCompleteStatusShouldThrow()
+        {
+            await InitStatus(TaskStatus.CanNotComplete);
+            var command = CreateCommand(TaskStatus.CanNotComplete, "User cannot complete task");
+            Should.Throw<Exception>(() => handler.Handle(command));
+        }
+
+        [Fact]
+        public void ShouldThrowIfTaskDoesNotExist()
+        {
+            var command = CreateCommand(TaskStatus.Accepted, "User accepted task");
+            command.TaskId = 90124;
+            Should.Throw<Exception>(() => handler.Handle(command));
+        }
+
+        [Fact]
+        public void ShouldThrowIfTaskSignupDoesNotExist()
+        {
+            var command = CreateCommand(TaskStatus.Completed, "User completed task");
+            command.UserId = Guid.NewGuid().ToString();
+            Should.Throw<Exception>(() => handler.Handle(command));
         }
     }
 }
