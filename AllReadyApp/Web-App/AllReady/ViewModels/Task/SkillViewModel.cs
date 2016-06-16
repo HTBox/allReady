ce301a935e83264e566f6a66f5300b924e4d8c93
David Paquette <david.paquette@usask.ca>
2016-06-16 15:30:49 -0600

    Added missing Id property to SkillViewModel class (#885)
    Fixes bug #884

--- AllReadyApp/Web-App/AllReady/ViewModels/Task/SkillViewModel.cs
+++ AllReadyApp/Web-App/AllReady/ViewModels/Task/SkillViewModel.cs
@@ -8,11 +8,13 @@ namespace AllReady.ViewModels.Task
 
         public SkillViewModel(Skill skill)
         {
+            this.Id = skill.Id;
             this.Name = skill.Name;
             this.Description = skill.Description;
             this.HierarchicalName = skill.HierarchicalName;
         }
 
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string HierarchicalName { get; set; }
