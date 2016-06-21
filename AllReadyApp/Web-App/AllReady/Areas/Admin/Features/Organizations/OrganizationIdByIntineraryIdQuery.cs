0a27c1157b602ade753390f6fa8bc4aa1e2a1b89
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-06-21 07:35:50 -0400

    reverting renaming of message/handler back to the original name

--- AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Organizations/OrganizationIdByIntineraryIdQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace AllReady.Areas.Admin.Features.Organizations
-{
-    public class OrganizationIdByIntineraryIdQuery : IAsyncRequest<int>
-    {
-        public int ItineraryId { get; set; }
-    }
-}
