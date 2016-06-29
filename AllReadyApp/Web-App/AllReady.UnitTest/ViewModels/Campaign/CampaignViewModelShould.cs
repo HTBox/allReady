512bcb9332e8e3d2ad71e3b053c5d4192a28167a
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-06-29 15:43:52 -0400

    - arrangin folders in AllReady.UnitTest\ViewModels to match the folder structure of the view models the tests are testing

--- AllReadyApp/Web-App/AllReady.UnitTest/ViewModels/Campaign/CampaignViewModelShould.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/ViewModels/Campaign/CampaignViewModelShould.cs
@@ -5,7 +5,7 @@ using AllReady.ViewModels.Campaign;
 using AllReady.ViewModels.Event;
 using Xunit;
 
-namespace AllReady.UnitTest.ViewModels
+namespace AllReady.UnitTest.ViewModels.Campaign
 {
     public class CampaignViewModelShould
     {
@@ -26,14 +26,14 @@ namespace AllReady.UnitTest.ViewModels
         [Fact]
         public void SetManagingOrganizationNameToEmptyString_WhenCampaignsManagingOrganizationIsNull()
         {
-            var sut = new CampaignViewModel(new Campaign());
+            var sut = new CampaignViewModel(new Models.Campaign());
             Assert.Equal(sut.ManagingOrganizationName, string.Empty);
         }
 
         [Fact]
         public void SetManagingOrganizationNameToEmptyString_WhenCampaignsManagingOrganizationIsNotNull_AndNameIsAnEmptyString()
         {
-            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = new Organization()});
+            var sut = new CampaignViewModel(new Models.Campaign { ManagingOrganization = new Organization()});
             Assert.Equal(sut.ManagingOrganizationName, string.Empty);
         }
 
@@ -41,14 +41,14 @@ namespace AllReady.UnitTest.ViewModels
         public void SetManagingOrganizationNameToCampaignsOrganizationsName_WhenCampaignsManagingOrganizationIsNotNull_AndNameIsNotAnEmptyString()
         {
             var organization = new Organization { Name = "organizationNamee" };
-            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = organization });
+            var sut = new CampaignViewModel(new Models.Campaign { ManagingOrganization = organization });
             Assert.Equal(sut.ManagingOrganizationName, organization.Name);
         }
 
         [Fact]
         public void SetManagingOrganizationIdToZero_WhenCampaignsManagingOrganizationIsNotNull_AndIdIsZero()
         {
-            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = new Organization() });
+            var sut = new CampaignViewModel(new Models.Campaign { ManagingOrganization = new Organization() });
             Assert.Equal(sut.ManagingOrganizationId, 0);
         }
 
@@ -56,28 +56,28 @@ namespace AllReady.UnitTest.ViewModels
         public void SetManagingOrganizationIdToCampaignsManagingOrganizationId_WhenCampaignsManagingOrganizationIsNotNull_AndIdIsNotZero()
         {
             var organization = new Organization { Id = 1 };
-            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = organization });
+            var sut = new CampaignViewModel(new Models.Campaign { ManagingOrganization = organization });
             Assert.Equal(sut.ManagingOrganizationId, organization.Id);
         }
 
         [Fact]
         public void SetManagingOrganizationIdToZero_WhenCampaignsManagingOrganizationIsNull()
         {
-            var sut = new CampaignViewModel(new Campaign());
+            var sut = new CampaignViewModel(new Models.Campaign());
             Assert.Equal(sut.ManagingOrganizationId, 0);
         }
 
         [Fact]
         public void SetManagingOrganizationLogoToEmptyString_WhenCampaignsManagingOrganizationIsNull()
         {
-            var sut = new CampaignViewModel(new Campaign());
+            var sut = new CampaignViewModel(new Models.Campaign());
             Assert.Equal(sut.ManagingOrganizationLogo, string.Empty);
         }
 
         [Fact]
         public void SetManagingOrganizationLogoToEmptyString_WhenCampaignsManagingOrganizationIsNotNull_AndLogoUrlIsAnEmptyString()
         {
-            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = new Organization() });
+            var sut = new CampaignViewModel(new Models.Campaign { ManagingOrganization = new Organization() });
             Assert.Equal(sut.ManagingOrganizationLogo, string.Empty);
         }
 
@@ -85,21 +85,21 @@ namespace AllReady.UnitTest.ViewModels
         public void SetManagingOrganizationLogoToCampaignsOrganizationsName_WhenCampaignsManagingOrganizationIsNotNull_AndLogoUrlIsNotAnEmptyString()
         {
             var organization = new Organization { LogoUrl = "logoUrl" };
-            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = organization });
+            var sut = new CampaignViewModel(new Models.Campaign { ManagingOrganization = organization });
             Assert.Equal(sut.ManagingOrganizationLogo, organization.LogoUrl);
         }
 
         [Fact]
         public void SetEventsToEmptyListOfEventViewModel_WhenCampaignsEventsIsNull()
         {
-            var sut = new CampaignViewModel(new Campaign());
+            var sut = new CampaignViewModel(new Models.Campaign());
             Assert.Empty(sut.Events);
         }
 
         [Fact]
         public void SetEventsToCampaignsEventsProjectedToEventViewModel_WhenCampaignsEventsIsNotNull()
         {
-            var campaign = new Campaign { Events = new List<Event> { new Event { Id = 1} }};
+            var campaign = new Models.Campaign { Events = new List<Event> { new Event { Id = 1} }};
             var eventViewModels = campaign.Events.ToViewModel();
             var sut = new CampaignViewModel(campaign);
             Assert.Equal(sut.Events.First().Id, eventViewModels.First().Id);
@@ -108,28 +108,28 @@ namespace AllReady.UnitTest.ViewModels
         [Fact]
         public void SetHasPrivacyPolicyToFalse_WhenCampaignsManagingOrganizationIsNull()
         {
-            var sut = new CampaignViewModel(new Campaign());
+            var sut = new CampaignViewModel(new Models.Campaign());
             Assert.False(sut.HasPrivacyPolicy);
         }
 
         [Fact]
         public void SetHasPrivacyPolicyToFalse_WhenCampaignsManagingOrganizationIsNotNull_AndPrivacyPolicyIsNullOrEmpty()
         {
-            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = new Organization()});
+            var sut = new CampaignViewModel(new Models.Campaign { ManagingOrganization = new Organization()});
             Assert.False(sut.HasPrivacyPolicy);
         }
 
         [Fact]
         public void SetHasPrivacyPolicyToTrue_WhenCampaignsManagingOrganizationIsNotNull_AndPrivacyPolicyIsNotNullOrEmpty()
         {
-            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = new Organization { PrivacyPolicy = "privacyPolicy" }});
+            var sut = new CampaignViewModel(new Models.Campaign { ManagingOrganization = new Organization { PrivacyPolicy = "privacyPolicy" }});
             Assert.True(sut.HasPrivacyPolicy);
         }
 
         [Fact]
         public void SetPrivacyPolicyUrlToNull_WhenCampaignsManagingOrganizationIsNull()
         {
-            var sut = new CampaignViewModel(new Campaign());
+            var sut = new CampaignViewModel(new Models.Campaign());
             Assert.Null(sut.PrivacyPolicyUrl);
         }
 
@@ -137,7 +137,7 @@ namespace AllReady.UnitTest.ViewModels
         public void SetPrivacyPolicyUrlToCampaignsManagingOrganizationsPrivacyPolicyUrl_WhenCampaignsManagingOrganizationIsNotNull()
         {
             var organization = new Organization { PrivacyPolicy = "privacyPolicy" };
-            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = organization });
+            var sut = new CampaignViewModel(new Models.Campaign { ManagingOrganization = organization });
             Assert.Equal(sut.PrivacyPolicyUrl, organization.PrivacyPolicyUrl);
         }
 
