8951269b5b9dbe8f1aea785002594edd13684f9d
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-06-29 08:52:53 -0400

    - adding missing unit tests to CampaignViewModelShould

--- AllReadyApp/Web-App/AllReady.UnitTest/ViewModels/CampaignViewModelShould.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/ViewModels/CampaignViewModelShould.cs
@@ -1,15 +1,132 @@
-﻿using AllReady.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AllReady.ViewModels.Campaign;
+﻿using AllReady.ViewModels.Campaign;
 using Xunit;
 
 namespace AllReady.UnitTest.ViewModels
 {
     public class CampaignViewModelShould
     {
+        [Fact]
+        public void ReturnImmediately_WhenConstructingWithNullCampaign()
+        {
+            var sut = new CampaignViewModel(null);
+            Assert.Equal(sut.Id, 0);
+        }
+
+        [Fact]
+        public void InitializeEventsPropertyToEmptyList_WhenCallingDefaultConstructor()
+        {
+            var sut = new CampaignViewModel();
+            Assert.NotNull(sut.Events);
+        }
+
+        //test happy constructor path
+        [Fact(Skip = "Not Implemented")]
+        public void HavePropertiesPopulatedCorrectly_WhenConstructingWithCampaign()
+        {
+        }
+
+        //test non-happy constructor path
+        [Fact]
+        public void SetManagingOrganizationNameToEmptyString_WhenCampaignsManagingOrganizationIsNull()
+        {
+            var sut = new CampaignViewModel(new Models.Campaign());
+            Assert.Equal(sut.ManagingOrganizationName, string.Empty);
+        }
+
+        [Fact]
+        public void SetManagingOrganizationIdToZero_WhenCampaignsManagingOrganizationIsNull()
+        {
+            var sut = new CampaignViewModel(new Models.Campaign());
+            Assert.Equal(sut.ManagingOrganizationId, 0);
+        }
+
+        [Fact]
+        public void SetManagingOrganizationLogoToEmptyString_WhenCampaignsManagingOrganizationIsNull()
+        {
+            var sut = new CampaignViewModel(new Models.Campaign());
+            Assert.Equal(sut.ManagingOrganizationLogo, string.Empty);
+        }
+
+        [Fact]
+        public void SetEventsToEmptyListOfEventViewModel_WhenCampaignsEventsIsNull()
+        {
+            var sut = new CampaignViewModel(new Models.Campaign());
+            Assert.Empty(sut.Events);
+        }
+
+        [Fact]
+        public void SetHasPrivacyPolicyToFalse_WhenCampaignsManagingOrganizationIsNull()
+        {
+            var sut = new CampaignViewModel(new Models.Campaign());
+            Assert.False(sut.HasPrivacyPolicy);
+        }
+
+        [Fact]
+        public void SetHasPrivacyPolicyToFalse_WhenCampaignsManagingOrganizationIsNotNull_AndPrivacyPolicyIsNullOrEmpty()
+        {
+            var sut = new CampaignViewModel(new Models.Campaign { ManagingOrganization = new Models.Organization()});
+            Assert.False(sut.HasPrivacyPolicy);
+        }
+
+        [Fact]
+        public void SetPrivacyPolicyUrlToNull_WhenCampaignsManagingOrganizationIsNull()
+        {
+            var sut = new CampaignViewModel(new Models.Campaign());
+            Assert.Null(sut.PrivacyPolicyUrl);
+        }
+
+        [Fact]
+        public void SetLocationSummaryToCorrectValue_WhenLocationIsNotNull_AndLocationsCityAndStateAreNotNullOrEmpty()
+        {
+            var location = new Models.Location { City = "city", State = "state" };
+            var sut = new CampaignViewModel { Location = location };
+            Assert.Equal(sut.LocationSummary, $"{location.City}, {location.State}");
+        }
+
+        [Fact]
+        public void SetLocationSummaryToCorrectValue_WhenLocationIsNotNull_AndLocationsCityIsNotNullOrEmpty()
+        {
+            var location = new Models.Location { City = "city" };
+            var sut = new CampaignViewModel { Location = location };
+            Assert.Equal(sut.LocationSummary, location.City);
+        }
+
+        [Fact]
+        public void SetLocationSummaryToCorrectValue_WhenLocationIsNotNull_AndLocationsStateIsNotNullOrEmpty()
+        {
+            var location = new Models.Location { State = "state" };
+            var sut = new CampaignViewModel { Location = location };
+            Assert.Equal(sut.LocationSummary, location.State);
+        }
+
+        [Fact]
+        public void SetHasPrivacyPolicyUrlToTrue_WhenPrivacyPolicyUrlIsNotNullOrEmpty()
+        {
+            var sut = new CampaignViewModel { PrivacyPolicyUrl = "privacyPolicyUrl" };
+            Assert.True(sut.HasPrivacyPolicyUrl);
+        }
+
+        [Fact]
+        public void SetHasPrivacyPolicyUrlToFalse_WhenPrivacyPolicyUrlIsNullOrEmpty()
+        {
+            var sut = new CampaignViewModel();
+            Assert.False(sut.HasPrivacyPolicyUrl);
+        }
+
+        [Fact]
+        public void SetHasHeadlineToTrue_WhenHeadlineIsNotNullOrEmpty()
+        {
+            var sut = new CampaignViewModel { Headline = "headline" };
+            Assert.True(sut.HasHeadline);
+        }
+
+        [Fact]
+        public void SetHasHeadlineToFalse_WhenHeadlineIsNullOrEmpty()
+        {
+            var sut = new CampaignViewModel();
+            Assert.False(sut.HasHeadline);
+        }
+
         [Fact]
         public void ReturnCityAndStateWhenBothArePresent()
         {
@@ -42,7 +159,6 @@ namespace AllReady.UnitTest.ViewModels
 
             // assert
             Assert.Equal("HappyTown", model.LocationSummary);
-
         }
 
         [Fact]
@@ -59,7 +175,6 @@ namespace AllReady.UnitTest.ViewModels
 
             // assert
             Assert.Equal("Utopia", model.LocationSummary);
-
         }
     }
 }
