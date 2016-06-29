31601d877dec748cf7233d695e831628564a0b22
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-06-29 10:59:26 -0400

    - finalizing unit tests for CampaignViewModel

--- AllReadyApp/Web-App/AllReady.UnitTest/ViewModels/CampaignViewModelShould.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/ViewModels/CampaignViewModelShould.cs
@@ -1,4 +1,8 @@
-﻿using AllReady.ViewModels.Campaign;
+﻿using System.Collections.Generic;
+using System.Linq;
+using AllReady.Models;
+using AllReady.ViewModels.Campaign;
+using AllReady.ViewModels.Event;
 using Xunit;
 
 namespace AllReady.UnitTest.ViewModels
@@ -19,84 +23,161 @@ namespace AllReady.UnitTest.ViewModels
             Assert.NotNull(sut.Events);
         }
 
-        //test happy constructor path
-        [Fact(Skip = "Not Implemented")]
-        public void HavePropertiesPopulatedCorrectly_WhenConstructingWithCampaign()
-        {
-        }
-
-        //test non-happy constructor path
         [Fact]
         public void SetManagingOrganizationNameToEmptyString_WhenCampaignsManagingOrganizationIsNull()
         {
-            var sut = new CampaignViewModel(new Models.Campaign());
+            var sut = new CampaignViewModel(new Campaign());
             Assert.Equal(sut.ManagingOrganizationName, string.Empty);
+        }
+
+        [Fact]
+        public void SetManagingOrganizationNameToEmptyString_WhenCampaignsManagingOrganizationIsNotNull_AndNameIsAnEmptyString()
+        {
+            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = new Organization()});
+            Assert.Equal(sut.ManagingOrganizationName, string.Empty);
+        }
+
+        [Fact]
+        public void SetManagingOrganizationNameToCampaignsOrganizationsName_WhenCampaignsManagingOrganizationIsNotNull_AndNameIsNotAnEmptyString()
+        {
+            var organization = new Organization { Name = "organizationNamee" };
+            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = organization });
+            Assert.Equal(sut.ManagingOrganizationName, organization.Name);
+        }
+
+        [Fact]
+        public void SetManagingOrganizationIdToZero_WhenCampaignsManagingOrganizationIsNotNull_AndIdIsZero()
+        {
+            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = new Organization() });
+            Assert.Equal(sut.ManagingOrganizationId, 0);
+        }
+
+        [Fact]
+        public void SetManagingOrganizationIdToCampaignsManagingOrganizationId_WhenCampaignsManagingOrganizationIsNotNull_AndIdIsNotZero()
+        {
+            var organization = new Organization { Id = 1 };
+            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = organization });
+            Assert.Equal(sut.ManagingOrganizationId, organization.Id);
         }
 
         [Fact]
         public void SetManagingOrganizationIdToZero_WhenCampaignsManagingOrganizationIsNull()
         {
-            var sut = new CampaignViewModel(new Models.Campaign());
+            var sut = new CampaignViewModel(new Campaign());
             Assert.Equal(sut.ManagingOrganizationId, 0);
         }
 
         [Fact]
         public void SetManagingOrganizationLogoToEmptyString_WhenCampaignsManagingOrganizationIsNull()
         {
-            var sut = new CampaignViewModel(new Models.Campaign());
+            var sut = new CampaignViewModel(new Campaign());
             Assert.Equal(sut.ManagingOrganizationLogo, string.Empty);
+        }
+
+        [Fact]
+        public void SetManagingOrganizationLogoToEmptyString_WhenCampaignsManagingOrganizationIsNotNull_AndLogoUrlIsAnEmptyString()
+        {
+            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = new Organization() });
+            Assert.Equal(sut.ManagingOrganizationLogo, string.Empty);
+        }
+
+        [Fact]
+        public void SetManagingOrganizationLogoToCampaignsOrganizationsName_WhenCampaignsManagingOrganizationIsNotNull_AndLogoUrlIsNotAnEmptyString()
+        {
+            var organization = new Organization { LogoUrl = "logoUrl" };
+            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = organization });
+            Assert.Equal(sut.ManagingOrganizationLogo, organization.LogoUrl);
         }
 
         [Fact]
         public void SetEventsToEmptyListOfEventViewModel_WhenCampaignsEventsIsNull()
         {
-            var sut = new CampaignViewModel(new Models.Campaign());
+            var sut = new CampaignViewModel(new Campaign());
             Assert.Empty(sut.Events);
+        }
+
+        [Fact]
+        public void SetEventsToCampaignsEventsProjectedToEventViewModel_WhenCampaignsEventsIsNotNull()
+        {
+            var campaign = new Campaign { Events = new List<Event> { new Event { Id = 1} }};
+            var eventViewModels = campaign.Events.ToViewModel();
+            var sut = new CampaignViewModel(campaign);
+            Assert.Equal(sut.Events.First().Id, eventViewModels.First().Id);
         }
 
         [Fact]
         public void SetHasPrivacyPolicyToFalse_WhenCampaignsManagingOrganizationIsNull()
         {
-            var sut = new CampaignViewModel(new Models.Campaign());
+            var sut = new CampaignViewModel(new Campaign());
             Assert.False(sut.HasPrivacyPolicy);
         }
 
         [Fact]
         public void SetHasPrivacyPolicyToFalse_WhenCampaignsManagingOrganizationIsNotNull_AndPrivacyPolicyIsNullOrEmpty()
         {
-            var sut = new CampaignViewModel(new Models.Campaign { ManagingOrganization = new Models.Organization()});
+            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = new Organization()});
             Assert.False(sut.HasPrivacyPolicy);
+        }
+
+        [Fact]
+        public void SetHasPrivacyPolicyToTrue_WhenCampaignsManagingOrganizationIsNotNull_AndPrivacyPolicyIsNotNullOrEmpty()
+        {
+            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = new Organization { PrivacyPolicy = "privacyPolicy" }});
+            Assert.True(sut.HasPrivacyPolicy);
         }
 
         [Fact]
         public void SetPrivacyPolicyUrlToNull_WhenCampaignsManagingOrganizationIsNull()
         {
-            var sut = new CampaignViewModel(new Models.Campaign());
+            var sut = new CampaignViewModel(new Campaign());
             Assert.Null(sut.PrivacyPolicyUrl);
         }
 
         [Fact]
-        public void SetLocationSummaryToCorrectValue_WhenLocationIsNotNull_AndLocationsCityAndStateAreNotNullOrEmpty()
+        public void SetPrivacyPolicyUrlToCampaignsManagingOrganizationsPrivacyPolicyUrl_WhenCampaignsManagingOrganizationIsNotNull()
         {
-            var location = new Models.Location { City = "city", State = "state" };
-            var sut = new CampaignViewModel { Location = location };
-            Assert.Equal(sut.LocationSummary, $"{location.City}, {location.State}");
+            var organization = new Organization { PrivacyPolicy = "privacyPolicy" };
+            var sut = new CampaignViewModel(new Campaign { ManagingOrganization = organization });
+            Assert.Equal(sut.PrivacyPolicyUrl, organization.PrivacyPolicyUrl);
         }
 
         [Fact]
-        public void SetLocationSummaryToCorrectValue_WhenLocationIsNotNull_AndLocationsCityIsNotNullOrEmpty()
+        public void ReturnCityAndStateWhenBothArePresent()
         {
-            var location = new Models.Location { City = "city" };
-            var sut = new CampaignViewModel { Location = location };
-            Assert.Equal(sut.LocationSummary, location.City);
+            // arrange
+            var model = new CampaignViewModel { Location = new Location { City = "HappyTown", State = "Utopia" }};
+            
+            //act 
+            var result = model.LocationSummary;
+
+            //aswert
+            Assert.Equal("HappyTown, Utopia", result);
         }
 
         [Fact]
-        public void SetLocationSummaryToCorrectValue_WhenLocationIsNotNull_AndLocationsStateIsNotNullOrEmpty()
+        public void ReturnCityWhenStateNotPresent()
         {
-            var location = new Models.Location { State = "state" };
-            var sut = new CampaignViewModel { Location = location };
-            Assert.Equal(sut.LocationSummary, location.State);
+            // arrange
+            var model = new CampaignViewModel { Location = new Location { City = "HappyTown" }};
+
+            // act
+            var result = model.LocationSummary;
+
+            // assert
+            Assert.Equal("HappyTown", result);
+        }
+
+        [Fact]
+        public void ReturnStateWhenCityNotPresent()
+        {
+            // arrange
+            var model = new CampaignViewModel { Location = new Location { State = "Utopia" }};
+
+            // act
+            var result = model.LocationSummary;
+
+            // assert
+            Assert.Equal("Utopia", result);
         }
 
         [Fact]
@@ -125,56 +206,6 @@ namespace AllReady.UnitTest.ViewModels
         {
             var sut = new CampaignViewModel();
             Assert.False(sut.HasHeadline);
-        }
-
-        [Fact]
-        public void ReturnCityAndStateWhenBothArePresent()
-        {
-            // arrange
-            var model = new CampaignViewModel();
-
-            // act
-            model.Location = new Models.Location
-            {
-                City = "HappyTown",
-                State = "Utopia"
-            };
-
-            // assert
-            Assert.Equal("HappyTown, Utopia", model.LocationSummary);
-
-        }
-
-        [Fact]
-        public void ReturnCityWhenStateNotPresent()
-        {
-            // arrange
-            var model = new CampaignViewModel();
-
-            // act
-            model.Location = new Models.Location
-            {
-                City = "HappyTown"
-            };
-
-            // assert
-            Assert.Equal("HappyTown", model.LocationSummary);
-        }
-
-        [Fact]
-        public void ReturnStateWhenCityNotPresent()
-        {
-            // arrange
-            var model = new CampaignViewModel();
-
-            // act
-            model.Location = new Models.Location
-            {
-                State = "Utopia"
-            };
-
-            // assert
-            Assert.Equal("Utopia", model.LocationSummary);
         }
     }
 }
