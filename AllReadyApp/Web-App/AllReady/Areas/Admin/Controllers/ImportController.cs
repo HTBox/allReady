f3b4111d58ac520884c0b0c2a81be4f5e19108ba
James Chambers <james@jameschambers.com>
2016-07-03 15:35:28 -0500

    basic mechanics for upload
    fleshing out import bits
    
    mechanics for mediator
    
    call mediator from controller
    
    rounding out feature and view

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Controllers/ImportController.cs
@@ -0,0 +1,67 @@
+﻿using AllReady.Areas.Admin.Features.Requests;
+using AllReady.Areas.Admin.Models.RequestModels;
+using AllReady.Models;
+using CsvHelper;
+using CsvHelper.Configuration;
+using MediatR;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.Areas.Admin.Controllers
+{
+
+
+
+    [Area("Admin")]
+    [Authorize("SiteAdmin")]
+    public class ImportController : Controller
+    {
+        private readonly ILogger<ImportController> _logger;
+        private readonly IMediator _mediator;
+
+        public ImportController(IMediator mediator, ILogger<ImportController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        } 
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormFile file)
+        {
+            using (var stream = file.OpenReadStream())
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var csvReader = new CsvReader(reader);
+                    csvReader.Configuration.WillThrowOnMissingField = false;
+                    csvReader.Configuration.RegisterClassMap<RedCrossRequestMap>();
+                    var requests = csvReader.GetRecords<Request>().ToList();
+
+                    var errors = _mediator.Send(new AddRequestsCommand { Requests = requests });
+
+                }
+            }
+
+            // todo: - add error handling logic/results view
+            //       - proper view model
+            //       - more complete result type/info
+
+            ViewBag.ImportSuccess = true;
+
+            return View();
+        }
+
+    }
+}
---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Requests/AddRequestsCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AllReady.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.Areas.Admin.Features.Requests
+{
+    public class AddRequestsCommandHandler : IRequestHandler<AddRequestsCommand, IEnumerable<RequestImportError>>
+    {
+        private readonly AllReadyContext _context;
+
+        public AddRequestsCommandHandler(AllReadyContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<RequestImportError> Handle(AddRequestsCommand message)
+        {
+            var errors = new List<RequestImportError>();
+
+            foreach (var request in message.Requests)
+            {
+                // todo: do basic data validation
+                _context.Requests.Add(request);
+            }
+            _context.SaveChanges();
+
+            return errors;
+        }
+
+    }
+}
