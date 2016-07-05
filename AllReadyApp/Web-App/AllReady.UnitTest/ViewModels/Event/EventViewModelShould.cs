0e4fa475bfb665c5bc238f144aa775c08902b3cb
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-07-05 15:18:35 -0400

    stubbing out unit test method in EventViewModelShould

--- AllReadyApp/Web-App/AllReady.UnitTest/ViewModels/Event/EventViewModelShould.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/ViewModels/Event/EventViewModelShould.cs
@@ -2,10 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AllReady.ViewModels.Event;
+using Xunit;
 
 namespace AllReady.UnitTest.ViewModels.Event
 {
     public class EventViewModelShould
     {
+        [Fact]
+        public void AssignsCampaignIdCampaignNameAndTimeZoneId_WhenConstructingWithNonNullCampaign()
+        {
+            var campaign = new Models.Campaign { Id = 1, Name = "Campaignname", TimeZoneId = "CampaignTimeZoneId" };
+            var @event = new Models.Event { Campaign = campaign };
+            var sut = new EventViewModel(@event);
+
+            Assert.Equal(sut.CampaignId, campaign.Id);
+            Assert.Equal(sut.CampaignName, campaign.Name);
+            Assert.Equal(sut.TimeZoneId, campaign.TimeZoneId);
+        }
+
+        [Fact]
+        public void AssignOrganizationIdOrganizationNameAndHasPrivacyPolicy_WhenConstructingEventThatHasANonNullCampaignWithANonNullOrganization()
+        {
+        }
+
+        [Fact]
+        public void CreatesNewLocationViewModelAndAssignsToLocation_WhenEventsLocationIsNotNull()
+        {
+        }
+
+        [Fact]
+        public void AssignsIsClosedToTrueWhen_WhenEventsEndDateTimeIsLessThanDateTimeOffsetUtcNow()
+        {
+        }
+
+        [Fact]
+        public void AssignsIsClosedToWhen_WhenEventsEndDateTimeIsGreaterThanDateTimeOffsetUtcNow()
+        {
+        }
+
+        [Fact]
+        public void PopulatesTasksWithListOfTaskViewModelsInDescendingOrderByStartDateTime_WhenEventsTasksAreNotNull()
+        {
+        }
+
+        [Fact]
+        public void PopulatesTasksWithEmptyListOfTaskViewModels_WhenEventsTasksAreNull()
+        {
+        }
+
+        [Fact]
+        public void PopulatesRequiredSkillsWithListOfSkillViewModelsForEventsRequiredSkills_WhenEventsRequiredSkillsIsNotNull()
+        {
+        }
+
+        [Fact]
+        public void AssignsNullToRequiredSkills_WhenEventsRequiredSkillsIsNull()
+        {
+        }
     }
 }
