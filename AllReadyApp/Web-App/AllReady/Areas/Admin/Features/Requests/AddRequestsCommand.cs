4397021839df57b10bf491bab99b9c4b839af1fc
James Chambers <james@jameschambers.com>
2016-07-20 15:36:18 -0500

    Import Red Cross data via CSV (#990)
    * basic mechanics for upload
    
    fleshing out import bits
    
    mechanics for mediator
    
    call mediator from controller
    
    rounding out feature and view
    
    * minor fixes
    
    * adding antiforgerytoken to POST method
    
    * fixing collision with namespaces
    
    * fixing namespaces

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Requests/AddRequestsCommand.cs
@@ -0,0 +1,14 @@
+﻿using AllReady.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.Areas.Admin.Features.Requests
+{
+    public class AddRequestsCommand : IRequest<IEnumerable<RequestImportError>>
+    {
+        public List<Request> Requests { get; set; }
+    }
+}
---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Requests/AddRequestsCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using AllReady.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.Areas.Admin.Features.Requests
+{
+    public class AddRequestsCommandHandler : IRequestHandler<AddRequestsCommand, IEnumerable<RequestImportError>>
+    {
+        private readonly AllReadyContext _context;
+
+        public AddRequestsCommandHandler(AllReadyContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<RequestImportError> Handle(AddRequestsCommand message)
+        {
+            var errors = new List<RequestImportError>();
+
+            foreach (var request in message.Requests)
+            {
+                // todo: do basic data validation
+                if(!_context.Requests.Any(r => r.ProviderId == request.ProviderId))
+                    _context.Requests.Add(request);
+            }
+            _context.SaveChanges();
+
+            return errors;
+        }
+
+    }
+}
