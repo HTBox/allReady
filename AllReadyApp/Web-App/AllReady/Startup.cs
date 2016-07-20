84de593385977809087e7b0e0f741335559b653d
stevejgordon <sgordon@hotmail.co.uk>
2016-07-20 13:27:03 +0100

    Ensuring that seed data campaigns set appropriate dates

--- AllReadyApp/Web-App/AllReady/Startup.cs
+++ AllReadyApp/Web-App/AllReady/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using AllReady.Areas.Admin.Models.Validators;
 using AllReady.Controllers;
+using AllReady.DataAccess;
 using AllReady.Models;
 using AllReady.Security;
 using AllReady.Services;
