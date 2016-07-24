f80185b37b8a9ea234c0534afb6d6580b8cdb904
Benjamin Ettori <Benjamin Ettori>
2016-07-24 10:07:20 -0400

    Get unit tests to pass taking into account thread

--- AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Tasks/TaskLIstQueryHandlerTests.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Tasks/TaskLIstQueryHandlerTests.cs
@@ -2,6 +2,7 @@
 using AllReady.Models;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -9,18 +10,14 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Tasks
 {
     public class TaskListQueryHandlerTests : InMemoryContextTest
     {
-        private readonly TaskListQueryHandler handler;
         private AllReadyTask _task;
-        public TaskListQueryHandlerTests()
-        {
-            handler = new TaskListQueryHandler(Context);
-        }
-
+        private AllReadyContext context;
+        private Random rnd;
         private AllReadyTask GenerateTask()
         {
             return new AllReadyTask
             {
-                Event = new Event(),
+                Id = rnd.Next(3, 1000),
                 Name = "TestTask",
                 StartDateTime = new DateTimeOffset(DateTime.Today),
                 EndDateTime = new DateTimeOffset(DateTime.Today.AddDays(1)),
@@ -30,18 +27,53 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Tasks
 
         protected override void LoadTestData()
         {
-            var context = ServiceProvider.GetService<AllReadyContext>();
+            rnd = new Random();
+            context = ServiceProvider.GetService<AllReadyContext>();
+            var tasks = context.Tasks.ToList();
             _task = GenerateTask();
+
+            var htb = new Organization
+            {
+                Name = "Humanitarian Test",
+                LogoUrl = "http://www.htbox.org/upload/home/ht-hero.png",
+                WebUrl = "http://www.htbox.org",
+                Campaigns = new List<Campaign>()
+            };
+
+            var firePrev = new Campaign
+            {
+                Name = "test Campaign",
+                ManagingOrganization = htb
+            };
+
+            var myEvent = new Event
+            {
+                Id = rnd.Next(3,1000),
+                Campaign = firePrev,
+                CampaignId = firePrev.Id,
+                StartDateTime = new DateTime(2015, 7, 4, 10, 0, 0).ToUniversalTime(),
+                EndDateTime = new DateTime(2015, 12, 31, 15, 0, 0).ToUniversalTime(),
+                Location = new Location { Id = 1 },
+                RequiredSkills = new List<EventSkill>()
+            };
+
+            _task.Event = myEvent;
+            context.Organizations.Add(htb);
+
+            context.Campaigns.Add(firePrev);
+            context.Events.Add(myEvent);
             context.Tasks.Add(_task);
+            tasks = context.Tasks.ToList();
             context.SaveChanges();
         }
 
         [Fact]
         public void TaskListQueryMade()
         {
+            var handler = new TaskListQueryHandler(context);
             var taskListQuery = new TaskListQuery
             {
-                EventId = _task.Id
+                EventId = _task.Event.Id
             };
             var result = handler.Handle(taskListQuery).ToList();
             Assert.NotNull(result);
