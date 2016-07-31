440f2af9fec611fd45f2b1e5ae44a8c4215d20c6
danielepo <dnl.pozzobon@gmail.con>
2016-07-31 23:26:25 +0200

    Add ShowEvent tests

--- AllReadyApp/Web-App/AllReady.UnitTest/Controllers/EventControllerTests.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Controllers/EventControllerTests.cs
@@ -1,11 +1,18 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using AllReady.Controllers;
 using AllReady.Features.Event;
+using AllReady.Models;
 using AllReady.UnitTest.Extensions;
+using AllReady.ViewModels.Event;
 using AllReady.ViewModels.Shared;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
@@ -16,18 +23,15 @@ namespace AllReady.UnitTest.Controllers
     {
         //delete this line when all unit tests using it have been completed
         private readonly Task taskFromResultZero = Task.FromResult(0);
-       
-
-
 
         [Fact]
         public void IndexReturnsTheCorrectView()
         {
             var sut = EventControllerBuilder.Instance().Build();
+
             var result = sut.Index();
+
             Assert.IsType<ViewResult>(result);
-
-
         }
 
         [Fact]
@@ -35,44 +39,86 @@ namespace AllReady.UnitTest.Controllers
         {
             var sut = EventControllerBuilder.Instance().Build();
 
-            var attribute = sut.GetAttributesOn(x => x.Index()).OfType<HttpGetAttribute>().FirstOrDefault();
+            var attribute = sut
+                            .GetAttributesOn(x => x.Index())
+                            .OfType<HttpGetAttribute>().FirstOrDefault();
+
             Assert.NotNull(attribute);
         }
 
         [Fact]
         public void ShowEventSendsShowEventQueryWithCorrectData()
         {
-            
+
         }
 
-        [Fact(Skip = "NotImplemented")]
+        [Fact]
         public void ShowEventReturnsHttpNotFoundResultWhenViewModelIsNull()
         {
+            var builder = EventControllerBuilder.Instance();
+            var sut = builder.WithMediator().Build();
+
+            builder
+                .MediatorMock
+                .Setup(x => x.Send(It.IsAny<ShowEventQuery>()))
+                .Returns(() => null);
+
+            var result = sut.ShowEvent(0);
+
+            Assert.True(result is NotFoundResult);
         }
 
-        [Fact(Skip = "NotImplemented")]
+        [Fact]
         public void ShowEventReturnsEventWithTasksViewWithCorrrectViewModelWhenViewModelIsNotNull()
         {
+            var builder = EventControllerBuilder.Instance();
+            var eventViewModel = new EventViewModel();
+            var sut = builder.WithMediator().Build();
+
+            builder
+                .MediatorMock
+                .Setup(x => x.Send(It.IsAny<ShowEventQuery>()))
+                .Returns(eventViewModel);
+
+            var result = sut.ShowEvent(0) as ViewResult;
+
+            Assert.Equal(eventViewModel, result.Model);
+            Assert.Equal("EventWithTasks", result.ViewName);
         }
 
         [Fact]
         public void ShowEventHasRouteAttributeWithCorrectRoute()
         {
-      
+            var sut = EventControllerBuilder.Instance().Build();
+
+            var attribute = sut
+                            .GetAttributesOn(x => x.ShowEvent(0))
+                            .OfType<RouteAttribute>().FirstOrDefault();
+
+            Assert.NotNull(attribute);
+            Assert.Equal("[controller]/{id}/", attribute.Template);
         }
 
-        [Fact(Skip = "NotImplemented")]
+        [Fact]
         public void ShowEventHasAllowAnonymousAttribute()
         {
+            var sut = EventControllerBuilder.Instance().Build();
+
+            var attribute = sut
+                            .GetAttributesOn(x => x.ShowEvent(0))
+                            .OfType<AllowAnonymousAttribute>().FirstOrDefault();
+
+            Assert.NotNull(attribute);
         }
 
         [Fact]
         public async Task SignupReturnsBadRequestResultWhenViewModelIsNull()
         {
             var sut = EventControllerBuilder.Instance().Build();
-            var result = await sut.Signup(null);
-            Assert.IsType<BadRequestResult>(result);
 
+            var result = await sut.Signup(null);
+
+            Assert.IsType<BadRequestResult>(result);
         }
 
         [Fact]
@@ -80,9 +126,12 @@ namespace AllReady.UnitTest.Controllers
         {
             var builder = EventControllerBuilder.Instance();
             var sut = builder.WithMediator().Build();
+
             await sut.Signup(new EventSignupViewModel());
 
-            builder.MediatorMock.Verify(x => x.SendAsync(It.IsAny<EventSignupCommand>()));
+            builder
+                .MediatorMock
+                .Verify(x => x.SendAsync(It.IsAny<EventSignupCommand>()));
         }
 
         [Fact]
@@ -102,7 +151,10 @@ namespace AllReady.UnitTest.Controllers
         {
             var sut = EventControllerBuilder.Instance().Build();
 
-            var attribute = sut.GetAttributesOn(x => x.Signup(null)).OfType<HttpPostAttribute>().FirstOrDefault();
+            var attribute = sut
+                            .GetAttributesOn(x => x.Signup(null))
+                            .OfType<HttpPostAttribute>().FirstOrDefault();
+
             Assert.NotNull(attribute);
         }
 
@@ -111,7 +163,10 @@ namespace AllReady.UnitTest.Controllers
         {
             var sut = EventControllerBuilder.Instance().Build();
 
-            var attribute = sut.GetAttributesOn(x => x.Signup(null)).OfType<AuthorizeAttribute>().FirstOrDefault();
+            var attribute = sut
+                            .GetAttributesOn(x => x.Signup(null))
+                            .OfType<AuthorizeAttribute>().FirstOrDefault();
+
             Assert.NotNull(attribute);
         }
 
@@ -120,7 +175,10 @@ namespace AllReady.UnitTest.Controllers
         {
             var sut = EventControllerBuilder.Instance().Build();
 
-            var attribute = sut.GetAttributesOn(x => x.Signup(null)).OfType<ValidateAntiForgeryTokenAttribute>().FirstOrDefault();
+            var attribute = sut
+                            .GetAttributesOn(x => x.Signup(null))
+                            .OfType<ValidateAntiForgeryTokenAttribute>().FirstOrDefault();
+
             Assert.NotNull(attribute);
         }
 
@@ -129,7 +187,10 @@ namespace AllReady.UnitTest.Controllers
         {
             var sut = EventControllerBuilder.Instance().Build();
 
-            var attribute = sut.GetAttributesOn(x => x.Signup(null)).OfType<RouteAttribute>().FirstOrDefault();
+            var attribute = sut
+                            .GetAttributesOn(x => x.Signup(null))
+                            .OfType<RouteAttribute>().FirstOrDefault();
+
             Assert.NotNull(attribute);
         }
 
@@ -172,6 +233,8 @@ namespace AllReady.UnitTest.Controllers
         private class EventControllerBuilder
         {
             private IMediator _mediator;
+            private ControllerContext _controllerContext;
+            private static EventControllerBuilder _builder;
             private EventControllerBuilder()
             {
                 MediatorMock = new Mock<IMediator>();
@@ -180,20 +243,47 @@ namespace AllReady.UnitTest.Controllers
 
             public static EventControllerBuilder Instance()
             {
-                return new EventControllerBuilder();
+                if (_builder == null)
+                    _builder = new EventControllerBuilder();
+
+                return _builder;
             }
 
             public EventController Build()
             {
+                if (_controllerContext == null)
+                    return new EventController(_mediator, null);
 
-                return new EventController(_mediator, null);
+                return new EventController(_mediator, null)
+                {
+                    ControllerContext = _controllerContext
+                };
             }
 
             public Mock<IMediator> MediatorMock { get; }
+
             public EventControllerBuilder WithMediator()
             {
 
                 _mediator = MediatorMock.Object;
+                return this;
+            }
+
+            public EventControllerBuilder WithUserLogged()
+            {
+                var httpContext = new Mock<HttpContext>();
+
+                var controllerContext = new Mock<ControllerContext>();
+                var principal = new Moq.Mock<ClaimsPrincipal>();
+
+
+                principal.Setup(p => p.IsInRole("Administrator")).Returns(true);
+                principal.SetupGet(x => x.Identity.Name).Returns("userName");
+
+                httpContext.Setup(x => x.User).Returns(principal.Object);
+                controllerContext.Object.HttpContext = httpContext.Object;
+                _controllerContext = controllerContext.Object;
+
                 return this;
             }
 
