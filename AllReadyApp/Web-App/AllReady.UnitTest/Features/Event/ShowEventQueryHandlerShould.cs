4d9943fd0b3f95cd78fb9da1925668c6dac6a177
gftrader <gage.trader@gmail.com>
2016-08-06 14:00:20 -0500

    Removed unnecessary using statement.

--- AllReadyApp/Web-App/AllReady.UnitTest/Features/Event/ShowEventQueryHandlerShould.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Features/Event/ShowEventQueryHandlerShould.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Security.Claims;
-using System.Security.Principal;
 using AllReady.Features.Event;
 using AllReady.Models;
 using Microsoft.AspNetCore.Identity;
