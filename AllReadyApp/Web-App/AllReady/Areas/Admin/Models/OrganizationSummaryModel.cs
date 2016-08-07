33088067c064d0f8137b553636cf6482474e3b16
Patricia Curry <PMCurry@users.noreply.github.com>
2016-08-07 15:54:56 -0500

    Capitalize URL across organization admin pages (#1112)

--- AllReadyApp/Web-App/AllReady/Areas/Admin/Models/OrganizationSummaryModel.cs
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Models/OrganizationSummaryModel.cs
@@ -1,10 +1,14 @@
-﻿namespace AllReady.Areas.Admin.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AllReady.Areas.Admin.Models
 {
     public class OrganizationSummaryModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [Display(Name = "Logo URL")]
         public string LogoUrl { get; set; }
+        [Display(Name = "Website URL")]
         public string WebUrl { get; set; }
     }
 }
