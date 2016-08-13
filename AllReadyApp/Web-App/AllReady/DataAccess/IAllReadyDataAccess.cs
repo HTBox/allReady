702e76c943fabb23201d368b692e800de6f65ca6
James Chambers <james@jameschambers.com>
2016-08-13 11:26:36 -0500

    marked interface and methods obsolete, added 0618 disable to proj

--- AllReadyApp/Web-App/AllReady/DataAccess/IAllReadyDataAccess.cs
+++ AllReadyApp/Web-App/AllReady/DataAccess/IAllReadyDataAccess.cs
@@ -1,77 +1,107 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace AllReady.Models
 {
+    [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
     public interface IAllReadyDataAccess
     {
 
         #region Event CRUD
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         IEnumerable<Event> Events { get; }
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         Event GetEvent(int eventId);
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         int GetManagingOrganizationId(int eventId);
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         IEnumerable<EventSignup> GetEventSignups(string userId);
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         IEnumerable<EventSignup> GetEventSignups(int eventId, string userId);
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         Task UpdateEvent(Event value);
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         IEnumerable<Event> EventsByPostalCode(string postalCode, int distance);
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         IEnumerable<Event> EventsByGeography(double latitude, double longitude, int distance);
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         IEnumerable<Resource> GetResourcesByCategory(string category);
 
         #endregion
 
         #region Campaign CRUD
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         IEnumerable<Campaign> Campaigns { get; }
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         Campaign GetCampaign(int campaignId);
 
         #endregion
 
         #region User CRUD
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         IEnumerable<ApplicationUser> Users { get; }
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         ApplicationUser GetUser(string userId);
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         Task UpdateUser(ApplicationUser value);
 
         #endregion
 
         #region EventSignup CRUD
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         IEnumerable<EventSignup> EventSignups { get; }
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         EventSignup GetEventSignup(int eventId, string userId);
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         Task AddEventSignupAsync(EventSignup userSignup);
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         Task DeleteEventAndTaskSignupsAsync(int eventSignupId);
 
         #endregion
 
         #region TaskSignup CRUD
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         IEnumerable<TaskSignup> TaskSignups { get; }
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         Task UpdateTaskSignupAsync(TaskSignup value);
         #endregion
 
         #region AllReadyTask CRUD
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         IEnumerable<AllReadyTask> Tasks { get; }
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         AllReadyTask GetTask(int taskId);
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         Task AddTaskAsync(AllReadyTask task);
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         Task DeleteTaskAsync(int taskId);
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         Task UpdateTaskAsync(AllReadyTask value);
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         IEnumerable<TaskSignup> GetTasksAssignedToUser(int eventId, string userId);
 
         #endregion
 
         #region Request CRUD
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         Task<Request> GetRequestByProviderIdAsync( string providerId );
 
+        [Obsolete("Please favor commands/queries; see https://github.com/HTBox/allReady/issues/1130", false)]
         Task AddRequestAsync( Request request ); 
         #endregion
 
