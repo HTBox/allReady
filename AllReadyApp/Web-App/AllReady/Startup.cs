551509a4cbf9918827f735ccbc8c417fbd9434e7
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-08-17 10:56:02 -0400

    - Add ITaskEditViewModelValidator/TaskEditViewModelValidator to Startup.cs

--- AllReadyApp/Web-App/AllReady/Startup.cs
+++ AllReadyApp/Web-App/AllReady/Startup.cs
@@ -131,6 +131,7 @@ namespace AllReady
         services.AddTransient<ITaskEditViewModelValidator, TaskEditViewModelValidator>();
         services.AddTransient<IItineraryEditModelValidator, ItineraryEditModelValidator>();
         services.AddTransient<IOrganizationEditModelValidator, OrganizationEditModelValidator>();
+        services.AddTransient<ITaskEditViewModelValidator, TaskEditViewModelValidator>();
         services.AddTransient<IRedirectAccountControllerRequests, RedirectAccountControllerRequests>();
         services.AddSingleton<IImageService, ImageService>();
         //services.AddSingleton<GeoService>();
