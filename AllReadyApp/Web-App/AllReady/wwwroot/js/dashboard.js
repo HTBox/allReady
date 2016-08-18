ac6734551dbce8bff7166e295b26500beab99c99
danielepo <dnl.pozzobon@gmail.com>
2016-08-18 13:15:10 +0200

    Pull javascript out of dashboard view

---
+++ AllReadyApp/Web-App/AllReady/wwwroot/js/dashboard.js
@@ -0,0 +1,36 @@
+﻿///<reference path="../lib/jquery/dist/jquery.js" />
+(function ($) {
+    $('#calendarr').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        defaultView: 'month',
+
+        eventSources: [{
+
+            events: function (star, end, timezone, callback) {
+
+                $.get("../../api/Event/" + star.format('YYYY-MM-DD') + "/" + end.format('YYYY-MM-DD'), function (result) {
+                    var events = [];
+                    debugger;
+                    for (var i = 0; i < result.length; i++) {
+                        var event = result[i];
+                        events.push({
+                            title: event.Title,
+                            start: event.StartDateTime,
+                            end: event.EndDateTime,
+                            url: "/Event/" + event.Id
+                        });
+                    }
+                    callback(events);
+
+                });
+            },
+            color: "#d58033"
+        }
+        ]
+    });
+
+})($);
