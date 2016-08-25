9a14d40dfe1bbc736f9d0aab9fc76a5b6f27baf9
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-08-25 14:17:07 -0400

    Fixes incorrect AdjustDateTimeOffsetTo implemenation on DateTimeOffsetProvider

--- AllReadyApp/Web-App/AllReady/Providers/IDateTimeOffsetProvider.cs
+++ AllReadyApp/Web-App/AllReady/Providers/IDateTimeOffsetProvider.cs
@@ -23,14 +23,15 @@ namespace AllReady.Providers
             return new DateTimeOffset(dateTimeOffset.Year, dateTimeOffset.Month, dateTimeOffset.Day, hour, minute, second, timeSpan);
         }
 
-        //only thing that sucks about this is we lose the ability to pass in the hour, minute and second optionally... if we did, the sig of this method would start to look pretty ugly
+        //only thing I don't like about this signature is we lose the ability to pass in the hour, minute and second optionally... if we did, the sig of this method would start to look pretty ugly
         //OR, we put the onus on the caller to make sure the correct hour/minute/second are attached to the start/endDateTimeOffset values provided to the method
         public void AdjustDateTimeOffsetTo(string timeZoneId, DateTimeOffset startDateTimeOffset, DateTimeOffset endDateTimeOffset, out DateTimeOffset convertedStartDate, out DateTimeOffset convertedEndDate)
         {
             var timeZoneInfo = FindSystemTimeZoneBy(timeZoneId);
-            var timeSpan = timeZoneInfo.GetUtcOffset(startDateTimeOffset);
-            convertedStartDate = new DateTimeOffset(startDateTimeOffset.Year, startDateTimeOffset.Month, startDateTimeOffset.Day, 0, 0, 0, timeSpan);
-            convertedEndDate = new DateTimeOffset(endDateTimeOffset.Year, endDateTimeOffset.Month, endDateTimeOffset.Day, 0, 0, 0, timeSpan);
+            var startDateTimeSpan = timeZoneInfo.GetUtcOffset(startDateTimeOffset);
+            var endDateTimeSpan = timeZoneInfo.GetUtcOffset(endDateTimeOffset);
+            convertedStartDate = new DateTimeOffset(startDateTimeOffset.Year, startDateTimeOffset.Month, startDateTimeOffset.Day, 0, 0, 0, startDateTimeSpan);
+            convertedEndDate = new DateTimeOffset(endDateTimeOffset.Year, endDateTimeOffset.Month, endDateTimeOffset.Day, 0, 0, 0, endDateTimeSpan);
         }
 
         private static TimeZoneInfo FindSystemTimeZoneBy(string timeZoneId)
