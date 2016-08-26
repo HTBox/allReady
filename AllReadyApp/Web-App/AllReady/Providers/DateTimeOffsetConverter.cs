630233bc3cec21918da6b30b643be63f0cf95d84
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-08-26 11:14:49 -0400

    Change TimeTagHelper to use IConvertDateTimeOffset

--- AllReadyApp/Web-App/AllReady/Providers/DateTimeOffsetConverter.cs
+++ AllReadyApp/Web-App/AllReady/Providers/DateTimeOffsetConverter.cs
@@ -18,6 +18,14 @@ namespace AllReady.Providers
         public DateTimeOffset ConvertDateTimeOffsetTo(TimeZoneInfo timeZoneInfo, DateTimeOffset dateTimeOffset, int hour = 0, int minute = 0, int second = 0)
         {
             return new DateTimeOffset(dateTimeOffset.Year, dateTimeOffset.Month, dateTimeOffset.Day, hour, minute, second, timeZoneInfo.GetUtcOffset(dateTimeOffset));
+
+            //both of these implemenations lose the ability to specificy the hour, minute and second unless the given dateTimeOffset value being passed into this method
+            //already has those values set
+            //1.
+            //return TimeZoneInfo.ConvertTime(dateTimeOffset, timeZoneInfo);
+            //2.
+            //var timeSpan = timeZoneInfo.GetUtcOffset(dateTimeOffset);
+            //return dateTimeOffset.ToOffset(timeSpan);
         }
 
         private static TimeZoneInfo FindSystemTimeZoneBy(string timeZoneId)
