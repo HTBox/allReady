b46d39c3a83f42f7387c37f1c0014fd6ffb60035
bsstahl <barry@bsstahl.com>
2016-08-27 12:44:04 -0700

    Stub tests to be implemented

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Features/Notifications/NotifyAdminForSignupShould.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using AllReady.Features.Notifications;
+using MediatR;
+
+namespace AllReady.UnitTest.Features.Notifications
+{
+    public class NotifyAdminForSignupShould : InMemoryContextTest
+    {
+        [Fact(Skip = "NotImplemented")]
+        public void PassANotifyVolunteersCommandToTheMediator()
+        {
+            // TODO: Implement test
+        }
+
+        [Fact(Skip = "NotImplemented")]
+        public void SendToTheAdminEmail()
+        {
+            // TODO: Implement test
+        }
+
+        [Fact(Skip = "NotImplemented")]
+        public void LogIfAnExceptionOccurs()
+        {
+            // TODO: Implement test
+        }
+
+        [Fact(Skip = "NotImplemented")]
+        public void SkipNotificationIfAdminEmailIsNotSpecified()
+        {
+            // TODO: Implement test
+        }
+
+
+    }
+}
