fc216d6719d4bfe2aca13848c136dc5aab985ff8
Kacey Gipson <kgipson@gmail.com>
2016-08-27 14:21:02 -0700

    Fixed #1059 Updated time zone diplay text formatting.

--- AllReadyApp/Web-App/AllReady/TagHelpers/TimeZoneNameTagHelper.cs
+++ AllReadyApp/Web-App/AllReady/TagHelpers/TimeZoneNameTagHelper.cs
@@ -12,7 +12,38 @@ namespace AllReady.TagHelpers
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "span";
-            output.Content.SetContent(TimeZoneInfo.FindSystemTimeZoneById(TimeZoneId).StandardName);
+            string timeZoneDisplayName = FormatTimeZoneDisplayName(TimeZoneInfo.FindSystemTimeZoneById(TimeZoneId).DisplayName);
+            output.Content.SetContent(timeZoneDisplayName);
+        }
+
+        /// <summary>
+        /// Method to format display name to remove text in parenthesis, e.g.: "(UTC-06:00) Central Time (US & Canada)" => "Central Time"
+        /// </summary>
+        /// <param name="unformatted">Unformatted time zone display name.</param>
+        /// <returns>Time zone display text that was previously nested in parenthetical text.</returns>
+        public string FormatTimeZoneDisplayName(string unformatted)
+        {
+            // 
+            int begin = 0;
+            int end = unformatted.Length;
+            bool firstParen = true;
+
+            for (int i = 0; i < end; i++)
+            {
+                if (firstParen && unformatted[i] == ')')
+                {
+                    // two char for ") "
+                    begin = i + 2;
+                    firstParen = false;
+                }
+
+                if (!firstParen && unformatted[i] == '(')
+                {
+                    end = i - 1;
+                }
+            }
+
+            return unformatted.Substring(begin, end - begin);
         }
     }
 }
