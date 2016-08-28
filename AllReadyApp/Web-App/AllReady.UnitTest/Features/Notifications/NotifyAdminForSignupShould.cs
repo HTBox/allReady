034a5977f3293a20bf227ce24ea53898e51d4fdf
bsstahl <barry@bsstahl.com>
2016-08-27 22:14:18 -0700

    Implement test SkipNotificationIfAdminEmailIsNotSpecified

--- AllReadyApp/Web-App/AllReady.UnitTest/Features/Notifications/NotifyAdminForSignupShould.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Features/Notifications/NotifyAdminForSignupShould.cs
@@ -24,14 +24,21 @@ namespace AllReady.UnitTest.Features.Notifications
                 Campaigns = new List<Campaign>()
             };
 
-            var testCampaign = new Campaign()
+            var testCampaign1 = new Campaign()
             {
                 Name = "Test Campaign 1",
                 ManagingOrganization = testOrg,
                 CampaignContacts = new List<CampaignContact>()
             };
 
-            var primaryCampaignContact = new CampaignContact()
+            var testCampaign2 = new Campaign()
+            {
+                Name = "Test Campaign 2",
+                ManagingOrganization = testOrg,
+                CampaignContacts = new List<CampaignContact>()
+            };
+
+            var primaryCampaignContact1 = new CampaignContact()
             {
                 ContactType = (int)ContactTypes.Primary,
                 Contact = new Contact()
@@ -40,15 +47,33 @@ namespace AllReady.UnitTest.Features.Notifications
                 }
             };
 
-            var testEvent = new Models.Event()
+            var primaryCampaignContact2 = new CampaignContact()
+            {
+                ContactType = (int)ContactTypes.Primary,
+                Contact = new Contact()
+            };
+
+            var testEvent1 = new Models.Event()
             {
                 Id = 5,
                 Name = "Test Event Name",
-                Campaign = testCampaign,
-                CampaignId = testCampaign.Id,
+                Campaign = testCampaign1,
+                CampaignId = testCampaign1.Id,
                 StartDateTime = DateTime.UtcNow.AddDays(10),
                 EndDateTime = DateTime.UtcNow.AddDays(10).AddHours(8),
                 Location = new Location { Id = 2 },
+                RequiredSkills = new List<EventSkill>()
+            };
+
+            var testEvent2 = new Models.Event()
+            {
+                Id = 7,
+                Name = "Test Event 2",
+                Campaign = testCampaign2,
+                CampaignId = testCampaign2.Id,
+                StartDateTime = DateTime.UtcNow.AddDays(3),
+                EndDateTime = DateTime.UtcNow.AddDays(3).AddHours(4),
+                Location = new Location { Id = 5 },
                 RequiredSkills = new List<EventSkill>()
             };
 
@@ -60,26 +85,40 @@ namespace AllReady.UnitTest.Features.Notifications
             var user2 = new ApplicationUser { UserName = username2, Email = username2, EmailConfirmed = true };
             Context.Users.Add(user2);
 
-            testCampaign.CampaignContacts.Add(primaryCampaignContact);
-            testOrg.Campaigns.Add(testCampaign);
+            testCampaign1.CampaignContacts.Add(primaryCampaignContact1);
+            testCampaign2.CampaignContacts.Add(primaryCampaignContact2);
+
+            testOrg.Campaigns.Add(testCampaign1);
+            testOrg.Campaigns.Add(testCampaign2);
 
             this.Context.Organizations.Add(testOrg);
-            this.Context.Events.Add(testEvent);
+            this.Context.Events.Add(testEvent1);
+            this.Context.Events.Add(testEvent2);
 
             var eventSignups = new List<EventSignup>();
-            eventSignups.Add(new EventSignup { Event = testEvent, User = user1, SignupDateTime = DateTime.UtcNow });
-            eventSignups.Add(new EventSignup { Event = testEvent, User = user2, SignupDateTime = DateTime.UtcNow });
+            eventSignups.Add(new EventSignup { Event = testEvent1, User = user1, SignupDateTime = DateTime.UtcNow });
+            eventSignups.Add(new EventSignup { Event = testEvent1, User = user2, SignupDateTime = DateTime.UtcNow });
+            eventSignups.Add(new EventSignup { Event = testEvent2, User = user1, SignupDateTime = DateTime.UtcNow.AddMinutes(-25) });
             this.Context.EventSignup.AddRange(eventSignups);
 
-            var testTask = new AllReadyTask()
+            var testTask1 = new AllReadyTask()
             {
                 Id = 7,
-                Event = testEvent,
-                EventId = testEvent.Id
+                Event = testEvent1,
+                EventId = testEvent1.Id
                 // Required Skills?
             };
 
-            this.Context.Tasks.Add(testTask);
+            var testTask2 = new AllReadyTask()
+            {
+                Id = 9,
+                Event = testEvent2,
+                EventId = testEvent2.Id
+                // Required Skills?
+            };
+
+            this.Context.Tasks.Add(testTask1);
+            this.Context.Tasks.Add(testTask2);
 
             this.Context.SaveChanges();
         }
@@ -170,12 +209,31 @@ namespace AllReady.UnitTest.Features.Notifications
             // TODO: Implement test
         }
 
-        [Fact(Skip = "NotImplemented")]
-        public void SkipNotificationIfAdminEmailIsNotSpecified()
+        [Fact]
+        public async void SkipNotificationIfAdminEmailIsNotSpecified()
         {
-            // TODO: Implement test
+            var testEvent = Context.Events.Skip(1).First();
+            //var testCampaign = testEvent.Campaign;
+            //var testContact = testCampaign.CampaignContacts.First().Contact;
+            //var expected = testContact.Email;
+
+            var mediator = new Mock<IMediator>();
+            var logger = Mock.Of<ILogger<NotifyAdminForUserUnenrolls>>();
+
+            var options = new TestOptions<GeneralSettings>();
+            options.Value.SiteBaseUrl = "localhost";
+
+            var notification = new VolunteerSignupNotification()
+            {
+                UserId = Context.Users.First().Id,
+                EventId = testEvent.Id,
+                TaskId = Context.Tasks.Skip(1).First().Id
+            };
+
+            var target = new NotifyAdminForSignup(this.Context, mediator.Object, options, logger);
+            await target.Handle(notification);
+
+            mediator.Verify(x => x.SendAsync(It.IsAny<NotifyVolunteersCommand>()), Times.Never);
         }
-
-
     }
 }
