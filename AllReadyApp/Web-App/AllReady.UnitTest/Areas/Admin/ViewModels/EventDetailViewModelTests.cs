9bbf5192e90891aa635052a44ed76fae54309133
stevejgordon <sgordon@hotmail.co.uk>
2016-08-30 09:37:40 +0100

    Adding unit tests for EventDetailViewModel percentage properties.
    Includes a rename of the EventDetailViewModel class also.

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/ViewModels/EventDetailViewModelTests.cs
@@ -0,0 +1,137 @@
+﻿using AllReady.Areas.Admin.ViewModels.Event;
+using Shouldly;
+using Xunit;
+
+namespace AllReady.UnitTest.Areas.Admin.ViewModels
+{
+    public class EventDetailViewModelTests
+    {
+        [Fact]
+        public void DisplayItineraries_ReturnsTrue_WhenEventIsItineraryManaged()
+        {
+            var sut = new EventDetailViewModel {EventType = AllReady.Models.EventType.Itinerary};
+
+            Assert.True(sut.IsItineraryEvent);
+        }
+
+        [Fact]
+        public void DisplayItineraries_ReturnsFalse_WhenEventIsNotItineraryManaged()
+        {
+            var sut = new EventDetailViewModel {EventType = AllReady.Models.EventType.Rally};
+
+            Assert.False(sut.IsItineraryEvent);
+        }
+
+        [Fact]
+        public void UnassignedPercentage_ReturnsZero_WhenTotalRequestsIsZero()
+        {
+            var sut = new EventDetailViewModel
+            {
+                TotalRequests = 0,
+                UnassignedRequests = 0
+            };
+
+            var result = sut.UnassignedPercentage;
+
+            result.ShouldBe("0.0");
+        }
+
+        [Fact]
+        public void UnassignedPercentage_ReturnsCorrectPercentage()
+        {
+            var sut = new EventDetailViewModel
+            {
+                TotalRequests = 100,
+                UnassignedRequests = 20
+            };
+
+            var result = sut.UnassignedPercentage;
+
+            result.ShouldBe("20.0");
+        }
+
+        [Fact]
+        public void AssignedPercentage_ReturnsZero_WhenTotalRequestsIsZero()
+        {
+            var sut = new EventDetailViewModel
+            {
+                TotalRequests = 0,
+                AssignedRequests = 0
+            };
+
+            var result = sut.AssignedPercentage;
+
+            result.ShouldBe("0.0");
+        }
+
+        [Fact]
+        public void AssignedPercentage_ReturnsCorrectPercentage()
+        {
+            var sut = new EventDetailViewModel
+            {
+                TotalRequests = 100,
+                AssignedRequests = 20
+            };
+
+            var result = sut.AssignedPercentage;
+
+            result.ShouldBe("20.0");
+        }
+
+        [Fact]
+        public void CompletedPercentage_ReturnsZero_WhenTotalRequestsIsZero()
+        {
+            var sut = new EventDetailViewModel
+            {
+                TotalRequests = 0,
+                CompletedRequests = 0
+            };
+
+            var result = sut.CompletedPercentage;
+
+            result.ShouldBe("0.0");
+        }
+
+        [Fact]
+        public void CompletedPercentage_ReturnsCorrectPercentage()
+        {
+            var sut = new EventDetailViewModel
+            {
+                TotalRequests = 100,
+                CompletedRequests = 20
+            };
+
+            var result = sut.CompletedPercentage;
+
+            result.ShouldBe("20.0");
+        }
+
+        [Fact]
+        public void CanceledPercentage_ReturnsZero_WhenTotalRequestsIsZero()
+        {
+            var sut = new EventDetailViewModel
+            {
+                TotalRequests = 0,
+                CanceledRequests = 0
+            };
+
+            var result = sut.CanceledPercentage;
+
+            result.ShouldBe("0.0");
+        }
+
+        [Fact]
+        public void CanceledPercentage_ReturnsCorrectPercentage()
+        {
+            var sut = new EventDetailViewModel
+            {
+                TotalRequests = 100,
+                CanceledRequests = 20
+            };
+
+            var result = sut.CanceledPercentage;
+
+            result.ShouldBe("20.0");
+        }
+    }
+}
