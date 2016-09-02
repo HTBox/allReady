a9812a52f5305917e76a4a2aac676bd69d4c3b3b
ankur <ankur@NZWLGPC1470>
2016-09-02 13:09:35 +1200

    write unit test for DeleteOrganizationHandlerAsync #786

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Organizations/DeleteOrganisationHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using AllReady.Areas.Admin.Features.Organizations;
+using AllReady.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AllReady.UnitTest.Areas.Admin.Features.Organizations
+{
+    public class DeleteOrganizationHandlerAsyncTests : InMemoryContextTest
+    {
+        protected override void LoadTestData()
+        {
+            Context.Organizations.Add(new Organization { Id = 1, Name = "Org 1" });
+            Context.Organizations.Add(new Organization { Id = 2, Name = "Org 2" });
+            Context.SaveChanges();
+        }
+
+        [Fact]
+        public async Task DeleteExistingOrganization()
+        {
+            var command = new DeleteOrganizationAsync { Id = 1 };
+            var handler = new DeleteOrganizationHandlerAsync(Context);
+            await handler.Handle(command);
+            var data = Context.Organizations.Count(_ => _.Id == 1);
+            Assert.Equal(0, data);
+        }
+
+        [Fact]
+        public async Task DeleteNonExistantOrganization()
+        {
+            var command = new DeleteOrganizationAsync { Id = 999 };
+            var handler = new DeleteOrganizationHandlerAsync(Context);
+            await handler.Handle(command);
+            Assert.Equal(2, Context.Organizations.Count());
+        }
+    }
+}
