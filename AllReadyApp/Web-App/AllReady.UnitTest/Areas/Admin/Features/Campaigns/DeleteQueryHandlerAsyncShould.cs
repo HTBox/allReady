d8af70e8914c587cdc35dd7d523539ee2ab1a12a
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-09-22 09:58:46 -0400

    Add DeleteQueryHandlerAsyncShould and one unit test

--- AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Campaigns/DeleteQueryHandlerAsyncShould.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Campaigns/DeleteQueryHandlerAsyncShould.cs
@@ -1,12 +1,27 @@
-﻿using Xunit;
+﻿using System.Threading.Tasks;
+using AllReady.Areas.Admin.Features.Campaigns;
+using AllReady.Areas.Admin.ViewModels.Campaign;
+using AllReady.Models;
+using Xunit;
 
 namespace AllReady.UnitTest.Areas.Admin.Features.Campaigns
 {
-    public class DeleteQueryHandlerAsyncShould
+    public class DeleteQueryHandlerAsyncShould : InMemoryContextTest
     {
         [Fact]
-        public void ReturnTheCorrectVieModel()
+        public async Task ReturnTheCorrectVieModel()
         {
+            const int campaignId = 1;
+
+            Context.Campaigns.Add(new Campaign { Id = campaignId, ManagingOrganization = new Organization() });
+            Context.SaveChanges();
+
+            var message = new DeleteQueryAsync { CampaignId = campaignId };
+
+            var sut = new DeleteQueryHandlerAsync(Context);
+            var result = await sut.Handle(message);
+
+            Assert.IsType<DeleteViewModel>(result);
         }
 
         [Fact]
