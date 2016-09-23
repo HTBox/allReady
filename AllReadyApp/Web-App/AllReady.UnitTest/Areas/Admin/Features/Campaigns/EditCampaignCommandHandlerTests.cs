9fe2ae0d8e21069891126071d86c6be52ba7df72
stevejgordon <sgordon@hotmail.co.uk>
2016-09-23 13:06:54 +0100

    Re-enabling and updating two tests for EditCampaignCommandHandler.
    Combining various test classes we have for this handler into a single test class.

--- AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Campaigns/EditCampaignCommandHandlerTests.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Campaigns/EditCampaignCommandHandlerTests.cs
@@ -1,5 +1,4 @@
 ﻿using AllReady.Areas.Admin.Features.Campaigns;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 using AllReady.Areas.Admin.ViewModels.Campaign;
@@ -7,85 +6,50 @@ using AllReady.Areas.Admin.ViewModels.Shared;
 using Xunit;
 using Moq;
 using AllReady.Providers;
+using Shouldly;
 
 namespace AllReady.UnitTest.Areas.Admin.Features.Campaigns
 {
     public class EditCampaignCommandHandlerTests : InMemoryContextTest
     {
-        [Fact(Skip = "RTM Broken Tests")]
-        public async Task AddNewCampaign()
+        [Fact]
+        public async Task WhenCampaignDoesNotExist_NewCampaignIsAddedToTheDatabase()
         {
-            //Arrange
-            var handler = new EditCampaignCommandHandlerAsync(Context, Mock.Of<IConvertDateTimeOffset>());
-            var newCampaign = new CampaignSummaryViewModel { Name = "New", Description = "Desc", TimeZoneId = "UTC" };
+            const string name = "Test campaign";
 
-            //Act
-            var result = await handler.Handle(new EditCampaignCommandAsync { Campaign = newCampaign });
-
-            // Assert
-            Assert.Equal(5, Context.Campaigns.Count());
-            Assert.True(result > 0);
-        }
-
-        /// <summary>
-        /// Tests that the columms belonging the campaign table record are actually updated when a campaign is edited
-        /// </summary>
-        /// <remarks>This test is not testing the creation of location record, or impact record as those should be seperate tests</remarks>
-        [Fact(Skip = "RTM Broken Tests")]
-        public async Task UpdatingExistingCampaignUpdatesAllCoreProperties()
-        {
-            // Arrange
-            var name = "New Name";
-            var desc = "New Desc";
-            var fullDesc = "New Full Desc";
-            var timezoneId = "GMT Standard Time";
-            var startDate = DateTime.Now;
-            var endDate = DateTime.Now.AddDays(30);
-            var org = 2;
-
-            var handler = new EditCampaignCommandHandlerAsync(Context, Mock.Of<IConvertDateTimeOffset>());
-            var updatedCampaign = new CampaignSummaryViewModel
+            // arrange
+            var vm = new CampaignSummaryViewModel
             {
-                Id = 2,
-                Name = name,
-                Description = desc,
-                FullDescription = fullDesc,
-                TimeZoneId = timezoneId,
-                StartDate = startDate,
-                EndDate = endDate,
-                OrganizationId = org,
+                Name = name
             };
 
-            //Act
-            var result = await handler.Handle(new EditCampaignCommandAsync { Campaign = updatedCampaign });
-            var savedCampaign = Context.Campaigns.SingleOrDefault(s => s.Id == 2);
+            var sut = new EditCampaignCommandHandlerAsync(Context, Mock.Of<IConvertDateTimeOffset>());
+            var query = new EditCampaignCommandAsync { Campaign = vm };
 
-            // Assert
-            Assert.Equal(4, Context.Campaigns.Count());
-            Assert.Equal(2, result);
+            // act
+            var result = await sut.Handle(query);
+            var data = Context.Campaigns.Single(rec => rec.Id == result);
 
-            Assert.Equal(name, savedCampaign.Name);
-            Assert.Equal(desc, savedCampaign.Description);
-            Assert.Equal(fullDesc, savedCampaign.FullDescription);
-            Assert.Equal(timezoneId, savedCampaign.TimeZoneId);
-            Assert.NotEqual(startDate.Date, new DateTime()); // We're not testing the date logic in this test, only that a datetime value is saved
-            Assert.NotEqual(endDate.Date, new DateTime()); // We're not testing the date logic in this test, only that a datetime value is saved
-            Assert.Equal(org, savedCampaign.ManagingOrganizationId);
+            // assert
+            result.ShouldBe(1); // Since no prior records ID should be 1
+            data.Name.ShouldBe(name); // The name stored in the DB should match the value passed into the command
         }
 
-        [Fact(Skip = "RTM Broken Tests")]
-        public async Task UpdatingExistingCampaignUpdatesLocationWithAllProperties()
+        [Fact]
+        public async Task UpdatingExistingCampaign_UpdatesLocationWithAllProperties()
         {
+            CampaignsHandlerTestHelper.LoadCampaignssHandlerTestData(Context);
+
             // Arrange
-            var name = "New Name";
-            var desc = "New Desc";
-            var org = 2;
-            var address1 = "Address 1";
-            var address2 = "Address 1";
-            var city = "City";
-            var state = "State";
-            var postcode = "45231";
-            var country = "USA";
+            const string name = "New Name";
+            const string desc = "New Desc";
+            const int org = 2;
+            const string address1 = "Address 1";
+            const string address2 = "Address 1";
+            const string city = "City";
+            const string state = "State";
+            const string postcode = "45231";
+            const string country = "USA";
 
             var handler = new EditCampaignCommandHandlerAsync(Context, Mock.Of<IConvertDateTimeOffset>());
             var updatedCampaign = new CampaignSummaryViewModel
@@ -98,7 +62,7 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Campaigns
                 Location = new LocationEditViewModel { Address1 = address1, Address2 = address2, City = city, State = state, PostalCode = postcode }
             };
 
-            //Act
+            // Act
             await handler.Handle(new EditCampaignCommandAsync { Campaign = updatedCampaign });
             var savedCampaign = Context.Campaigns.SingleOrDefault(s => s.Id == 2);
 
@@ -111,16 +75,19 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Campaigns
             Assert.Equal(country, savedCampaign.Location.Country);
         }
 
-        public async Task UpdatingExistingCampaignWithNoPriorContactAddsContactWithAllProperties()
+        [Fact]
+        public async Task UpdatingExistingCampaign_WithNoPriorContactAddsContactWithAllProperties()
         {
+            CampaignsHandlerTestHelper.LoadCampaignssHandlerTestData(Context);
+
             // Arrange
-            var name = "New Name";
-            var desc = "New Desc";
-            var org = 2;
-            var contactEmail = "jdoe@example.com";
-            var firstname = "John";
-            var lastname = "Doe";
-            var telephone = "01323 111111";
+            const string name = "New Name";
+            const string desc = "New Desc";
+            const int org = 2;
+            const string contactEmail = "jdoe@example.com";
+            const string firstname = "John";
+            const string lastname = "Doe";
+            const string telephone = "01323 111111";
 
             var handler = new EditCampaignCommandHandlerAsync(Context, Mock.Of<IConvertDateTimeOffset>());
             var updatedCampaign = new CampaignSummaryViewModel
@@ -136,7 +103,7 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Campaigns
                 PrimaryContactPhoneNumber = telephone
             };
 
-            //Act
+            // Act
             await handler.Handle(new EditCampaignCommandAsync { Campaign = updatedCampaign });
             var newContact = Context.Contacts.OrderBy(c => c.Id).LastOrDefault();
 
@@ -150,11 +117,6 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Campaigns
             Assert.Equal(firstname, newContact.FirstName);
             Assert.Equal(lastname, newContact.LastName);
             Assert.Equal(telephone, newContact.PhoneNumber);
-        }
-
-        protected override void LoadTestData()
-        {
-            CampaignsHandlerTestHelper.LoadCampaignssHandlerTestData(Context);
         }
     }
 }
