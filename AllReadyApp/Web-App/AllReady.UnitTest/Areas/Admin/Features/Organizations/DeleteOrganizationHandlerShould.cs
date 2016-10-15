a9692955b064e8b45f67b641e95b4d6083b40c1c
Sanil Paul <sanilpaul@gmail.com>
2016-10-15 09:35:54 -0400

    Add tests and cleanup the handler

--- AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Organizations/DeleteOrganizationHandlerShould.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Organizations/DeleteOrganizationHandlerShould.cs
@@ -11,27 +11,19 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Organizations
         protected override void LoadTestData()
         {
             Context.Organizations.Add(new Organization { Id = 1, Name = "Org 1" });
-            Context.Organizations.Add(new Organization { Id = 2, Name = "Org 2" });
             Context.SaveChanges();
         }
 
         [Fact]
-        public async Task DeleteOrganizationThatMatchesOrganizationIdOnCommand()
+        public async Task DeleteTheOrganization()
         {
             var command = new DeleteOrganization { Id = 1 };
+
             var handler = new DeleteOrganizationHandler(Context);
             await handler.Handle(command);
-            var data = Context.Organizations.Count(_ => _.Id == 1);
-            Assert.Equal(0, data);
+
+            Assert.False(Context.Organizations.Any(x => x.Id == command.Id));
         }
 
-        [Fact]
-        public async Task NotDeleteOrganizationsThatDoNotMatchOrganizationIdOnCommand()
-        {
-            var command = new DeleteOrganization { Id = 999 };
-            var handler = new DeleteOrganizationHandler(Context);
-            await handler.Handle(command);
-            Assert.Equal(2, Context.Organizations.Count());
-        }
     }
 }
