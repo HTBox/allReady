1a7d06324bef845a4f60764f55cbd77c84ad1903
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-11-03 20:27:19 -0400

    Add checks to TwitterRepository to see if any of the Twitter authetication settings are the default value. If there is any default values, TwitterRepository will return null and not attempt to use LinqToTwitter to authorize with the SingleUserAuthorizer

--- AllReadyApp/Web-App/AllReady/Providers/ExternalUserInformationProviders/Providers/TwitterRepository.cs
+++ AllReadyApp/Web-App/AllReady/Providers/ExternalUserInformationProviders/Providers/TwitterRepository.cs
@@ -16,6 +16,11 @@ namespace AllReady.Providers.ExternalUserInformationProviders.Providers
 
         public async Task<Account> GetTwitterAccount(string userId, string screenName)
         {
+            if (AnyTwitterAuthenticationSettingsAreNotSet())
+            {
+                return null;
+            }
+
             var authTwitter = new SingleUserAuthorizer
             {
                 CredentialStore = new SingleUserInMemoryCredentialStore
@@ -24,7 +29,6 @@ namespace AllReady.Providers.ExternalUserInformationProviders.Providers
                     ConsumerSecret = twitterAuthenticationSettings.Value.ConsumerSecret,
                     OAuthToken = twitterAuthenticationSettings.Value.OAuthToken,
                     OAuthTokenSecret = twitterAuthenticationSettings.Value.OAuthSecret,
-
                     UserID = ulong.Parse(userId),
                     ScreenName = screenName
                 }
@@ -36,10 +40,16 @@ namespace AllReady.Providers.ExternalUserInformationProviders.Providers
 
             //VERY important you explicitly keep the "== true" part of comparison. ReSharper will prompt you to remove this, and if it does, the query will not work
             var account = await (from acct in twitterCtx.Account
-                                 where (acct.Type == AccountType.VerifyCredentials) && (acct.IncludeEmail == true)
-                                 select acct).SingleOrDefaultAsync();
+                where (acct.Type == AccountType.VerifyCredentials) && (acct.IncludeEmail == true)
+                select acct).SingleOrDefaultAsync();
 
             return account;
+        }
+
+        private bool AnyTwitterAuthenticationSettingsAreNotSet()
+        {
+            return twitterAuthenticationSettings.Value.ConsumerKey == "[twitterconsumerkey]" || twitterAuthenticationSettings.Value.ConsumerSecret == "[twitterconsumersecret]" ||
+                   twitterAuthenticationSettings.Value.OAuthToken == "[twitteroauthtoken]" || twitterAuthenticationSettings.Value.OAuthSecret == "[twitteroauthsecret]";
         }
     }
 }
