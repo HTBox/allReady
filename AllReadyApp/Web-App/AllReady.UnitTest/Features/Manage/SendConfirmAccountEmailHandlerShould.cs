db5b1b87d9f04f9f93571a9e16a47d14eb625203
odia1 <ppatra@seedsnet.org>
2016-11-10 11:21:47 -0700

    Fixed to display the URL in the confirmation email
    Closes #https://github.com/HTBox/allReady/issues/1321

--- AllReadyApp/Web-App/AllReady.UnitTest/Features/Manage/SendConfirmAccountEmailHandlerShould.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Features/Manage/SendConfirmAccountEmailHandlerShould.cs
@@ -12,7 +12,7 @@ namespace AllReady.UnitTest.Features.Manage
         public async Task SendNewEmailAddressApprovalEamilHandlerInvokesSendEmailAsyncWithTheCorrectParameters()
         {
             var message = new SendConfirmAccountEmail { Email = "email", CallbackUrl = "CallbackUrl" };
-            var emailMessage = $"Please confirm your allReady account by clicking this link: <a href=\"{message.CallbackUrl}\">link</a>";
+            var emailMessage = $"Please confirm your allReady account by clicking this link: <a href=\"{message.CallbackUrl}\">{message.CallbackUrl}</a>";
 
             var emailSender = new Mock<IEmailSender>();
             var sut = new SendConfirmAccountEmailHandler(emailSender.Object);
