015cdcacc9877ac99fdc70d8ac5016c3d342a06c
Tony Lambert <tonylambert@mgmail.com>
2016-11-16 07:44:07 -0500

    EditRequestCommandHandler - Verify geocode method
    * Verify the Request's lattitude and longitude are set when a request is
      created or updated.

--- AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Requests/EditRequestCommandHandlerShould.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Requests/EditRequestCommandHandlerShould.cs
@@ -8,6 +8,8 @@ using System;
 using System.Threading.Tasks;
 using Xunit;
 using System.Linq;
+using Geocoding;
+using System.Collections.Generic;
 
 namespace AllReady.UnitTest.Areas.Admin.Features.Requests
 {
@@ -19,8 +21,8 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Requests
             _existingRequest = new Request
             {
                 Address = "1234 Nowhereville",
-                City = "",
-                Name = "",
+                City = "Seattle",
+                Name = "Request unit test",
                 DateAdded = DateTime.MinValue,
                 EventId = 1,
                 Phone = "555-555-5555",
@@ -58,6 +60,24 @@ namespace AllReady.UnitTest.Areas.Admin.Features.Requests
 
             var request = Context.Requests.First(r => r.RequestId == _existingRequest.RequestId);
             Assert.Equal(expectedName, request.Name );
+        }
+
+        [Fact]
+        public async Task AttemptToGeocodeAddressToGetLatitudeAndLongitude()
+        {
+            var mockMediator = new Mock<IMediator>();
+            var mockGeocoder = new Mock<IGeocoder>();
+
+            // Because the Geocode method takes a set of strings as arguments, verify the arguments are passed in to the mock the correct order.
+            mockGeocoder.Setup(g => g.Geocode(_existingRequest.Address, _existingRequest.City, _existingRequest.State, _existingRequest.Zip, It.IsAny<string>())).Returns(new List<Address>());
+
+            var handler = new EditRequestCommandHandler(Context, mockGeocoder.Object);
+            var requestId = await handler.Handle(new EditRequestCommand
+            {
+                RequestModel = new EditRequestViewModel { Address = _existingRequest.Address, City = _existingRequest.City, State = _existingRequest.State, Zip = _existingRequest.Zip }
+            });
+
+            mockGeocoder.Verify(x => x.Geocode(_existingRequest.Address, _existingRequest.City, _existingRequest.State, _existingRequest.Zip, It.IsAny<string>()), Times.Once);
         }
     }
 }
