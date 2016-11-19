80625f01d7da20e1f642577d486fc997a89b2492
naebliss <naebliss666@hotmail.com>
2016-11-19 13:28:51 +0100

    #1530: fixed unittest to be property specific and language fixed

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Extensions/PropertyValidationHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace AllReady.UnitTest.Extensions
+{
+    public class PropertyValidationHelper<TModel>
+    {
+        private readonly TModel _model;
+
+        public PropertyValidationHelper(TModel model)
+        {
+            _model = model;
+        }
+
+        public IList<ValidationResult> ValidateProperty<TProperty>(Expression<Func<TModel, TProperty>> propertyToValidate)
+        {
+            var member = GetMemberInfo(propertyToValidate);
+
+            var ctx = new ValidationContext(_model, null, null)
+            {
+                MemberName = member.Name
+            };
+
+            var propertyValue = propertyToValidate.Compile()(_model);
+
+            var validationResults = new List<ValidationResult>();
+            Validator.TryValidateProperty(propertyValue, ctx, validationResults);
+            return validationResults;
+        }
+
+        private MemberInfo GetMemberInfo<TProperty>(Expression<Func<TModel, TProperty>> expression)
+        {
+            var member = expression.Body as MemberExpression;
+            if (member != null)
+                return member.Member;
+
+            throw new ArgumentException("Expression is not a member access", nameof(expression));
+        }
+    }
+}
