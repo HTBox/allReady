cc0128102efe1d6ed68b4f268aaf8d5fffcbdfc6
rkiggen <robby.kiggen@essential-it.be>
2016-11-19 15:34:45 +0100

    changed validation message for Rule 'End date cannot be out of range of parent event'

--- AllReadyApp/Web-App/AllReady/Areas/Admin/ViewModels/Validators/Task/TaskEditViewModelValidator.cs
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/ViewModels/Validators/Task/TaskEditViewModelValidator.cs
@@ -39,13 +39,13 @@ namespace AllReady.Areas.Admin.ViewModels.Validators.Task
             // Rule - Start date cannot be out of range of parent event
             if (viewModel.StartDateTime < parentEvent.StartDateTime)
             {
-                result.Add(new KeyValuePair<string, string>(nameof(viewModel.StartDateTime), "Start date cannot be earlier than the event start date " + parentEvent.StartDateTime.ToString("d")));
+                result.Add(new KeyValuePair<string, string>(nameof(viewModel.StartDateTime), "Start date cannot be earlier than the event start date " + parentEvent.StartDateTime.ToString("f")));
             }
 
             // Rule - End date cannot be out of range of parent event
             if (viewModel.EndDateTime > parentEvent.EndDateTime)
             {
-                result.Add(new KeyValuePair<string, string>(nameof(viewModel.EndDateTime), "End date cannot be later than the event end date " + parentEvent.EndDateTime.ToString("d")));
+                result.Add(new KeyValuePair<string, string>(nameof(viewModel.EndDateTime), "The end date of this task cannot be after the end date of the event " + parentEvent.EndDateTime.ToString("f")));
             }
 
             // Rule - Itinerary tasks must start and end on same calendar day
