a0ee3fb677b019009127b586e2ceb44d27ecfd9a
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-11-25 09:41:19 -0500

    Implement RequestExistsByProviderIdQueryHandlerShould

--- AllReadyApp/Web-App/AllReady.UnitTest/Features/Requests/RequestExistsByProviderIdQueryHandlerShould.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Features/Requests/RequestExistsByProviderIdQueryHandlerShould.cs
@@ -1,11 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AllReady.Features.Requests;
+using AllReady.Models;
+using Xunit;
 
 namespace AllReady.UnitTest.Features.Requests
 {
-    public class RequestExistsByProviderIdQueryHandlerShould
+    public class RequestExistsByProviderIdQueryHandlerShould : InMemoryContextTest
     {
+        [Fact]
+        public void ReturnsTrueWhenRequestExists()
+        {
+            const string providerRequestId = "ProviderId";
+
+            var request = new Request { ProviderId = providerRequestId };
+            Context.Requests.Add(request);
+            Context.SaveChanges();
+
+            var message = new RequestExistsByProviderIdQuery { ProviderRequestId = providerRequestId };
+
+            var sut = new RequestExistsByProviderIdQueryHandler(Context);
+            var result = sut.Handle(message);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ReturnsFalseWhenRequestDoesNotExist()
+        {
+            const string providerRequestId = "AnotherProviderId";
+
+            var message = new RequestExistsByProviderIdQuery { ProviderRequestId = providerRequestId };
+
+            var sut = new RequestExistsByProviderIdQueryHandler(Context);
+            var result = sut.Handle(message);
+
+            Assert.False(result);
+        }
     }
 }
