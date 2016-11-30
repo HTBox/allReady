7b66859eeb7a579e16b8ca7dd62275da7cf05822
Hamid Mosalla <Xellarix@gmail.com>
2016-11-30 19:55:42 +0330

    Finish event image deletion via ajax request

---
+++ AllReadyApp/Web-App/AllReady/wwwroot/js/Admin/Event/Edit/Main.js
@@ -0,0 +1,10 @@
+﻿SystemJS.config({
+    baseURL: '/js/Admin/Event/Edit',
+    defaultJSExtensions: true
+});
+
+SystemJS.import('EditModule').then(function (editModule) {
+
+    editModule.addDeleteEventImageHandler();
+    editModule.checkForMobileDeviceAndShowImageDeleteButton();
+});
