69a0b95e9ba4ff6abb12b1e973f98b063a4040b6
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-11-30 14:07:25 -0500

    Update SendRequestStatusToGetASmokeAlarmEndpointHandler to use Polly to invoke GASA's endpoint

--- AllReadyApp/Web-App/AllReady/Features/Requests/SendRequestStatusToGetASmokeAlarmEndpointHandler.cs
+++ AllReadyApp/Web-App/AllReady/Features/Requests/SendRequestStatusToGetASmokeAlarmEndpointHandler.cs
@@ -4,6 +4,7 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Polly;
 
 namespace AllReady.Features.Requests
 {
@@ -35,55 +36,38 @@ namespace AllReady.Features.Requests
 
             var updateRequestMessage = new { message.Acceptance, message.Status };
 
-            using (var httpClient = new HttpClient())
+            var retryPolicy = Policy
+                .Handle<HttpRequestException>()
+                .WaitAndRetryAsync(retry, retryCount => TimeSpan.FromSeconds(retryCount * retryCount), onRetry: (exception, timeSpan, retryCount) =>
+                {
+                    logger.LogWarning($"Call to getasmokealarm's {baseAddress} failed for serial {message.Serial} : Retry {retryCount} out of {retry} retries : TimeSpan {timeSpan} : Ex {exception.Message}");
+                });
+
+            try
             {
-                httpClient.BaseAddress = new Uri(baseAddress);
-                httpClient.DefaultRequestHeaders.Accept.Clear();
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                httpClient.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(token);
+                var response = await retryPolicy.ExecuteAsync(async () =>
+                {
+                    using (var httpClient = new HttpClient())
+                    {
+                        httpClient.BaseAddress = new Uri(baseAddress);
+                        httpClient.DefaultRequestHeaders.Accept.Clear();
+                        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        httpClient.DefaultRequestHeaders.Add("Authorization", token);
 
-                //https://www.asp.net/web-api/overview/advanced/calling-a-web-api-from-a-net-client
-                //"the PostAsJsonAsync method serializes an object to JSON and then sends the JSON payload in a POST request."
-                var response = await httpClient.PostAsJsonAsync($"admin/requests/status/{message.Serial}", updateRequestMessage);
-                //return await HttpClient.PostAsJsonAsync($"admin/requests/status/{message.Serial}", updateRequestMessage);
+                        //https://www.asp.net/web-api/overview/advanced/calling-a-web-api-from-a-net-client
+                        //"the PostAsJsonAsync method serializes an object to JSON and then sends the JSON payload in a POST request."
+                        return await httpClient.PostAsJsonAsync($"admin/requests/status/{message.Serial}", updateRequestMessage);
+                    }
+                });
 
+                //Throw if not a success code. (this will throw HttpRequestException). HttpClient will not throw an exception on a non-200 http status code the way WebClient did
                 response.EnsureSuccessStatusCode();
             }
-
-            //var retryPolicy = Policy
-            //    .Handle<HttpRequestException>()
-            //    .WaitAndRetryAsync(retry, retryCount => TimeSpan.FromSeconds(retryCount * retryCount), onRetry: (exception, timeSpan, retryCount) =>
-            //    {
-            //        logger.LogWarning($"Call to getasmokealarm's {baseAddress} failed for serial {message.Serial} : Retry {retryCount} out of {retry} retries : TimeSpan {timeSpan} : Ex {exception.Message}");
-            //    });
-
-            //try
-            //{
-            //    var response = await retryPolicy.ExecuteAsync(async () =>
-            //    {
-            //        using (var HttpClient = new HttpClient())
-            //        {
-            //            HttpClient.BaseAddress = new Uri(baseAddress);
-            //            HttpClient.DefaultRequestHeaders.Accept.Clear();
-            //            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            //            HttpClient.DefaultRequestHeaders.Add("Authorization", token);
-
-            //            //return await HttpClient.PostAsJsonAsync($"admin/requests/status/{message.Serial}", json);
-            //            //https://www.asp.net/web-api/overview/advanced/calling-a-web-api-from-a-net-client
-            //            //"the PostAsJsonAsync method serializes an object to JSON and then sends the JSON payload in a POST request."
-            //            //return await HttpClient.PostAsJsonAsync($"admin/requests/status/CHNI-16-00030", json);
-            //            return await HttpClient.PostAsJsonAsync($"admin/requests/status/CHNI-16-00030", updateRequestMessage);
-            //        }
-            //    });
-
-            //    //Throw if not a success code. (this will throw HttpRequestException). HttpClient will not throw an exception on a non-200 http status code the way WebClient did
-            //    response.EnsureSuccessStatusCode();
-            //}
-            ////if an exception is thrown that is NOT part of the .Handle<TException> configured exception, Polly will throw an exception, and we will end up here
-            //catch (Exception ex)
-            //{
-            //    logger.LogCritical($"Call to getasmokealarm's {baseAddress} failed for serial {message.Serial} - with unhandled exception : Ex {ex.Message}");
-            //}
+            //if an exception is thrown that is NOT part of the .Handle<TException> configured exception, Polly will throw an exception, and we will end up here
+            catch (Exception ex)
+            {
+                logger.LogCritical($"Call to getasmokealarm's {baseAddress} failed for serial {message.Serial} - with unhandled exception : Ex {ex.Message}");
+            }
         }
     }
 }
