29be633c750c3a185d5b3be1feadfc46e5e0da9b
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-12-09 14:46:36 -0500

    Change HttpClient to static/singleton for request made from ProcessApiRequests

--- AllReadyApp/Web-App/AllReady/Hangfire/Jobs/SendRequestStatusToGetASmokeAlarm.cs
+++ AllReadyApp/Web-App/AllReady/Hangfire/Jobs/SendRequestStatusToGetASmokeAlarm.cs
@@ -5,17 +5,10 @@ using Microsoft.Extensions.Options;
 
 namespace AllReady.Hangfire.Jobs
 {
-    //TODO mgmccarthy: see if we can run HttpClient statically
-    //http://aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/ BUT,
-    //setting HttpClient to singleton/static instance is NOT a true fix. Other work needs to be done in order for a static HttpClient to honor DNS changes
-    //this article explicitly mentinos deployments with Azure and the blue/green swap that happens when you move from Staging to Productoin. If the change in this blog
-    //is not made and HttpClient is run as singleton, you could potentially be posting requests to the wrong environment/server(s)
-    //http://byterot.blogspot.co.uk/2016/07/singleton-httpclient-dns.html
-    //private static readonly HttpClient HttpClient = new HttpClient();
-
     public class SendRequestStatusToGetASmokeAlarm : ISendRequestStatusToGetASmokeAlarm
     {
         private readonly IOptions<GetASmokeAlarmApiSettings> getASmokeAlarmApiSettings;
+        private static readonly HttpClient HttpClient = new HttpClient();
 
         public SendRequestStatusToGetASmokeAlarm(IOptions<GetASmokeAlarmApiSettings> getASmokeAlarmApiSettings)
         {
@@ -29,18 +22,15 @@ namespace AllReady.Hangfire.Jobs
 
             var updateRequestMessage = new { acceptance, status };
 
-            using (var httpClient = new HttpClient())
-            {
-                httpClient.BaseAddress = new Uri(baseAddress);
-                httpClient.DefaultRequestHeaders.Accept.Clear();
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                httpClient.DefaultRequestHeaders.Add("Authorization", token);
+            HttpClient.BaseAddress = new Uri(baseAddress);
+            HttpClient.DefaultRequestHeaders.Accept.Clear();
+            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpClient.DefaultRequestHeaders.Add("Authorization", token);
 
-                var response = httpClient.PostAsJsonAsync($"admin/requests/status/{serial}", updateRequestMessage).Result;
+            var response = HttpClient.PostAsJsonAsync($"admin/requests/status/{serial}", updateRequestMessage).Result;
 
-                //Throw HttpRequestException response is not a success code.
-                response.EnsureSuccessStatusCode();
-            }
+            //throw HttpRequestException if response is not a success code.
+            response.EnsureSuccessStatusCode();
         }
     }
 
