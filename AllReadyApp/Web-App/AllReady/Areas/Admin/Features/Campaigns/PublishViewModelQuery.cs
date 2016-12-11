1f5eeb71603397d3df16cbe794fc9fb49e36fbef
Hamid Mosalla <Xellarix@gmail.com>
2016-12-11 21:19:27 +0330

    add migration for published field, change query handlers and related classed across application to only return published campaign, add queryhandler for published confirmed

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Campaigns/PublishViewModelQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AllReady.Areas.Admin.ViewModels.Campaign;
+using MediatR;
+
+namespace AllReady.Areas.Admin.Features.Campaigns
+{
+    public class PublishViewModelQuery: IAsyncRequest<PublishViewModel>
+    {
+        public int CampaignId { get; set; }
+    }
+}
