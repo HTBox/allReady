5ac125c09caa5522d4c9c42e1760068cc174cb5b
Hamid Mosalla <Xellarix@gmail.com>
2016-12-12 23:20:52 +0330

    clean up un-used using statements, add unit tests for PublishViewModelQueryHandler and PublishCampaignCommandHandler

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Features/Campaigns/PublishCampaignCommandHandlerShould.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AllReady.Areas.Admin.Features.Campaigns;
+using AllReady.Models;
+using Xunit;
+
+namespace AllReady.UnitTest.Areas.Admin.Features.Campaigns
+{
+    public class PublishCampaignCommandHandlerShould : InMemoryContextTest
+    {
+        protected override void LoadTestData()
+        {
+            var campaign = new Campaign { Id = 1 };
+            Context.Campaigns.Add(campaign);
+            Context.SaveChanges();
+        }
+
+        [Fact]
+        public async Task ChangePublishedPropertyToTrue()
+        {
+            var command = new PublishCampaignCommand { CampaignId = 1 };
+            var handler = new PublishCampaignCommandHandler(Context);
+            await handler.Handle(command);
+
+            var result = Context.Campaigns.Single(c => c.Id == 1);
+
+            Assert.Equal(true, result.Published);
+        }
+    }
+}
--- AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Campaigns/PublishViewModelQuery.cs
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Campaigns/PublishViewModelQuery.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AllReady.Areas.Admin.ViewModels.Campaign;
+﻿using AllReady.Areas.Admin.ViewModels.Campaign;
 using MediatR;
 
 namespace AllReady.Areas.Admin.Features.Campaigns
