4285ed3cd024a4ca69a2d8058bf5662daf51a95e
Steve Gordon <sgordon@hotmail.co.uk>
2016-12-19 17:31:50 +0000

    Adding unit tests around handlers to ensure they return only published campaigns

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Features/Home/ActiveOrUpcomingCampaignsQueryHandlerShould.cs
@@ -0,0 +1,68 @@
+﻿using AllReady.Features.Home;
+using AllReady.Models;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AllReady.UnitTest.Features.Home
+{
+    public class ActiveOrUpcomingCampaignsQueryHandlerShould : InMemoryContextTest
+    {
+        [Fact]
+        public async Task NotReturnUnPublishedCampaigns()
+        {
+            // Arrange
+            var handler = new ActiveOrUpcomingCampaignsQueryHandler(Context);
+
+            // Act
+            var result = await handler.Handle(new ActiveOrUpcomingCampaignsQuery());
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.Count.ShouldBe(2);
+        }
+
+        protected override void LoadTestData()
+        {
+            var org = new Organization
+            {
+                Name = "Some Organization"
+            };
+
+            Context.Organizations.Add(org);
+
+            Context.Campaigns.Add(new Campaign
+            {
+                Name = "This is published",
+                Featured = false,
+                ManagingOrganization = org,
+                Published = true,
+                StartDateTime = DateTime.UtcNow.AddDays(1),
+                EndDateTime = DateTime.UtcNow.AddDays(10)
+            });
+
+            Context.Campaigns.Add(new Campaign
+            {
+                Name = "This is also published",
+                Featured = false,
+                ManagingOrganization = org,
+                Published = true,
+                StartDateTime = DateTime.UtcNow.AddDays(1),
+                EndDateTime = DateTime.UtcNow.AddDays(10)
+            });
+
+            Context.Campaigns.Add(new Campaign
+            {
+                Name = "This is not published",
+                Featured = false,
+                ManagingOrganization = org,
+                Published = false,
+                StartDateTime = DateTime.UtcNow.AddDays(1),
+                EndDateTime = DateTime.UtcNow.AddDays(10)
+            });
+
+            Context.SaveChanges();
+        }
+    }
+}
