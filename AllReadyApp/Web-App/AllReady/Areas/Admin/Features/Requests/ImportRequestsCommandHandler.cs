ab1232b7cd340ce470ca40ac5d81c64ed06f7c15
Michael McCarthy <mike_mccarthy3@yahoo.com>
2016-12-19 14:08:32 -0500

    Add empty unit test class ImportRequestsCommandHandlerShould
    - update comment in ImportRequestsCommandHandler

--- AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Requests/ImportRequestsCommandHandler.cs
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Requests/ImportRequestsCommandHandler.cs
@@ -41,7 +41,7 @@ namespace AllReady.Areas.Admin.Features.Requests
             
             context.Requests.AddRange(requests);
 
-            //TODO: eventually move IGeocoder invocations to async using azure. Issue #1626 and #1639
+            //TODO mgmccarthy: eventually move IGeocoder invocations to async using azure. Issue #1626 and #1639
             foreach (var request in requests)
             {
                 if (request.Latitude == 0 && request.Longitude == 0)
