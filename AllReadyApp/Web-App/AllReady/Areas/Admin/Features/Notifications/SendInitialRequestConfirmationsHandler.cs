22f043344d552feaf90b2d5f9b3277635a230757
Steve Gordon <sgordon@hotmail.co.uk>
2016-12-20 19:01:14 +0000

    Update requests assigned to itinerary handler to notify when notifications sent

--- AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Notifications/SendInitialRequestConfirmationsHandler.cs
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Notifications/SendInitialRequestConfirmationsHandler.cs
@@ -4,6 +4,7 @@ using AllReady.Models;
 using AllReady.Services;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using AllReady.Features.Notifications;
 
 namespace AllReady.Areas.Admin.Features.Notifications
 {
@@ -22,12 +23,23 @@ namespace AllReady.Areas.Admin.Features.Notifications
 
         public async Task Handle(RequestsAssignedToItinerary notification)
         {
-            var requestorPhoneNumbers = await context.Requests.Where(x => notification.RequestIds.Contains(x.RequestId)).Select(x => x.Phone).ToListAsync();
+            var requestData = await context.Requests
+                .Where(x => notification.RequestIds.Contains(x.RequestId))
+                .Select(x => new {
+                    phone = x.Phone,
+                    requestId = x.RequestId
+                }).ToListAsync();
+
             var itinerary = await context.Itineraries.SingleAsync(x => x.Id == notification.ItineraryId);
 
             //TODO mgmccarthy: need to convert itinerary.Date to local time of the request's intinerary's campaign's timezone
-            await smsSender.SendSmsAsync(requestorPhoneNumbers, 
+            await smsSender.SendSmsAsync(requestData.Select(x => x.phone).ToList(), 
                 $@"Your request has been scheduled by allReady for {itinerary.Date.Date}. Please respond with ""Y"" to confirm this request or ""N"" to cancel this request.");
+
+            foreach(var item in requestData)
+            {
+                await mediator.PublishAsync(NotificationSentMessage.SmsMessage(NotificationMessageType.InitialItineraryAssignment, item.phone, requestId: item.requestId, responseRequired: true));
+            }
 
             await mediator.PublishAsync(new InitialRequestConfirmationsSent { ItineraryId = itinerary.Id, RequestIds = notification.RequestIds });
         }
