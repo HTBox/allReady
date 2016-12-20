e4f394afa1551370919906aa44c4286bd9455e8b
Steve Gordon <sgordon@hotmail.co.uk>
2016-12-20 19:00:05 +0000

    Adding EF migration

---
+++ AllReadyApp/Web-App/AllReady/Migrations/20161220185618_AddNotifications.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AllReady.Migrations
+{
+    public partial class AddNotifications : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notification",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(maxLength: 150, nullable: true),
+                    MessageType = table.Column<string>(maxLength: 75, nullable: false),
+                    Method = table.Column<string>(maxLength: 10, nullable: false),
+                    PhoneNumber = table.Column<string>(maxLength: 50, nullable: true),
+                    RequestId = table.Column<Guid>(nullable: true),
+                    ResponseRequired = table.Column<bool>(nullable: false),
+                    SentDateTime = table.Column<DateTime>(nullable: false),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notification", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Notification_Request_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Request",
+                        principalColumn: "RequestId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Notification_ApplicationUser_UserId",
+                        column: x => x.UserId,
+                        principalTable: "ApplicationUser",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notification_RequestId",
+                table: "Notification",
+                column: "RequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notification_UserId",
+                table: "Notification",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notification");
+        }
+    }
+}
