25f6f285e8b90cc53392649b0ccbaea2d0cb7fc7
Chris Sargood <chris.sargood@madgex.com>
2016-12-22 09:50:52 +0000

    #1596 - Added new 'unlinked requests' option to the 'Admin' menu

---
+++ AllReadyApp/Web-App/AllReady.UnitTest/Areas/Admin/Controllers/UnlinkedRequestsControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using AllReady.Areas.Admin.Controllers;
+using AllReady.Areas.Admin.Features.Requests;
+using AllReady.Models;
+using AllReady.UnitTest.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace AllReady.UnitTest.Areas.Admin.Controllers
+{
+    public class UnlinkedRequestsControllerTests
+    {
+        [Fact]
+        public async void ListReturnsRequestUnathorized_WhenUserIsNotAnOrgAdmin()
+        {
+            var mediator = new Mock<IMediator>();
+            var sut = new UnlinkedRequestsController(mediator.Object);
+            sut.MakeUserNotAnOrgAdmin();
+
+            await sut.List();
+            Assert.IsType<UnauthorizedResult>(await sut.List());
+        }
+
+        [Fact]
+        public async void ListCallsRequestListItemsQueryWithUsersOrgId_WhenUserIsOrgAdmin()
+        {
+            const string orgId = "1001";
+
+            var mediator = new Mock<IMediator>();
+            var sut = new UnlinkedRequestsController(mediator.Object);
+            sut.MakeUserAnOrgAdmin(orgId);
+
+            await sut.List();
+
+            mediator.Verify(x => x.SendAsync(It.Is<RequestListItemsQuery>(y => y.Criteria.OrganizationId == 1001)), Times.Once);
+        }
+
+        [Fact]
+        public async void ListCallsRequestListItemsQuerWithRequestStatusUnassigned_WhenUserIsOrgAdmin()
+        {
+            const string orgId = "1001";
+
+            var mediator = new Mock<IMediator>();
+            var sut = new UnlinkedRequestsController(mediator.Object);
+            sut.MakeUserAnOrgAdmin(orgId);
+
+            await sut.List();
+
+            mediator.Verify(x => x.SendAsync(It.Is<RequestListItemsQuery>(y => y.Criteria.Status == RequestStatus.Unassigned)), Times.Once);
+        }
+    }
+}
---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/ViewModels/Request/UnlinkedRequestsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using AllReady.Areas.Admin.ViewModels.Itinerary;
+
+namespace AllReady.Areas.Admin.ViewModels.Request
+{
+    public class UnlinkedRequestsViewModel
+    {
+        public List<RequestListViewModel> Requests { get; set; } = new List<RequestListViewModel>();
+    }
+}
