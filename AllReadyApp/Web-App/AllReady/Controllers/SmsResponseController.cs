90de131c69555b06a333476a26e4fa1f3efed58f
Steve Gordon <sgordon@hotmail.co.uk>
2016-12-29 22:02:29 +0000

    Initial code for handling Twilio responses

---
+++ AllReadyApp/Web-App/AllReady/Controllers/SmsResponseController.cs
@@ -0,0 +1,81 @@
+﻿using AllReady.Features.Requests;
+using AllReady.Hangfire.Jobs;
+using AllReady.Models;
+using AllReady.Services;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace AllReady.Controllers
+{
+    public class SmsResponseController : Controller
+    {
+        private const string Accepted = "y";
+        private const string Declined = "n";
+
+        private readonly IChangeRequestStatus _changeRequestStatus;
+        private readonly IMediator _mediator;
+        private readonly ISmsSender _smsSender;
+
+        public SmsResponseController(IChangeRequestStatus changeRequestStatus, IMediator mediator, ISmsSender smsSender)
+        {
+            _changeRequestStatus = changeRequestStatus;
+            _mediator = mediator;
+            _smsSender = smsSender;
+        }
+
+        [HttpPost("smsresponse")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index(string from, string body)
+        {
+            if (string.IsNullOrWhiteSpace(from))
+            { 
+                // this should never be the case for legitimate requests from Twilio
+                return BadRequest();
+            }
+
+            var requestId = await _mediator.SendAsync(new FindRequestIdByPhoneNumberQuery(from));
+
+            if (requestId != Guid.Empty)
+            {
+                var response = body.ToLower();
+
+                try
+                {
+                    switch (response)
+                    {
+                        case (Accepted):
+                            _changeRequestStatus.To(RequestStatus.Confirmed, requestId); // this method is not async and uses non async db access also - leaving this as is whilst we refine the requirements past v1
+                            await _smsSender.SendSmsAsync(from, "Thank you for confirming you availability.");
+                            break;
+
+                        case (Declined):
+                            _changeRequestStatus.To(RequestStatus.Unassigned, requestId); // this method is not async and uses non async db access also - leaving this as is whilst we refine the requirements past v1
+                            await _smsSender.SendSmsAsync(from, "We have canceled your request and once it is rescheduled you will receive further communication.");
+                            break;
+
+                        default:
+                            // log this
+                            break;
+                    }
+
+                    return Ok();
+                }
+                catch (Exception)
+                {
+                    // log / handle once we determine how logging will be managed in production
+
+                    return StatusCode(500);
+                }
+            }
+            else
+            {
+                // todo - store failed phone matches into DB or a log for review / diagnostics - post v1?
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ AllReadyApp/Web-App/AllReady/Features/Requests/FindRequestIdByPhoneNumberQuery.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System;
+
+namespace AllReady.Features.Requests
+{
+    /// <summary>
+    /// Returns 
+    /// </summary>
+    public class FindRequestIdByPhoneNumberQuery : IAsyncRequest<Guid>
+    {
+        public FindRequestIdByPhoneNumberQuery(string phoneNumber)
+        {
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+            { 
+                throw new ArgumentOutOfRangeException(nameof(phoneNumber));
+            }
+
+            PhoneNumber = phoneNumber;
+        }
+
+        public string PhoneNumber { get; private set; }
+    }
+}
