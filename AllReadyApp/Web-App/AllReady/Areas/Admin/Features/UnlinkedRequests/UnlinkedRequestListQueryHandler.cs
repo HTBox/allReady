1da532fe192dafff203d36a47d652d01d56370e3
Chris Sargood <chris.sargood@madgex.com>
2017-01-06 09:03:18 +0000

    #1596 - Code review changes: Unlinked request controller now queries with null EventId

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/UnlinkedRequests/UnlinkedRequestListQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AllReady.Areas.Admin.ViewModels.UnlinkedRequests;
+using AllReady.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace AllReady.Areas.Admin.Features.UnlinkedRequests
+{
+    public class UnlinkedRequestListQueryHandler : IAsyncRequestHandler<UnlinkedRequestListQuery, List<UnlinkedRequestViewModel>>
+    {
+        private readonly AllReadyContext _context;
+        public UnlinkedRequestListQueryHandler(AllReadyContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<UnlinkedRequestViewModel>> Handle(UnlinkedRequestListQuery message)
+        {
+            return await 
+                _context.Requests
+                .AsNoTracking()
+                .Where(r => r.OrganizationId == message.OrganizationId && !r.EventId.HasValue)
+                .Select(r => new UnlinkedRequestViewModel
+                {
+                    Name = r.Name,
+                    Address = r.Address,
+                    City = r.City,
+                    DateAdded = r.DateAdded,
+                    Zip = r.Zip
+                })
+                .ToListAsync();
+        }
+    }
+}
---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/ViewModels/UnlinkedRequests/UnlinkedRequestViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AllReady.Areas.Admin.ViewModels.UnlinkedRequests
+{
+    public class UnlinkedRequestViewModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
