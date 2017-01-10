17596c0cfb0e6fac7f299d1bfe3c09c6ec662740
Steve Gordon <sgordon@hotmail.co.uk>
2017-01-10 12:45:42 +0000

    Switing to use new IGeocodeService

--- AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Requests/ImportRequestsCommandHandler.cs
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Requests/ImportRequestsCommandHandler.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using AllReady.Models;
-using Geocoding;
 using MediatR;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using AllReady.Services.Mapping;
 
 namespace AllReady.Areas.Admin.Features.Requests
 {
     public class ImportRequestsCommandHandler : AsyncRequestHandler<ImportRequestsCommand>
     {
         private readonly AllReadyContext context;
-        private readonly IGeocoder geocoder;
+        private readonly IGeocodeService geocoder;
         public Func<Guid> NewRequestId = () => Guid.NewGuid();
         public Func<DateTime> DateTimeUtcNow = () => DateTime.UtcNow;
 
-        public ImportRequestsCommandHandler(AllReadyContext context, IGeocoder geocoder)
+        public ImportRequestsCommandHandler(AllReadyContext context, IGeocodeService geocoder)
         {
             this.context = context;
             this.geocoder = geocoder;
@@ -57,10 +57,10 @@ namespace AllReady.Areas.Admin.Features.Requests
                 {
                     if (request.Latitude == 0 && request.Longitude == 0)
                     {
-                        //Assume the first returned address is correct
-                        var address = geocoder.Geocode(request.Address, request.City, request.State, request.Zip, string.Empty).FirstOrDefault();
-                        request.Latitude = address?.Coordinates.Latitude ?? 0;
-                        request.Longitude = address?.Coordinates.Longitude ?? 0;
+                        var coordinates = await geocoder.GetCoordinatesFromAddress(request.Address, request.City, request.State, request.Zip, string.Empty);
+
+                        request.Latitude = coordinates?.Latitude ?? 0;
+                        request.Longitude = coordinates?.Longitude ?? 0;
                     }
                 }
 
