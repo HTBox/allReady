4bf63bfcb254de7a4cb2794bd7732438784aad6a
David Arno <david@davidarno.org>
2017-01-19 22:22:50 +0000

    Fixed one test that I previously missed and left "skipped" (#1746)

--- AllReadyApp/Web-App/AllReady.UnitTest/Controllers/ManageControllerPhoneNumberTests.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Controllers/ManageControllerPhoneNumberTests.cs
@@ -399,7 +399,7 @@ namespace AllReady.UnitTest.Controllers
             Assert.Equal(phoneNumber, viewModel.PhoneNumber);
         }
 
-        [Fact(Skip = "NotImplemented")]
+        [Fact]
         public async Task VerifyPhoneNumberPostSendsUserByUserIdQueryWithCorrectUserId()
         {
             //Arrange
@@ -408,7 +408,7 @@ namespace AllReady.UnitTest.Controllers
             userManagerMock.Setup(x => x.GetUserId(It.IsAny<ClaimsPrincipal>())).Returns(userId);
             var signInManagerMock = MockHelper.CreateSignInManagerMock(userManagerMock);
             var mediator = new Mock<IMediator>();
-            mediator.Setup(x => x.SendAsync(It.IsAny<UserByUserIdQuery>())).ReturnsAsync(new ApplicationUser());
+            mediator.Setup(x => x.SendAsync(It.IsAny<UserByUserIdQuery>())).ReturnsAsync(null);
 
             var controller = new ManageController(userManagerMock.Object, signInManagerMock.Object, mediator.Object);
             controller.SetFakeUser(userId);
