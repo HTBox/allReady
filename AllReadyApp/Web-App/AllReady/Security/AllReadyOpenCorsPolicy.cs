d2b7a809099b3c45cd8f0c8a2ea6dcdec64bdb28
James Chambers <james@jameschambers.com>
2017-01-21 10:25:51 -0600

    Clean up on Startup.cs
    This is a non-functional change to address the growing (and
    disorienting!) `startup.cs`.
    
    - moved configuration options objects and intialization to new
    configuration folder
    - broke out DI/autofac config (would prefer this to be a module, but
    this is a start)
    - moved security config
    - cleaned up namespaces

---
+++ AllReadyApp/Web-App/AllReady/Security/AllReadyOpenCorsPolicy.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.Security
+{
+    public static class AllReadyCorsPolicyFactory
+    {
+        public static CorsPolicy BuildAllReadyOpenCorsPolicy()
+        {
+            var corsBuilder = new CorsPolicyBuilder();
+
+            corsBuilder.AllowAnyHeader();
+            corsBuilder.AllowAnyMethod();
+            // we need to revisit this and build a whitelise of IP addresses
+            corsBuilder.AllowAnyOrigin();
+            corsBuilder.AllowCredentials();
+
+            return corsBuilder.Build();
+        }
+    }
+}
