5d472d0962aef801a010ea3d03217368c73d36d0
Shahid Iqbal <shahid@headforcloud.com>
2017-01-24 19:02:34 +0000

    user email is now included in claims returned from twitter middleware

--- AllReadyApp/Web-App/AllReady/Providers/ExternalUserInformationProviders/Providers/TwitterExternalUserInformationProvider.cs
+++ AllReadyApp/Web-App/AllReady/Providers/ExternalUserInformationProviders/Providers/TwitterExternalUserInformationProvider.cs
@@ -2,16 +2,19 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using AllReady.Services.Twitter;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 
 namespace AllReady.Providers.ExternalUserInformationProviders.Providers
 {
     public class TwitterExternalUserInformationProvider : IProvideExternalUserInformation
     {
         private readonly ITwitterService _twitterService;
+        private readonly ILogger _logger;
 
-        public TwitterExternalUserInformationProvider(ITwitterService twitterService)
+        public TwitterExternalUserInformationProvider(ITwitterService twitterService, ILogger logger)
         {
             _twitterService = twitterService;
+            _logger = logger;
         }
 
         public async Task<ExternalUserInformation> GetExternalUserInformation(ExternalLoginInfo externalLoginInfo)
@@ -20,21 +23,31 @@ namespace AllReady.Providers.ExternalUserInformationProviders.Providers
 
             var userId = externalLoginInfo.Principal.FindFirstValue("urn:twitter:userid");
             var screenName = externalLoginInfo.Principal.FindFirstValue("urn:twitter:screenname");
+            var email = externalLoginInfo.Principal.FindFirstValue(@"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
 
-            var twitterUser = await _twitterService.GetTwitterAccount(userId, screenName);
-
-            if (twitterUser != null)
+            if (string.IsNullOrEmpty(email))
             {
-                if (!string.IsNullOrEmpty(twitterUser.Name))
-                {
-                    var array = twitterUser.Name.Split(' ');
-                    if (array.Length > 1)
-                    {
-                        externalUserInformation.FirstName = array[0];
-                        externalUserInformation.LastName = array[1];
-                    }
-                }
+                _logger.LogWarning($"Failed to retrieve user email address from Twitter, this could be either the setup of the app hasn't enabled email address or this user has a blank/unverified email in Twitter");
+                //either the setup of the app hasn't enabled email address or this user has an unverified twitter email
+                return externalUserInformation;
             }
+
+            externalUserInformation.Email = email;
+
+            //var twitterUser = await _twitterService.GetTwitterAccount(userId, screenName);
+
+            //if (twitterUser != null)
+            //{
+            //    if (!string.IsNullOrEmpty(twitterUser.Name))
+            //    {
+            //        var array = twitterUser.Name.Split(' ');
+            //        if (array.Length > 1)
+            //        {
+            //            externalUserInformation.FirstName = array[0];
+            //            externalUserInformation.LastName = array[1];
+            //        }
+            //    }
+            //}
 
             return externalUserInformation;
         }
