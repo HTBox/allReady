94f7f6fe22e4284e18c44c549c952dd563cceb59
stevejgordon <sgordon@hotmail.co.uk>
2017-01-30 12:39:53 +0000

    Rebase finalisation

---
+++ AllReadyApp/Web-App/AllReady/Services/Mapping/Routing/OptimizeRouteStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace AllReady.Services.Mapping.Routing
+{
+    public class OptimizeRouteResultStatus
+    {
+        public bool IsSuccess { get; set; } = true;
+        public string StatusMessage { get; set; }
+    }
+}
