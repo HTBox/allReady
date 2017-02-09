c44fbf857cd88781f659b258841a179e3d338f54
Martin Rock-Evans <martin.rockevans@gmail.com>
2017-02-09 19:16:19 +0000

    Starting to allow editing of individual goals

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Controllers/GoalController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AllReady.Areas.Admin.Features.Campaigns;
+using AllReady.Areas.Admin.ViewModels.Goal;
+using AllReady.Security;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllReady.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize("OrgAdmin")]
+    public class GoalController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public GoalController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Route("Admin/Goal/Create/{campaignId}")]
+        public async Task<IActionResult> Create(int campaignId)
+        {
+            var campaign = await _mediator.SendAsync(new CampaignSummaryQuery { CampaignId = campaignId });
+            if (campaign == null || !User.IsOrganizationAdmin(campaign.OrganizationId))
+            {
+                return Unauthorized();
+            }
+            var viewModel = new GoalEditViewModel()
+            {
+                CampaignId = campaign.Id,
+                CampaignName = campaign.Name,
+                OrganizationId = campaign.OrganizationId,
+                OrganizationName = campaign.OrganizationName
+            };
+            
+            return View("Edit", viewModel);
+        }
+    }
+}
---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/ViewModels/Goal/GoalSummaryViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using AllReady.Models;
+
+namespace AllReady.Areas.Admin.ViewModels.Goal
+{
+    public class GoalSummaryViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Campaign")]
+        public int CampaignId { get; set; }
+
+        [Display(Name = "Campaign")]
+        public string CampaignName { get; set; }
+
+        [Display(Name = "Organization")]
+        public int OrganizationId { get; set; }
+
+        [Display(Name = "Organization")]
+        public string OrganizationName { get; set; }
+
+        public ImpactType ImpactType { get; set; }
+
+        /// <summary>
+        /// If the impact type is numeric, the value is the number
+        /// of x to reach the goal.
+        /// </summary>
+        public int NumericImpactGoal { get; set; }
+
+        /// <summary>
+        /// If the impact type is numeric, the value is the current
+        /// number of x achieved to reach the NumericImpactGoal value.
+        /// Ideally less than or equal to NumericImpactGoal, but there
+        /// may be certain scenarios where it's greater.
+        /// </summary>
+        public int CurrentImpactLevel { get; set; }
+
+        /// <summary>
+        /// If the impact type is textual, the TextualImpactGoal value
+        /// is displayed. This value is also displayed (if not empty)
+        /// as a descriptor underneath the thermometer display if the
+        /// impact type is numeric.
+        /// </summary>
+        public string TextualImpactGoal { get; set; }
+        public bool Display { get; set; }
+        public int PercentComplete
+        {
+            get
+            {
+                if (NumericImpactGoal > 0)
+                {
+                    return (int)Math.Round(((CurrentImpactLevel * 100.0f) / NumericImpactGoal));
+                }
+                return 0;
+            }
+
+        }
+
+    }
+}
