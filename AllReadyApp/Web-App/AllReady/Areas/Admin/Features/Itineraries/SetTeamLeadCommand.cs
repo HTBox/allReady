f6d4e92f94c2b3301f112b203635129f26c16c13
stevejgordon <sgordon@hotmail.co.uk>
2017-03-02 12:45:16 +0000

    Adding ability to add / replace a team lead

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/Itineraries/SetTeamLeadCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+namespace AllReady.Areas.Admin.Features.Itineraries
+{
+    public class SetTeamLeadCommand : IAsyncRequest<SetTeamLeadResult>
+    {
+        public SetTeamLeadCommand(int itineraryId, int volunteerTaskId)
+        {
+            ItineraryId = itineraryId;
+            VolunteerTaskId = volunteerTaskId;
+        }
+
+        public int ItineraryId { get; }
+        public int VolunteerTaskId { get; }
+    }
+
+    public enum SetTeamLeadResult
+    {
+        Success,
+        VolunteerTaskSignupNotFound,
+        SaveChangesFailed
+    }
+}
