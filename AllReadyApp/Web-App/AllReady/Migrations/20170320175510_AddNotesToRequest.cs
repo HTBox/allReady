9b9a62e38aacad8742ebe267277c9b1147e3e6ab
Shahid Iqbal <shahid@headforcloud.com>
2017-03-20 18:37:35 +0000

    Add db migrations for notes column

---
+++ AllReadyApp/Web-App/AllReady/Migrations/20170320175510_AddNotesToRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace AllReady.Migrations
+{
+    public partial class AddNotesToRequest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name:"Notes", 
+                table:"Request", 
+                nullable:true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notes", 
+                table: "Request");
+        }
+    }
+}
