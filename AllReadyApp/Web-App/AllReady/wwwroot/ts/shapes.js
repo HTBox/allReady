bdde7740ccff612bd476881d7f734593fedb44f1
Daniel Schiavini <daniel.schiavini@gmail.com>
2017-03-27 21:52:50 +0200

    Recovering files deleted by accident

---
+++ AllReadyApp/Web-App/AllReady/wwwroot/ts/shapes.js
@@ -0,0 +1,26 @@
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Shapes;
+    return {
+        setters: [],
+        execute: function () {
+            (function (Shapes) {
+                var Rectangle = (function () {
+                    function Rectangle(height, width) {
+                        this.height = height;
+                        this.width = width;
+                        this.area = width * height;
+                    }
+                    Rectangle.prototype.getArea = function () {
+                        return this.area;
+                    };
+                    return Rectangle;
+                }());
+                Shapes.Rectangle = Rectangle;
+            })(Shapes || (Shapes = {}));
+            exports_1("Shapes", Shapes);
+        }
+    };
+});
+//# sourceMappingURL=shapes.js.map
