8ed0c61cfc30be5a4c89e610eccca09e716a301f
Shahid Iqbal <shahid@headforcloud.com>
2017-03-27 20:52:06 +0100

    re-add migration after rebase

---
+++ AllReadyApp/Web-App/AllReady/Migrations/20170327195124_AddNotesToRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AllReady.Migrations
+{
+    public partial class AddNotesToRequest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Request",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Request");
+        }
+    }
+}
