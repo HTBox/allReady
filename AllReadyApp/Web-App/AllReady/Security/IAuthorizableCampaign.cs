8fffbb41c07c7c1d11917bb52b1477b9f3506fef
Steve Gordon <sgordon@hotmail.co.uk>
2017-04-13 20:14:07 +0100

    Fixing a comment

--- AllReadyApp/Web-App/AllReady/Security/IAuthorizableCampaign.cs
+++ AllReadyApp/Web-App/AllReady/Security/IAuthorizableCampaign.cs
@@ -15,7 +15,7 @@ namespace AllReady.Security
         int OrganizationId { get; }
 
         /// <summary>
-        /// Indicates the user can child objects (events/requests/tasks/itineraries) for the campaign
+        /// Indicates the user can manage child objects (events/requests/tasks/itineraries) for the campaign
         /// </summary>
         /// <remarks>
         /// This can be broken out into sub methods if we require discrete control over managing different type of child object (tasks/requests etc).
