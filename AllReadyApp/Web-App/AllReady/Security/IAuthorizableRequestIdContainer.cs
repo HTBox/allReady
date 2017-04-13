4754d6fc099af2a187314369f7c610b65786f3fc
Steve Gordon <sgordon@hotmail.co.uk>
2017-04-13 22:31:20 +0100

    Adding AuthorizableRequest and Builder

---
+++ AllReadyApp/Web-App/AllReady/Security/IAuthorizableRequestIdContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AllReady.Security
+{
+    /// <summary>
+    /// Contains the IDs that form full knowledge of components required to establish authorization on an <see cref="IAuthorizableRequest"/>
+    /// </summary>
+    /// <remarks>This is used for caching of the results of an <see cref="IAuthorizableRequest"/></remarks>
+    public interface IAuthorizableRequestIdContainer
+    {
+        Guid RequestId { get; }
+        int ItineraryId { get; }
+        int EventId { get; }
+        int CampaignId { get; }        
+        int OrganizationId { get; }
+    }
+}
