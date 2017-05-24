2a1677751876ddb6c0149ddc3d29cffeea520506
Jonas Soderholm <soderholm.jonas@gmail.com>
2017-05-24 21:09:12 +0100

    Added view, controller and handlers for manager invite functionality

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/ViewModels/Invite/InviteViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllReady.Areas.Admin.ViewModels.Invite
+{
+    public enum InviteType
+    {
+        CampaignManagerInvite = 0,
+        EventManagerInvite = 1,
+    }
+
+    public class InviteViewModel
+    {
+        [EmailAddress]
+        [Required]
+        [Display(Name = "Email")]
+        public string InviteeEmailAddress { get; set; }
+
+        [Display(Name = "Message")]
+        public string CustomMessage { get; set; }
+
+        public int EventId { get; set; }
+
+        public int CampaignId { get; set; }
+
+        public InviteType InviteType { get; set; }
+    }
+}
