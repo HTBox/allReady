7289c068cca9707b135dd6e43f1bddb1743b56d2
Jonas Soderholm <soderholm.jonas@gmail.com>
2017-06-11 13:16:19 +0100

    Added Details and List pages

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Features/CampaignManagerInvites/CreateCampaignManagerInviteCommand.cs
@@ -0,0 +1,11 @@
+﻿using AllReady.Areas.Admin.ViewModels.ManagerInvite;
+using MediatR;
+
+namespace AllReady.Areas.Admin.Features.CampaignManagerInvites
+{
+    public class CreateCampaignManagerInviteCommand : IAsyncRequest
+    {
+        public CampaignManagerInviteViewModel Invite { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/ViewModels/ManagerInvite/ManagerInviteViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AllReady.Areas.Admin.ViewModels.ManagerInvite
+{
+    public abstract class ManagerInviteViewModel
+    {
+        public int Id { get; set; }
+
+        [EmailAddress]
+        [Required]
+        [Display(Name = "Email")]
+        public string InviteeEmailAddress { get; set; }
+
+        [Display(Name = "Message")]
+        public string CustomMessage { get; set; }
+
+        public int CampaignId { get; set; }
+
+        public string CampaignName { get; set; }
+    }
+
+    public class EventManagerInviteViewModel : ManagerInviteViewModel
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+    }
+
+    public class CampaignManagerInviteViewModel : ManagerInviteViewModel
+    {
+    }
+}
