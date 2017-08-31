f83632fe8ae467f9b53fff821b5a70d843a3de2a
Steve Gordon <sgordon@hotmail.co.uk>
2017-08-31 13:54:41 +0100

    DbContext fixes post 2.0 upgrade

--- AllReadyApp/Web-App/AllReady/Models/ApplicationUser.cs
+++ AllReadyApp/Web-App/AllReady/Models/ApplicationUser.cs
@@ -81,13 +81,18 @@ namespace AllReady.Models
         }
 
         /// <summary>
+        /// Navigation property for the roles this user belongs to.
+        /// </summary>
+        public virtual ICollection<IdentityUserRole<string>> Roles { get; } = new List<IdentityUserRole<string>>();
+
+        /// <summary>
         /// Navigation property for the claims this user possesses.
         /// </summary>
         public virtual ICollection<IdentityUserClaim<string>> Claims { get; } = new List<IdentityUserClaim<string>>();
 
         /// <summary>
-        /// Navigation property for the roles this user belongs to.
+        /// Navigation property for this users login accounts.
         /// </summary>
-        public virtual ICollection<IdentityRole> Roles { get; } = new List<IdentityRole>();
+        public virtual ICollection<IdentityUserLogin<string>> Logins { get; } = new List<IdentityUserLogin<string>>();
     }
 }
