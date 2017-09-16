2b82dbe0779c06a83538137bf2fa02e2ecc06d0a
jgauffin <jonas@gauffin.org>
2017-09-16 15:01:31 +0200

    Added the possibility to remove volunteers

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/ViewModels/VolunteerTask/RemoveVolunteerViewModel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using AllReady.Models;
+
+namespace AllReady.Areas.Admin.ViewModels.VolunteerTask
+{
+    public class RemoveVolunteerViewModel
+    {
+        [Required]
+        public int TaskId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public bool Notify { get; set; }
+
+    }
+}
