e4c8276b18d9228abaa23f6cd5b08a01a96d562e
Vishal Madhvani <vishal@vishalmadhvani.com>
2017-10-17 19:05:09 +0100

    Remove dependency on bower

--- AllReadyApp/Web-App/AllReady/gulpfile.js
+++ AllReadyApp/Web-App/AllReady/gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require("gulp"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
-    ts = require('gulp-typescript');
+    ts = require('gulp-typescript'),
+    fs = require('fs');
 
 // https://www.npmjs.com/package/gulp-typescript
 // http://weblogs.asp.net/dwahlin/creating-a-typescript-workflow-with-gulp
@@ -18,10 +19,34 @@ paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
 
-
 gulp.task("clean", function (cb) {
     rimraf(paths.concatCssDest, cb);
 });
+
+gulp.task('build:lib', function () {
+    function getNPMDependencies() {
+        var buffer, packages, keys;
+        buffer = fs.readFileSync('package.json');
+        packages = JSON.parse(buffer.toString());
+        keys = [];
+    
+        for (var key in packages.dependencies) {
+            keys.push(key);
+        }
+    
+        return keys;
+    }
+
+    function copyNodeModule(name) {
+        gulp.src('node_modules/' + name + '/**/*')
+            .pipe(gulp.dest('wwwroot/lib/' + name + '/'));
+    };
+
+    var dependencies = getNPMDependencies();
+    for (var i in dependencies) {
+        copyNodeModule(dependencies[i])
+    }
+})
 
 gulp.task('build:ts', function () {
     var tsResult = tsProject.src()
@@ -36,7 +61,7 @@ gulp.task("build:css", function () {
         .pipe(gulp.dest("."));
 });
 
-gulp.task("build", ["build:ts", "build:css"]);
+gulp.task("build", ["build:lib", "build:ts", "build:css"]);
 gulp.task("min", ["build"]);
 
 gulp.task("watch", function () {
