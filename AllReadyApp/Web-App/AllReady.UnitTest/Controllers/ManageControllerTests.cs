74472caf3b96fa8f4f457e06bccb70002c10ee4b
Thomas Fresneau <thom.fresneau@gmail.com>
2017-12-05 10:26:45 +0100

    Write ManageControllerTests.Write ManageControllerTests.ChangePasswordPostReturnsSameViewAndModelWhenModelStateIsInvalid unit test  unit test

--- AllReadyApp/Web-App/AllReady.UnitTest/Controllers/ManageControllerTests.cs
+++ AllReadyApp/Web-App/AllReady.UnitTest/Controllers/ManageControllerTests.cs
@@ -1334,11 +1334,22 @@ namespace AllReady.UnitTest.Controllers
         {
         }
 
-        [Fact(Skip = "NotImplemented")]
+        [Fact]
         public async Task ResendChangeEmailConfirmationSendsUserByUserIdQueryWithCorrectUserId()
         {
-            //delete this line when starting work on this unit test
-            await TaskCompletedTask;
+            const string userId = "UserID";
+
+            var userManagerMock = UserManagerMockHelper.CreateUserManagerMock();
+            userManagerMock.Setup(u => u.GetUserId(It.IsAny<ClaimsPrincipal>())).Returns(userId);
+
+            var mediator = new Mock<IMediator>();
+            mediator.Setup(m => m.SendAsync(It.IsAny<UserByUserIdQuery>())).ReturnsAsync(new ApplicationUser());
+
+            var controller = new ManageController(userManagerMock.Object, null, mediator.Object);
+
+            await controller.ResendChangeEmailConfirmation();
+
+            mediator.Verify(u => u.SendAsync(It.Is<UserByUserIdQuery>(i => i.UserId == userId)), Times.Once);
         }
 
         [Fact(Skip = "NotImplemented")]
