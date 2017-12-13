cd45d124adb6816d5b424c389656f2d4302a747d
Steve Gordon <sgordon@hotmail.co.uk>
2017-12-13 08:23:54 +0000

    Adding controller and view

---
+++ AllReadyApp/Web-App/AllReady/Areas/Admin/Controllers/TeamLeadController.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using AllReady.Areas.Admin.Features.TeamLead;
+using AllReady.Constants;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllReady.Areas.Admin.Controllers
+{
+    [Area(AreaNames.Admin)]
+    public class TeamLeadController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public TeamLeadController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = await _mediator.SendAsync(new TeamLeadItineraryListViewModelQuery(User));
+
+            return View(viewModel);
+        }
+    }
+}
