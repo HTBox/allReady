5575aa60cf95d5a7809ddec2d9d0b124914a0bc9
Gary Chan <gary.chan@c0g1t8.com>
2019-03-07 18:56:37 -0800

    Migrated to Canopy 2 and core 2.1

--- AllReadyApp/AllReady.IntegrationTest/Program.fs
+++ AllReadyApp/AllReady.IntegrationTest/Program.fs
@@ -1,52 +1,36 @@
-﻿open canopy
-open runner
+open canopy.classic
+open canopy.runner.classic
+open canopy.types
 open System
 
-Threading.Thread.CurrentThread.CurrentCulture <- Globalization.CultureInfo "en-US"
+//Threading.Thread.CurrentThread.CurrentCulture <- Globalization.CultureInfo "en-US"
 
 Console.WriteLine("Launching Scenario Tests")
 
 CreatingOrgsAndCampaigns.All Constants.UrlLocalHost
 CreatingActivitiesForCampaigns.All Constants.UrlLocalHost
 
-//Chrome Driver: https://github.com/SeleniumHQ/selenium/wiki/ChromeDriver
+// want to use the chromedriver in current directory to ensure match with current version of Chrome
 canopy.configuration.chromeDir <- System.AppDomain.CurrentDomain.BaseDirectory
-let chromeOptions = OpenQA.Selenium.Chrome.ChromeOptions()
-chromeOptions.AddArgument("--disable-extensions")
-start <| ChromeWithOptions chromeOptions
-pin Left
-resize (1024, 768)
-Console.WriteLine("Launched Chrome")
 
-////Firefox has issues with Selnium 2.5.x.  see https://github.com/lefthandedgoat/canopy/issues/288.  You have to installed version 48.
-//start firefox 
-//pin Right
-//resize(1024, 768)
-//Console.WriteLine("Launched Firefox")
-
-//Test don't work in IE right now.  Many times IE adds a space to the end of the element text
-// IE Driver: https://github.com/SeleniumHQ/selenium/wiki/InternetExplorerDriver
-//canopy.configuration.ieDir <- System.AppDomain.CurrentDomain.BaseDirectory
-//start ie
+//let chromeOptions = OpenQA.Selenium.Chrome.ChromeOptions()
+//chromeOptions.AddArgument("--disable-extensions")
+//start <| ChromeWithOptions chromeOptions
 //pin Left
-//resize(1024, 768)
-//Console.WriteLine("Launched IE")
+//resize (1024, 768)
 
-//Test don't work in Edge right now.  Many times Edge adds a space to the end of the element text
-// Edge Driver: https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
-//canopy.configuration.edgeDir <- System.AppDomain.CurrentDomain.BaseDirectory
-//
-//start edgeBETA
-//pin Right
-//resize(1024, 768)
-//Console.WriteLine("Launched EDGE")
+// start chrome browser, pin to left of screen and resize
+start chrome
+pin Left
+resize (1024,768)
 
+Console.WriteLine("Launched Chrome")
 
 //run all tests
 run()
 
-System.Environment.ExitCode <- runner.failedCount
-Console.WriteLine(String.Format("{0} passed, {1} failed", runner.passedCount, runner.failedCount))
+System.Environment.ExitCode <- failedCount
+Console.WriteLine(String.Format("{0} passed, {1} failed", passedCount, failedCount))
 
 // only turn on when running directly from Visual Studio so that you can look at any errors.
 //Console.WriteLine("Press Enter to Exit")
