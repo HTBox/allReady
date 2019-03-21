d836a51b9aead775775332bacb6dc8e59f965f25
James Chambers <james@jameschambers.com>
2019-03-21 15:42:41 -0500

    about and aesop content

--- AllReadyApp/WebAppV2/Frontend/allready/src/views/About.vue
+++ AllReadyApp/WebAppV2/Frontend/allready/src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div>
+        <p>
+        <router-link to="/about">About AllReady</router-link> /
+        <router-link to="about/aesop">Aesop's Tale</router-link>
+        </p>
+
+        <router-view></router-view>
+    </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import AboutAllready from '@/components/AboutAllready.vue';
+
+export default Vue.extend({
+  name: 'about',
+  components: {
+    AboutAllready,
+  },
+});
+</script>
