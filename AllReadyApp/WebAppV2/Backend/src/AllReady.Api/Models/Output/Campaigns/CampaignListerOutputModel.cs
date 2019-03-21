c18957553700fb024743d66723099adb2abc01c1
Steve Gordon <sgordon@hotmail.co.uk>
2019-03-21 11:22:29 -0700

    Adding short location

--- AllReadyApp/WebAppV2/Backend/src/AllReady.Api/Models/Output/Campaigns/CampaignListerOutputModel.cs
+++ AllReadyApp/WebAppV2/Backend/src/AllReady.Api/Models/Output/Campaigns/CampaignListerOutputModel.cs
@@ -3,6 +3,8 @@ namespace AllReady.Api.Models.Output.Campaigns
 {
     public class CampaignListerOutputModel : CampaignOutputModelBase
     {
+        public string ShortLocation { get; set; }
+
         public string Link { get; set; }
     }
 }
