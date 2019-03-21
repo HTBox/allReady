9a7d7ce2998ebe96b855c26be6061798f4383bd7
David Paquette <david.paquette@usask.ca>
2019-03-21 11:52:29 -0600

    Footer and routes

--- AllReadyApp/WebAppV2/Frontend/allready/src/App.vue
+++ AllReadyApp/WebAppV2/Frontend/allready/src/App.vue
@@ -1,12 +1,8 @@
 <template>
- <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="showDrawer"
-      fixed
-      app
-    >
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="showDrawer" fixed app>
       <v-list dense>
-        <v-list-tile @click="">
+        <v-list-tile to="/">
           <v-list-tile-action>
             <v-icon>home</v-icon>
           </v-list-tile-action>
@@ -14,47 +10,73 @@
             <v-list-tile-title>Home</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile @click="">
+        <v-list-tile to="/about">
           <v-list-tile-action>
             <v-icon>contact_mail</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Contact</v-list-tile-title>
+            <v-list-tile-title>About</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar color="primary" dark fixed app>
       <v-toolbar-side-icon @click.stop="showDrawer = !showDrawer"></v-toolbar-side-icon>
-      <v-toolbar-title><img src="./assets/Web-Logo-small.png"/></v-toolbar-title>
+      <v-toolbar-title>
+        <img src="./assets/Web-Logo-small.png">
+      </v-toolbar-title>
     </v-toolbar>
     <v-content>
       <v-container fluid fill-height>
-        <v-layout
-          justify-center
-          align-center
-        >
-            <router-view/>
+        <v-layout justify-center align-center>
+          <router-view/>
         </v-layout>
       </v-container>
     </v-content>
-    <v-footer color="primary" app>
-      <span class="white--text">&copy; {{new Date().getFullYear()}}</span>
+    <v-footer app>
+      <v-layout justify-center align-left>
+        <a class="footer-icon" href="http://www.htbox.org/blog/" target="_blank">
+          <img alt="Check out the Humanitarian Toolbox blog" src="./assets/blog-small.png">
+        </a>
+        <a class="footer-icon" href="https://www.facebook.com/allReadyApp/" target="_blank">
+          <img alt="Go to allReady on Facebook" src="./assets/fb-small.png">
+        </a>
+        <a class="footer-icon" href="https://twitter.com/htbox" target="_blank">
+          <img alt="Follow Humanitarian Toolbox on Twitter" src="./assets/twitter-small.png">
+        </a>
+        <a
+          class="footer-icon"
+          href="https://www.youtube.com/watch?v=BgV9jUJduwE&amp;index=3&amp;list=PLReL099Y5nRfJCkJtGFHZlVCvx2t8OWdA"
+          target="_blank"
+        >
+          <img alt="YouTube" src="./assets/youtube-small.png">
+        </a>
+        <v-spacer></v-spacer>
+        <div>
+        Powered by
+        <a href="http://www.htbox.org/" target="_blank">Humanitarian Toolbox</a>
+      </div>
+      </v-layout>
+
     </v-footer>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-  components: {
-  },
+  components: {},
   data() {
     return {
-        showDrawer: false
+      showDrawer: false
       //
     };
   }
 };
 </script>
+<style>
+.footer-icon {
+    margin: 5px;
+}
+</style>
+
