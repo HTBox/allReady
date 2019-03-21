6b8f7268f9a8981c0b507f8d6a0214e704f6b9c2
Steve Gordon <sgordon@hotmail.co.uk>
2019-03-21 11:22:18 -0700

    Adding events to campaign

--- AllReadyApp/WebAppV2/Backend/src/AllReady.Api/Controllers/CampaignsController.cs
+++ AllReadyApp/WebAppV2/Backend/src/AllReady.Api/Controllers/CampaignsController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using AllReady.Api.Models.Output.Campaigns;
+using AllReady.Api.Models.Output.Events;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using NodaTime;
@@ -68,7 +69,23 @@ namespace AllReady.Api.Controllers
                 FullDesription = "This is even longer, rambling description of the campaign, which most users will not bother to read!",
                 StartDateTime = new LocalDate(2019, 01, 01),
                 EndDateTime = new LocalDate(2019, 06, 30),
-                TimeZone = DateTimeZoneProviders.Tzdb["Europe/London"]
+                TimeZone = DateTimeZoneProviders.Tzdb["Europe/London"],
+                Events = new List<EventListerOutputModel>
+                {
+                    new EventListerOutputModel
+                    {
+                        Id = 1,
+                        Name = "Charity Event 1",
+                        ShortDescription = "Marketing activities",
+                        StartDateTime = new LocalDate(2019, 01, 01),
+                        EndDateTime = new LocalDate(2019, 01, 31),
+                        TimeZone = DateTimeZoneProviders.Tzdb["Europe/London"],
+                        Link = _linkGenerator.GetPathByAction(
+                            HttpContext,
+                            "Get",
+                            values: new { id = 1 })
+                    }
+                }
             };
 
             return campaign;
--- AllReadyApp/WebAppV2/Backend/src/AllReady.Api/Models/Output/Campaigns/CampaignOutputModel.cs
+++ AllReadyApp/WebAppV2/Backend/src/AllReady.Api/Models/Output/Campaigns/CampaignOutputModel.cs
@@ -1,8 +1,12 @@
+using AllReady.Api.Models.Output.Events;
+using System.Collections.Generic;
 
 namespace AllReady.Api.Models.Output.Campaigns
 {
     public class CampaignOutputModel : CampaignOutputModelBase
     {
         public string FullDesription { get; set; }
+
+        public IEnumerable<EventListerOutputModel> Events { get; set; }
     }
 }
