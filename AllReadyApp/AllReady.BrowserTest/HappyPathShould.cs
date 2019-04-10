2d62d8dd2df6908664fe3055183e53be45137a17
Gary Chan <gary.chan@c0g1t8.com>
2019-04-10 13:54:15 -0700

    Made CreateNewOrganization test data driven

--- AllReadyApp/AllReady.BrowserTest/HappyPathShould.cs
+++ AllReadyApp/AllReady.BrowserTest/HappyPathShould.cs
@@ -1,3 +1,4 @@
+using AllReady.BrowserTest.Models;
 using AllReady.BrowserTest.Pages;
 using Microsoft.Extensions.Configuration;
 using OpenQA.Selenium;
@@ -56,7 +57,7 @@ namespace AllReady.BrowserTest
             Assert.Equal(_driver.Title, homePage.Title);
         }
 
-        string UniqueName(string s)
+        static string UniqueName(string s)
         {
             string tag = DateTime.Now.ToString("yyyyMMddHHmmss");
             string name = $"[ST] {s} {tag}";
@@ -64,9 +65,27 @@ namespace AllReady.BrowserTest
             return name;
         }
 
+        public static IEnumerable<object[]> TestDataForCreateNewOrganization()
+        {
+            yield return new object[]
+            {
+                User.Role.AllReadyAdministrator,
+                new Organization
+                {
+                    Name = UniqueName("Organization"),
+                    PrivacyPolicy = "Privacy for all",
+                    LocationAddress1 = "1 Microsoft Way",
+                    LocationCity = "Redmond",
+                    LocationState = "WA",
+                    LocationPostalCode = "98052",
+                    LocationCountry = "US"
+                }
+            };
+        }
+
         [Theory]
-        [InlineData(User.Role.AllReadyAdministrator)]
-        public void CreateNewOrganization(User.Role role)
+        [MemberData(nameof(TestDataForCreateNewOrganization))]
+        public void CreateNewOrganization(User.Role role, Organization organization)
         {
             var user = new User(role);
             var loginPage = new Page(_driver).Menu.OpenLoginPage();
@@ -80,23 +99,22 @@ namespace AllReady.BrowserTest
             var adminOrganizationCreatePage = adminOrganizationPage.ClickCreateNew();
             Assert.Equal(_driver.Title, adminOrganizationCreatePage.Title);
 
-            string organizationName = UniqueName("Organization");
-
+            // fill in form
             var adminOrganizationDetatilsPage = adminOrganizationCreatePage
-                .Set(p => p.Name, organizationName)
+                .Set(p => p.Name, organization.Name)
                 // need to click privacy policy text button for PrivacyPolicy to appear
                 .Click(p => p.PrivacyPolicyTextButton)
-                .SetControl(p => p.PrivacyPolicy, "Privacy for all")
+                .SetControl(p => p.PrivacyPolicy, organization.PrivacyPolicy)
                 // location information
-                .Set(p => p.LocationAddress1, "123 Main St")
-                .Set(p => p.LocationCity, "Hollywood")
-                .Set(p => p.LocationState, "CA")
-                .Set(p => p.LocationPostalCode, "99120")
-                .Set(p => p.LocationCountry, "USA")
+                .Set(p => p.LocationAddress1, organization.LocationAddress1)
+                .Set(p => p.LocationCity, organization.LocationCity)
+                .Set(p => p.LocationState, organization.LocationState)
+                .Set(p => p.LocationPostalCode, organization.LocationPostalCode)
+                .Set(p => p.LocationCountry, organization.LocationCountry)
                 // 
                 .Submit();
 
-            Assert.Equal($"{organizationName} - allReady", _driver.Title);
+            Assert.Equal($"{organization.Name} - allReady", _driver.Title);
 
             var homePage = adminOrganizationDetatilsPage.Menu.Logoff();
             Assert.Equal(_driver.Title, homePage.Title);
---
+++ AllReadyApp/AllReady.BrowserTest/Models/Organization.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AllReady.BrowserTest.Models
+{
+    public class Organization
+    {
+        public string Name { get; set; }
+        public string PrivacyPolicy { get; set; }
+        public string LocationAddress1 { get; set; }
+        public string LocationCity { get; set; }
+        public string LocationState { get; set; }
+        public string LocationPostalCode { get; set; }
+        public string LocationCountry { get; set; }
+    }
+}
